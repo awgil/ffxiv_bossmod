fa7512d901de796aa88b31d9a3604eb663f291dc
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2022-03-30 22:43:21 +0300

    Automatic boss module registration.

--- BossMod/Modules/ModuleRegistry.cs
+++ BossMod/Modules/ModuleRegistry.cs
@@ -1,33 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace BossMod
 {
     public static class ModuleRegistry
     {
+        private static Dictionary<uint, Type> _modules = new();
+
+        private static IEnumerable<Type?> GetAllTypes()
+        {
+            try
+            {
+                return Assembly.GetExecutingAssembly().DefinedTypes;
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                return e.Types;
+            }
+        }
+
+        static ModuleRegistry()
+        {
+            var baseType = typeof(BossModule);
+            foreach (var t in GetAllTypes().Where(baseType.IsAssignableFrom))
+            {
+                var oidType = t?.Module.GetType($"{t.Namespace}.OID");
+                if (oidType == null || !oidType.IsEnum)
+                    continue;
+                object? oid;
+                if (Enum.TryParse(oidType, "Boss", out oid))
+                    _modules[(uint)oid!] = t!;
+            }
+        }
+
         public static Type? TypeForOID(uint oid)
         {
-            return oid switch {
-                (uint)Endwalker.ZodiarkEx.OID.Boss => typeof(Endwalker.ZodiarkEx.ZodiarkEx),
-                (uint)Endwalker.HydaelynEx.OID.Boss => typeof(Endwalker.HydaelynEx.HydaelynEx),
-                (uint)Endwalker.P1S.OID.Boss => typeof(Endwalker.P1S.P1S),
-                (uint)Endwalker.P2S.OID.Boss => typeof(Endwalker.P2S.P2S),
-                (uint)Endwalker.P3S.OID.Boss => typeof(Endwalker.P3S.P3S),
-                (uint)Endwalker.P4S1.OID.Boss => typeof(Endwalker.P4S1.P4S1),
-                (uint)Endwalker.P4S2.OID.Boss => typeof(Endwalker.P4S2.P4S2),
-                (uint)Endwalker.ARanks.Hulder.OID.Boss => typeof(Endwalker.ARanks.Hulder.Hulder),
-                (uint)Endwalker.ARanks.Storsie.OID.Boss => typeof(Endwalker.ARanks.Storsie.Storsie),
-                (uint)Endwalker.ARanks.Sugriva.OID.Boss => typeof(Endwalker.ARanks.Sugriva.Sugriva),
-                (uint)Endwalker.ARanks.Yilan.OID.Boss => typeof(Endwalker.ARanks.Yilan.Yilan),
-                (uint)Endwalker.ARanks.Minerva.OID.Boss => typeof(Endwalker.ARanks.Minerva.Minerva),
-                (uint)Endwalker.ARanks.Aegeiros.OID.Boss => typeof(Endwalker.ARanks.Aegeiros.Aegeiros),
-                (uint)Endwalker.ARanks.MoussePrincess.OID.Boss => typeof(Endwalker.ARanks.MoussePrincess.MoussePrincess),
-                (uint)Endwalker.ARanks.LunatenderQueen.OID.Boss => typeof(Endwalker.ARanks.LunatenderQueen.LunatenderQueen),
-                (uint)Endwalker.ARanks.Petalodus.OID.Boss => typeof(Endwalker.ARanks.Petalodus.Petalodus),
-                (uint)Endwalker.ARanks.Gurangatch.OID.Boss => typeof(Endwalker.ARanks.Gurangatch.Gurangatch),
-                (uint)Endwalker.ARanks.ArchEta.OID.Boss => typeof(Endwalker.ARanks.ArchEta.ArchEta),
-                (uint)Endwalker.ARanks.FanAil.OID.Boss => typeof(Endwalker.ARanks.FanAil.FanAil),
-                _ => null,
-            };
+            return _modules.GetValueOrDefault(oid);
         }
 
         public static BossModule? CreateModule(Type? type, BossModuleManager manager, Actor primary)
