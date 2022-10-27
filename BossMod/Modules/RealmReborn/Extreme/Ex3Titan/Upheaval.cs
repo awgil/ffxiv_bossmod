d973f13060dbb8a29acc657c1ffb6885d151a483
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2022-10-27 15:36:22 +0300

    Upheaval hack.

--- BossMod/Modules/RealmReborn/Extreme/Ex3Titan/Upheaval.cs
+++ BossMod/Modules/RealmReborn/Extreme/Ex3Titan/Upheaval.cs
@@ -1,20 +1,35 @@
-﻿using System.Linq;
+﻿using System;
 
 namespace BossMod.RealmReborn.Extreme.Ex3Titan
 {
-    class Upheaval : Components.KnockbackFromCaster
+    // TODO: most of what's here should be handled by KnockbackFromCaster component...
+    class Upheaval : Components.KnockbackFromPoints
     {
-        public Upheaval() : base(ActionID.MakeSpell(AID.Upheaval), 13) { }
+        private DateTime _remainInPosition;
+
+        public Upheaval() : base(13, ActionID.MakeSpell(AID.Upheaval)) { }
 
         public override void AddAIHints(BossModule module, int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
         {
-            var caster = ActiveCasters.FirstOrDefault();
-            if (caster != null)
+            if (_remainInPosition > module.WorldState.CurrentTime)
             {
                 // stack just behind boss, this is a good place to bait imminent landslide correctly
-                var pos = module.PrimaryActor.Position + new WDir(0, 2);
-                hints.AddForbiddenZone(ShapeDistance.InvertedCircle(pos, 1.5f), caster.CastInfo!.FinishAt);
+                var dirToCenter = (module.Bounds.Center - module.PrimaryActor.Position).Normalized();
+                var pos = module.PrimaryActor.Position + 2 * dirToCenter;
+                hints.AddForbiddenZone(ShapeDistance.InvertedCircle(pos, 1.5f), _remainInPosition);
             }
+        }
+
+        public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if (spell.Action == WatchedAction)
+                _remainInPosition = spell.FinishAt.AddSeconds(1); // TODO: just wait for effectresult instead...
+        }
+
+        public override void OnCastFinished(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if (spell.Action == WatchedAction)
+                _remainInPosition = module.WorldState.CurrentTime.AddSeconds(1); // TODO: just wait for effectresult instead...
         }
     }
 }
