d8339278c5622100b9650bc13824d14f0471b54b
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2022-10-27 15:22:53 +0300

    Fixed upheaval positioning for titan ex.

--- BossMod/Modules/RealmReborn/Extreme/Ex3Titan/Upheaval.cs
+++ BossMod/Modules/RealmReborn/Extreme/Ex3Titan/Upheaval.cs
@@ -12,8 +12,8 @@ namespace BossMod.RealmReborn.Extreme.Ex3Titan
             if (caster != null)
             {
                 // stack just behind boss, this is a good place to bait imminent landslide correctly
-                var pos = module.PrimaryActor.Position + new WDir(0, 1);
-                hints.AddForbiddenZone(ShapeDistance.InvertedCircle(pos, 2), caster.CastInfo!.FinishAt);
+                var pos = module.PrimaryActor.Position + new WDir(0, 2);
+                hints.AddForbiddenZone(ShapeDistance.InvertedCircle(pos, 1.5f), caster.CastInfo!.FinishAt);
             }
         }
     }
