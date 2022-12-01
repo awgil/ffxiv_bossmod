7e0404f542d0278d22a31f8fbf330c8c3c766146
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2022-12-01 23:14:41 +0200

    UWU gaol order hints.

--- BossMod/Modules/Stormblood/Ultimate/UWU/P3Gaols.cs
+++ BossMod/Modules/Stormblood/Ultimate/UWU/P3Gaols.cs
@@ -1,10 +1,21 @@
-﻿namespace BossMod.Stormblood.Ultimate.UWU
+﻿using System.Linq;
+
+namespace BossMod.Stormblood.Ultimate.UWU
 {
     class P3Gaols : BossComponent
     {
         private BitMask _targets;
 
         public bool Active => _targets.Any();
+
+        public override void AddGlobalHints(BossModule module, GlobalHints hints)
+        {
+            if (Active)
+            {
+                var hint = string.Join(" > ", Service.Config.Get<UWUConfig>().P3GaolPriorities.Resolve(module.Raid).Where(i => _targets[i.slot]).OrderBy(i => i.group).Select(i => module.Raid[i.slot]?.Name));
+                hints.Add($"Gaols: {hint}");
+            }
+        }
 
         public override void OnEventCast(BossModule module, Actor caster, ActorCastEvent spell)
         {
--- BossMod/Modules/Stormblood/Ultimate/UWU/UWUConfig.cs
+++ BossMod/Modules/Stormblood/Ultimate/UWU/UWUConfig.cs
@@ -3,6 +3,10 @@
     [ConfigDisplay(Order = 0x200, Parent = typeof(StormbloodConfig))]
     public class UWUConfig : CooldownPlanningConfigNode
     {
+        [PropertyDisplay("Titan gaols priorities (close < far)")]
+        [GroupDetails(new string[] { "0", "1", "2", "3", "4", "5", "6", "7" })]
+        public GroupAssignmentUnique P3GaolPriorities = GroupAssignmentUnique.Default();
+
         public UWUConfig() : base(70) { }
     }
 }
