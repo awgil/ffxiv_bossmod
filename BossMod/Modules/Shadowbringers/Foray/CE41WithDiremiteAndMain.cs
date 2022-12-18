5cacb7fff085af0cd3fec789b72557a1244dc788
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2022-12-18 13:57:39 +0200

    Bozja CE WIP.

---
+++ BossMod/Modules/Shadowbringers/Foray/CE41WithDiremiteAndMain.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossMod.Shadowbringers.Foray.CE41WithDiremiteAndMain
+{
+    public enum OID : uint
+    {
+        Helper = 0x233C, // R0.500, x6
+        Boss = 0x31CC, // R7.200, x1
+        DimCrystal = 0x31CD, // R1.600, spawn during fight
+        CorruptedCrystal = 0x31CE, // R1.600, spawn during fight
+        SandSphere = 0x31CF, // R4.000, spawn during fight
+    };
+
+    public enum AID : uint
+    {
+        AutoAttack = 6499, // Boss->player, no cast, single-target
+
+        Shardfall = 24071, // Boss->self, 2.0s cast, single-target, visual
+        CrystallineFracture = 24072, // CorruptedCrystal/DimCrystal->self, 3.0s cast, range 4 circle aoe (cast on spawn)
+        ResonantFrequencyDim = 24073, // DimCrystal->self, 3.0s cast, range 6 circle, suicide, cast early if crystal is hit by shardstrike
+        ResonantFrequencyCorrupted = 24074, // CorruptedCrystal->self, 3.0s cast, range 6 circle, suicide, cast early if crystal is hit by shardstrike
+        ResonantFrequencyDimStinger = 24075, // DimCrystal->self, no cast, single-target, suicide, after aetherial stingers
+        ResonantFrequencyCorruptedStinger = 24076, // CorruptedCrystal->self, no cast, single-target, suicide, after crystalline stingers
+        CrystallineStingers = 24077, // Boss->self, 5.0s cast, range 60 circle, hide behind dim
+        AetherialStingers = 24078, // Boss->self, 5.0s cast, range 60 circle, hide behind corrupted
+        SandSphere = 24079, // Boss->self, 5.0s cast, single-target, visual
+        Subduction = 24080, // SandSphere->self, 4.0s cast, range 8 circle aoe with knockback 10
+        Earthbreaker = 24081, // Boss->self, 5.0s cast, single-target, visual
+        EarthbreakerAOE1 = 24082, // Helper->self, 5.0s cast, range 10 circle
+        EarthbreakerAOE2 = 24083, // Helper->self, 3.0s cast, range 10-20 donut
+        EarthbreakerAOE3 = 24084, // Helper->self, 3.0s cast, range 20-30 donut
+
+        CrystalNeedle = 24085, // Boss->player, 5.0s cast, single-target, tankbuster
+        Shardstrike = 24086, // Boss->self, 2.0s cast, single-target, visual
+        ShardstrikeAOE = 24087, // Helper->players, 5.0s cast, range 5 circle spread
+        Hailfire = 24088, // Boss->self, 8.0s cast, single-target, visual
+        HailfireAOE = 24089, // Boss->self, no cast, range 40 width 4 rect aoe
+        Teleport = 24090, // Boss->location, no cast, single-target
+    };
+
+    public enum IconID : uint
+    {
+        Shardstrike = 96, // player
+        Hailfire1 = 79, // player
+        Hailfire2 = 80, // player
+        Hailfire3 = 81, // player
+        Hailfire4 = 82, // player
+    };
+
+    class CrystallineFracture : Components.SelfTargetedAOEs
+    {
+        public CrystallineFracture() : base(ActionID.MakeSpell(AID.CrystallineFracture), new AOEShapeCircle(4)) { }
+    }
+
+    class ResonantFrequencyDim : Components.SelfTargetedAOEs
+    {
+        public ResonantFrequencyDim() : base(ActionID.MakeSpell(AID.ResonantFrequencyDim), new AOEShapeCircle(6)) { }
+    }
+
+    class ResonantFrequencyCorrupted : Components.SelfTargetedAOEs
+    {
+        public ResonantFrequencyCorrupted() : base(ActionID.MakeSpell(AID.ResonantFrequencyCorrupted), new AOEShapeCircle(6)) { }
+    }
+
+    class CrystallineStingers : Components.CastLineOfSightAOE
+    {
+        public CrystallineStingers() : base(ActionID.MakeSpell(AID.CrystallineStingers), 60, false) { }
+        public override IEnumerable<Actor> BlockerActors(BossModule module) => module.Enemies(OID.DimCrystal).Where(a => !a.IsDead);
+    }
+
+    class AetherialStingers : Components.CastLineOfSightAOE
+    {
+        public AetherialStingers() : base(ActionID.MakeSpell(AID.AetherialStingers), 60, false) { }
+        public override IEnumerable<Actor> BlockerActors(BossModule module) => module.Enemies(OID.CorruptedCrystal).Where(a => !a.IsDead);
+    }
+
+    class Subduction : Components.SelfTargetedAOEs
+    {
+        public Subduction() : base(ActionID.MakeSpell(AID.Subduction), new AOEShapeCircle(8)) { }
+    }
+
+    // next aoe starts casting slightly before previous, so use a custom component
+    class Earthbreaker : Components.GenericAOEs
+    {
+        private List<(Actor caster, AOEShape shape)> _active = new();
+
+        public override IEnumerable<(AOEShape shape, WPos origin, Angle rotation, DateTime time)> ActiveAOEs(BossModule module, int slot, Actor actor)
+        {
+            return _active.Take(1).Select(e => (e.shape, e.caster.Position, e.caster.CastInfo!.Rotation, e.caster.CastInfo.FinishAt));
+        }
+
+        public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            AOEShape? shape = (AID)spell.Action.ID switch
+            {
+                AID.EarthbreakerAOE1 => new AOEShapeCircle(10),
+                AID.EarthbreakerAOE2 => new AOEShapeDonut(10, 20),
+                AID.EarthbreakerAOE3 => new AOEShapeDonut(20, 30),
+                _ => null
+            };
+            if (shape != null)
+                _active.Add((caster, shape));
+        }
+
+        public override void OnCastFinished(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            _active.RemoveAll(c => c.caster == caster);
+        }
+    }
+
+    class CrystalNeedle : Components.SingleTargetCast
+    {
+        public CrystalNeedle() : base(ActionID.MakeSpell(AID.CrystalNeedle)) { }
+    }
+
+    // TODO: this should really be a 'spread', however our spread component works only with party...
+    class Shardstrike : BossComponent
+    {
+        private List<Actor> _dimCrystals = new();
+        private List<Actor> _corruptedCrystals = new();
+        private List<Actor> _targets = new();
+        private static float _spreadRadius = 5;
+
+        public override void Init(BossModule module)
+        {
+            _dimCrystals = module.Enemies(OID.DimCrystal);
+            _corruptedCrystals = module.Enemies(OID.CorruptedCrystal);
+        }
+
+        public override void AddHints(BossModule module, int slot, Actor actor, TextHints hints, MovementHints? movementHints)
+        {
+            if (_targets.Contains(actor))
+            {
+                if (module.WorldState.Actors.Any(a => a != actor && !a.IsDead && (a.Type == ActorType.Player || (OID)a.OID is OID.DimCrystal or OID.CorruptedCrystal) && a.Position.InCircle(actor.Position, _spreadRadius)))
+                    hints.Add("Spread!");
+            }
+            else if (_targets.Any(t => actor.Position.InCircle(t.Position, _spreadRadius)))
+            {
+                hints.Add("GTFO from spread targets!");
+            }
+        }
+
+        public override void DrawArenaForeground(BossModule module, int pcSlot, Actor pc, MiniArena arena)
+        {
+            arena.Actors(ActiveCrystals(), ArenaColor.Object, true);
+            foreach (var t in _targets)
+                arena.AddCircle(t.Position, _spreadRadius, ArenaColor.Danger);
+        }
+
+        public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if ((AID)spell.Action.ID == AID.ShardstrikeAOE && module.WorldState.Actors.Find(spell.TargetID) is var target && target != null)
+                _targets.Add(target);
+        }
+
+        public override void OnCastFinished(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if ((AID)spell.Action.ID == AID.ShardstrikeAOE)
+                _targets.RemoveAll(t => t.InstanceID == spell.TargetID);
+        }
+
+        private IEnumerable<Actor> ActiveCrystals() => _dimCrystals.Concat(_corruptedCrystals).Where(c => !c.IsDead);
+    }
+
+    // TODO: this should probably be generalized
+    class Hailfire : Components.GenericAOEs
+    {
+        private Actor?[] _targets = new Actor?[4];
+        private DateTime _activation;
+
+        private static AOEShapeRect _shape = new(40, 2);
+
+        private Actor? NextTarget => NumCasts < _targets.Length ? _targets[NumCasts] : null;
+
+        public Hailfire() : base(ActionID.MakeSpell(AID.HailfireAOE)) { }
+
+        public override IEnumerable<(AOEShape shape, WPos origin, Angle rotation, DateTime time)> ActiveAOEs(BossModule module, int slot, Actor actor)
+        {
+            if (NextTarget is var target && target != null && target != actor)
+                yield return (_shape, module.PrimaryActor.Position, Angle.FromDirection(target.Position - module.PrimaryActor.Position), _activation);
+        }
+
+        public override void DrawArenaForeground(BossModule module, int pcSlot, Actor pc, MiniArena arena)
+        {
+            if (NextTarget == pc)
+                _shape.Outline(arena, module.PrimaryActor.Position, Angle.FromDirection(pc.Position - module.PrimaryActor.Position), ArenaColor.Danger);
+        }
+
+        public override void OnEventCast(BossModule module, Actor caster, ActorCastEvent spell)
+        {
+            if (spell.Action == WatchedAction && NumCasts < _targets.Length)
+            {
+                _targets[NumCasts] = null;
+                _activation = module.WorldState.CurrentTime.AddSeconds(2.3f);
+            }
+            base.OnEventCast(module, caster, spell);
+        }
+
+        public override void OnEventIcon(BossModule module, Actor actor, uint iconID)
+        {
+            var order = (IconID)iconID switch
+            {
+                IconID.Hailfire1 => 0,
+                IconID.Hailfire2 => 1,
+                IconID.Hailfire3 => 2,
+                IconID.Hailfire4 => 3,
+                _ => -1
+            };
+            if (order >= 0)
+            {
+                NumCasts = 0;
+                _targets[order] = actor;
+                _activation = module.WorldState.CurrentTime.AddSeconds(8.2f);
+            }
+        }
+    }
+
+    class CE41WithDiremiteAndMainStates : StateMachineBuilder
+    {
+        public CE41WithDiremiteAndMainStates(BossModule module) : base(module)
+        {
+            TrivialPhase()
+                .ActivateOnEnter<CrystallineFracture>()
+                .ActivateOnEnter<ResonantFrequencyDim>()
+                .ActivateOnEnter<ResonantFrequencyCorrupted>()
+                .ActivateOnEnter<CrystallineStingers>()
+                .ActivateOnEnter<AetherialStingers>()
+                .ActivateOnEnter<Subduction>()
+                .ActivateOnEnter<Earthbreaker>()
+                .ActivateOnEnter<CrystalNeedle>()
+                .ActivateOnEnter<Shardstrike>()
+                .ActivateOnEnter<Hailfire>();
+        }
+    }
+
+    public class CE41WithDiremiteAndMain : BossModule
+    {
+        public CE41WithDiremiteAndMain(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsCircle(new(-220, 530), 25)) { }
+    }
+}
---
+++ BossMod/Modules/Shadowbringers/Foray/CE52TimeToBurn.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BossMod.Shadowbringers.Foray.CE52TimeToBurn
+{
+    public enum OID : uint
+    {
+        Boss = 0x31C8, // R9.000, x4
+        Helper = 0x233C, // R0.500, x23
+        Clock = 0x1EB17A, // R0.500, x9, EventObj type
+        TimeBomb1 = 0x1EB17B, // R0.500, EventObj type, spawn during fight
+        TimeBomb2 = 0x1EB1D4, // R0.500, EventObj type, spawn during fight
+    };
+
+    public enum AID : uint
+    {
+        AutoAttack = 6499, // Boss->player, no cast, single-target
+        TimeEruption = 23953, // Boss->self, 3.0s cast, single-target, visual
+        PauseTime = 23954, // Boss->self, 3.0s cast, single-target, visual
+        StartTime = 23955, // Boss->self, 3.0s cast, single-target, visual
+        TimeEruptionAOE = 23956, // Helper->location, no cast, range 20 width 20 rect aoe
+        TimeBomb = 23957, // Boss->self, 3.0s cast, single-target, visual
+        TimeBombAOE = 23958, // Helper->self, 1.0s cast, range 60 90-degree cone
+        Reproduce = 24809, // Boss->self, 3.0s cast, single-target, visual
+        CrimsonCyclone = 23959, // Boss->self, 10.0s cast, range 60 width 20 rect aoe
+        Eruption = 23960, // Helper->location, 3.0s cast, range 8 circle aoe
+        FireTankbuster = 23961, // Boss->player, 5.0s cast, single-target
+        FireRaidwide = 23962, // Boss->self, 5.0s cast, single-target, visual
+        FireRaidwideAOE = 23963, // Helper->self, no cast, ???
+    };
+
+    // these three main mechanics can overlap in a complex way, so we have a single component to handle them. Potential options:
+    // - eruption only (fast/slow clocks): visual cast -> 9 eobjanims -> pause cast -> start cast -> resolve
+    // - bombs only (cones): visual cast -> spawn bombs -> countdown eobjanims -> resolve
+    // - reproduce only (clone charges): visual cast -> resolve
+    // - bombs only (x3 instead of x2)
+    // - complex: eruption visual -> 9 eruption eobjanims -> pause cast -> bomb visual -> spawn bombs -> reproduce visual & bomb countdown -> cyclone cast start -> bomb resolve -> cyclone resolve -> start cast -> eruption resolve
+    // => rules: show bombs if active (activate by visual, deactivate by resolve, show for each object); otherwise show cyclone cast if active; otherwise show eruptions
+    class TimeEruptionBombReproduce : Components.GenericAOEs
+    {
+        private DateTime _bombsActivation;
+        private DateTime _eruptionStart; // timestamp of StartTime cast start
+        private List<Actor> _bombs = new();
+        private List<Actor> _cycloneCasters = new();
+        private List<(WPos pos, TimeSpan delay)> _clocks = new();
+        private List<WPos> _eruptionSafeSpots = new();
+
+        private static AOEShapeCone _shapeBomb = new(60, 45.Degrees());
+        private static AOEShapeRect _shapeCyclone = new(60, 10);
+        private static AOEShapeRect _shapeEruption = new(10, 10, 10);
+
+        public override IEnumerable<(AOEShape shape, WPos origin, Angle rotation, DateTime time)> ActiveAOEs(BossModule module, int slot, Actor actor)
+        {
+            if (_bombsActivation != new DateTime())
+            {
+                foreach (var b in _bombs)
+                    yield return (_shapeBomb, b.Position, b.Rotation, _bombsActivation);
+            }
+            else if (_cycloneCasters.Count > 0)
+            {
+                foreach (var c in _cycloneCasters)
+                    yield return (_shapeCyclone, c.Position, c.CastInfo!.Rotation, c.CastInfo.FinishAt);
+            }
+            else if (_eruptionStart != new DateTime())
+            {
+                foreach (var e in _clocks.Count > 2 ? _clocks.Take(_clocks.Count - 2) : _clocks)
+                    yield return (_shapeEruption, e.pos, new(), _eruptionStart + e.delay);
+            }
+        }
+
+        public override void Init(BossModule module)
+        {
+            _bombs = module.Enemies(OID.TimeBomb1); // either 1 or 2 works, dunno what's the difference
+        }
+
+        public override void DrawArenaBackground(BossModule module, int pcSlot, Actor pc, MiniArena arena)
+        {
+            foreach (var p in _eruptionSafeSpots)
+                _shapeEruption.Draw(arena, p, new(), ArenaColor.SafeFromAOE);
+            base.DrawArenaBackground(module, pcSlot, pc, arena);
+        }
+
+        public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            switch ((AID)spell.Action.ID)
+            {
+                case AID.TimeBomb:
+                    if (caster == module.PrimaryActor)
+                        _bombsActivation = module.WorldState.CurrentTime.AddSeconds(23.2f);
+                    break;
+                case AID.CrimsonCyclone:
+                    _cycloneCasters.Add(caster);
+                    break;
+                case AID.StartTime:
+                    _eruptionStart = module.WorldState.CurrentTime;
+                    break;
+            }
+        }
+
+        public override void OnEventCast(BossModule module, Actor caster, ActorCastEvent spell)
+        {
+            switch ((AID)spell.Action.ID)
+            {
+                case AID.TimeBombAOE:
+                    _bombsActivation = new();
+                    break;
+                case AID.CrimsonCyclone:
+                    _cycloneCasters.Remove(caster);
+                    break;
+                case AID.TimeEruptionAOE:
+                    _eruptionSafeSpots.Clear();
+                    _clocks.RemoveAll(c => c.pos.AlmostEqual(caster.Position, 1));
+                    if (_clocks.Count == 0)
+                        _eruptionStart = new();
+                    break;
+            }
+        }
+
+        public override void OnActorEAnim(BossModule module, Actor actor, uint state)
+        {
+            if ((OID)actor.OID != OID.Clock)
+                return;
+
+            var delay = state switch
+            {
+                0x00010002 => TimeSpan.FromSeconds(9.7),
+                0x00010020 => TimeSpan.FromSeconds(11.7),
+                _ => TimeSpan.Zero
+            };
+            if (delay == TimeSpan.Zero)
+                return;
+
+            _clocks.Add((actor.Position, delay));
+            if (_clocks.Count == 9)
+            {
+                _clocks.SortBy(e => e.delay);
+                _eruptionSafeSpots.AddRange(_clocks.TakeLast(2).Select(e => e.pos));
+            }
+        }
+    }
+
+    class Eruption : Components.LocationTargetedAOEs
+    {
+        public Eruption() : base(ActionID.MakeSpell(AID.Eruption), 8) { }
+    }
+
+    class FireTankbuster : Components.SingleTargetCast
+    {
+        public FireTankbuster() : base(ActionID.MakeSpell(AID.FireTankbuster)) { }
+    }
+
+    class FireRaidwide : Components.RaidwideCast
+    {
+        public FireRaidwide() : base(ActionID.MakeSpell(AID.FireRaidwide)) { }
+    }
+
+    class CE52TimeToBurnStates : StateMachineBuilder
+    {
+        public CE52TimeToBurnStates(BossModule module) : base(module)
+        {
+            TrivialPhase()
+                .ActivateOnEnter<TimeEruptionBombReproduce>()
+                .ActivateOnEnter<Eruption>()
+                .ActivateOnEnter<FireTankbuster>()
+                .ActivateOnEnter<FireRaidwide>();
+        }
+    }
+
+    public class CE52TimeToBurn : BossModule
+    {
+        public CE52TimeToBurn(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsSquare(new(-550, 0), 30)) { }
+    }
+}
