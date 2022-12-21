1b11ff562e7f4a048502f8b17033160ff40d248e
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2022-12-21 20:22:38 +0200

    Bozja CE WIP.

---
+++ BossMod/Modules/Shadowbringers/Foray/CriticalEngagement/CE63WornToShadow.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossMod.Shadowbringers.Foray.CriticalEngagement.CE63WornToShadow
+{
+    public enum OID : uint
+    {
+        Boss = 0x31D0, // R7.500, x1
+        Helper = 0x233C, // R0.500, x18
+        AlkonostsShadow = 0x31D1, // R3.750-7.500, spawn during fight
+        VorticalOrb1 = 0x3239, // R0.500, spawn during fight
+        VorticalOrb2 = 0x323A, // R0.500, spawn during fight
+        VorticalOrb3 = 0x31D2, // R0.500, spawn during fight
+        //_Gen_Actor1ea1a1 = 0x1EA1A1, // R2.000, x2, EventObj type
+        //_Gen_Actor1eb17c = 0x1EB17C, // R0.500, x1, EventObj type
+    };
+
+    public enum AID : uint
+    {
+        AutoAttack = 6499, // Boss->player, no cast, single-target
+        Stormcall = 24121, // Boss->self, 5.0s cast, single-target, visual
+        Explosion = 24122, // VorticalOrb1/VorticalOrb2/VorticalOrb3->self, 1.5s cast, range 35 circle aoe
+        BladedBeak = 24123, // Boss->player, 5.0s cast, single-target, tankbuster
+        NihilitysSong = 24124, // Boss->self, 5.0s cast, single-target, visual
+        NihilitysSongAOE = 24125, // Helper->self, no cast, ???, raidwide
+        Fantod = 24126, // Boss->self, 2.0s cast, single-target, visual
+        FantodAOE = 24127, // Helper->location, 3.0s cast, range 3 circle puddle
+        Foreshadowing = 24128, // Boss->self, 5.0s cast, single-target, visual
+        ShadowsCast = 24129, // AlkonostsShadow->Boss, 5.0s cast, single-target, visual (applies transfiguration status to caster and stores casted spell)
+        FrigidPulse = 24130, // Boss->self, 5.0s cast, range 8-25 donut
+        PainStorm = 24131, // Boss->self, 5.0s cast, range 36 130-degree cone aoe
+        PainfulGust = 24132, // Boss->self, 5.0s cast, range 20 circle aoe
+        ForeshadowingPulse = 24133, // AlkonostsShadow->self, 5.0s cast, range 8-25 donut
+        ForeshadowingStorm = 24134, // AlkonostsShadow->self, 5.0s cast, range 36 130-degree cone aoe
+        ForeshadowingGust = 24135, // AlkonostsShadow->self, 5.0s cast, range 20 circle aoe
+    };
+
+    public enum SID : uint
+    {
+        OrbMovement = 2234, // none->VorticalOrb1/VorticalOrb2/VorticalOrb3, extra=0x1E (fast)/0x49 (slow)
+        Transfiguration = 705, // AlkonostsShadow->AlkonostsShadow, extra=0x1A4
+    };
+
+    class Stormcall : Components.GenericAOEs
+    {
+        private List<(Actor source, WPos dest, DateTime activation)> _sources = new();
+        private static AOEShapeCircle _shape = new(35);
+
+        public Stormcall() : base(ActionID.MakeSpell(AID.Explosion)) { }
+
+        public override IEnumerable<AOEInstance> ActiveAOEs(BossModule module, int slot, Actor actor)
+        {
+            return _sources.Take(2).Select(e => new AOEInstance(_shape, e.dest, activation: e.activation));
+        }
+
+        public override void OnStatusGain(BossModule module, Actor actor, ActorStatus status)
+        {
+            if ((SID)status.ID == SID.OrbMovement)
+            {
+                var dest = module.Bounds.Center + 29 * (actor.Position - module.Bounds.Center).Normalized();
+                _sources.Add((actor, dest, module.WorldState.CurrentTime.AddSeconds(status.Extra == 0x1E ? 9.7 : 19.9)));
+                _sources.SortBy(e => e.activation);
+            }
+        }
+
+        public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if (spell.Action == WatchedAction && _sources.FindIndex(e => e.source == caster) is var index && index >= 0)
+                _sources[index] = (caster, caster.Position, spell.FinishAt);
+        }
+
+        public override void OnCastFinished(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if (spell.Action == WatchedAction)
+                _sources.RemoveAll(e => e.source == caster);
+        }
+    }
+
+    class BladedBeak : Components.SingleTargetCast
+    {
+        public BladedBeak() : base(ActionID.MakeSpell(AID.BladedBeak)) { }
+    }
+
+    class NihilitysSong : Components.RaidwideCast
+    {
+        public NihilitysSong() : base(ActionID.MakeSpell(AID.NihilitysSong)) { }
+    }
+
+    class Fantod : Components.LocationTargetedAOEs
+    {
+        public Fantod() : base(ActionID.MakeSpell(AID.FantodAOE), 3) { }
+    }
+
+    class FrigidPulse : Components.SelfTargetedAOEs
+    {
+        public FrigidPulse() : base(ActionID.MakeSpell(AID.FrigidPulse), new AOEShapeDonut(8, 25)) { }
+    }
+
+    class PainStorm : Components.SelfTargetedAOEs
+    {
+        public PainStorm() : base(ActionID.MakeSpell(AID.PainStorm), new AOEShapeCone(36, 65.Degrees())) { }
+    }
+
+    class PainfulGust : Components.SelfTargetedAOEs
+    {
+        public PainfulGust() : base(ActionID.MakeSpell(AID.PainfulGust), new AOEShapeCircle(20)) { }
+    }
+
+    // TODO: prediction for foreshadowing?..
+    class ForeshadowingPulse : Components.SelfTargetedAOEs
+    {
+        public ForeshadowingPulse() : base(ActionID.MakeSpell(AID.ForeshadowingPulse), new AOEShapeDonut(8, 25)) { }
+    }
+
+    class ForeshadowingStorm : Components.SelfTargetedAOEs
+    {
+        public ForeshadowingStorm() : base(ActionID.MakeSpell(AID.ForeshadowingStorm), new AOEShapeCone(36, 65.Degrees())) { }
+    }
+
+    class ForeshadowingGust : Components.SelfTargetedAOEs
+    {
+        public ForeshadowingGust() : base(ActionID.MakeSpell(AID.ForeshadowingGust), new AOEShapeCircle(20)) { }
+    }
+
+    class CE63WornToShadowStates : StateMachineBuilder
+    {
+        public CE63WornToShadowStates(BossModule module) : base(module)
+        {
+            TrivialPhase()
+                .ActivateOnEnter<Stormcall>()
+                .ActivateOnEnter<BladedBeak>()
+                .ActivateOnEnter<NihilitysSong>()
+                .ActivateOnEnter<Fantod>()
+                .ActivateOnEnter<FrigidPulse>()
+                .ActivateOnEnter<PainStorm>()
+                .ActivateOnEnter<PainfulGust>()
+                .ActivateOnEnter<ForeshadowingPulse>()
+                .ActivateOnEnter<ForeshadowingStorm>()
+                .ActivateOnEnter<ForeshadowingGust>();
+        }
+    }
+
+    public class CE63WornToShadow : BossModule
+    {
+        public CE63WornToShadow(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsCircle(new(-480, -690), 30)) { }
+    }
+}
---
+++ BossMod/Modules/Shadowbringers/Foray/CriticalEngagement/CE64FeelingTheBurn.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossMod.Shadowbringers.Foray.CriticalEngagement.CE64FeelingTheBurn
+{
+    public enum OID : uint
+    {
+        Boss = 0x31A0, // R3.640, x1
+        Escort1 = 0x31A1, // R2.800, x24
+        Escort2 = 0x32FD, // R2.800, spawn during fight
+        Helper = 0x233C, // R0.500, x26
+        //_Gen_Actor1ea1a1 = 0x1EA1A1, // R2.000, x2, EventObj type
+    };
+
+    public enum AID : uint
+    {
+        AutoAttackBoss = 24281, // Boss->player, no cast, single-target
+        AutoAttackEscort = 24319, // Escort2->player, no cast, single-target
+        ReadOrdersCoordinatedAssault = 23604, // Boss->self, 19.0s cast, single-target, visual
+        DiveFormation = 23606, // Escort1->self, 5.0s cast, range 60 width 6 rect aoe
+        AntiPersonnelMissile1 = 23609, // Boss->self, 10.0s cast, single-target, visual (3 impact pairs)
+        AntiPersonnelMissile2 = 23618, // Boss->self, 12.0s cast, single-target, visual (4 impact pairs)
+        BallisticImpact = 23610, // Helper->self, no cast, range 24 width 24 rect aoe
+        ReadOrdersShotsFired = 23611, // Boss->self, 3.0s cast, single-target, visual
+        ChainCannonEscort = 23612, // Escort2->self, 1.0s cast, range 60 width 5 rect visual
+        ChainCannonEscortAOE = 23613, // Helper->self, no cast, range 60 width 5 rect 'voidzone'
+        ChainCannonBoss = 24658, // Boss->self, 3.0s cast, range 60 width 6 rect, visual
+        ChainCannonBossAOE = 24659, // Helper->self, no cast, range 60 width 6 rect
+        SurfaceMissile = 23614, // Boss->self, 3.0s cast, single-target, visual
+        SurfaceMissileAOE = 23615, // Helper->location, 3.0s cast, range 6 circle puddle
+        SuppressiveMagitekRays = 23616, // Boss->self, 5.0s cast, single-target, visual
+        SuppressiveMagitekRaysAOE = 23617, // Helper->self, 5.5s cast, ???, raidwide
+        Analysis = 23607, // Boss->self, 3.0s cast, single-target, visual
+        PreciseStrike = 23619, // Escort1->self, 5.0s cast, range 60 width 6 rect aoe (should orient correctly to avoid vuln)
+    };
+
+    public enum SID : uint
+    {
+        FrontUnseen = 2644, // Boss->player, extra=0x120
+        BackUnseen = 1709, // Boss->player, extra=0xE8
+    };
+
+    public enum IconID : uint
+    {
+        BallisticImpact = 261, // Helper
+        ChainCannon = 164, // player
+    };
+
+    class DiveFormation : Components.SelfTargetedAOEs
+    {
+        public DiveFormation() : base(ActionID.MakeSpell(AID.DiveFormation), new AOEShapeRect(60, 3)) { }
+    }
+
+    class AntiPersonnelMissile : Components.GenericAOEs
+    {
+        private List<WPos> _positions = new();
+        private static AOEShapeRect _shape = new(12, 12, 12);
+
+        public AntiPersonnelMissile() : base(ActionID.MakeSpell(AID.BallisticImpact)) { }
+
+        public override IEnumerable<AOEInstance> ActiveAOEs(BossModule module, int slot, Actor actor)
+        {
+            return _positions.Take(2).Select(p => new AOEInstance(_shape, p));
+        }
+
+        public override void OnEventIcon(BossModule module, Actor actor, uint iconID)
+        {
+            // TODO: activation time (icon pairs are ~3s apart, but explosion pairs are ~2.6s apart; first explosion is ~2.1s after visual cast end)
+            if ((IconID)iconID == IconID.BallisticImpact)
+                _positions.Add(actor.Position);
+        }
+
+        public override void OnEventCast(BossModule module, Actor caster, ActorCastEvent spell)
+        {
+            if (spell.Action == WatchedAction && _positions.Count > 0)
+                _positions.RemoveAt(0);
+        }
+    }
+
+    // TODO: bait hints for escorts - can we even find the source?..
+    class ChainCannon : Components.GenericAOEs
+    {
+        private List<(WPos origin, Angle dir)> _casters = new();
+        private static AOEShapeRect _shape = new(60, 2.5f);
+
+        public override IEnumerable<AOEInstance> ActiveAOEs(BossModule module, int slot, Actor actor)
+        {
+            return _casters.Select(c => new AOEInstance(_shape, c.origin, c.dir));
+        }
+
+        public override void Update(BossModule module)
+        {
+            // TODO: this is extremely stupid, fix! find better end condition
+            if (((CE64FeelingTheBurn)module).Escorts.Any(e => e.IsTargetable) || // end condition for escorts
+                _casters.Count == 1 && module.PrimaryActor.CastInfo != null) // end condition for boss
+                _casters.Clear();
+        }
+
+        public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if ((AID)spell.Action.ID is AID.ChainCannonEscort or AID.ChainCannonBoss)
+                _casters.Add((caster.Position, spell.Rotation));
+        }
+    }
+
+    class SurfaceMissile : Components.LocationTargetedAOEs
+    {
+        public SurfaceMissile() : base(ActionID.MakeSpell(AID.SurfaceMissileAOE), 6) { }
+    }
+
+    class SuppressiveMagitekRays : Components.RaidwideCast
+    {
+        public SuppressiveMagitekRays() : base(ActionID.MakeSpell(AID.SuppressiveMagitekRays)) { }
+    }
+
+    // TODO: analysis/precise strike component
+    class CE64FeelingTheBurnStates : StateMachineBuilder
+    {
+        public CE64FeelingTheBurnStates(BossModule module) : base(module)
+        {
+            TrivialPhase()
+                .ActivateOnEnter<DiveFormation>()
+                .ActivateOnEnter<AntiPersonnelMissile>()
+                .ActivateOnEnter<ChainCannon>()
+                .ActivateOnEnter<SurfaceMissile>()
+                .ActivateOnEnter<SuppressiveMagitekRays>();
+        }
+    }
+
+    public class CE64FeelingTheBurn : BossModule
+    {
+        public List<Actor> Escorts;
+
+        public CE64FeelingTheBurn(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsSquare(new(-240, -230), 24))
+        {
+            Escorts = Enemies(OID.Escort2);
+        }
+
+        protected override void DrawEnemies(int pcSlot, Actor pc)
+        {
+            base.DrawEnemies(pcSlot, pc);
+            Arena.Actors(Escorts, ArenaColor.Enemy);
+        }
+    }
+}
