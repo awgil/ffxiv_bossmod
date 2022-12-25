8ee8acac4cd8edf64386fd16bac013d96ffa94ee
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2022-12-25 19:27:06 +0200

    Bozja WIP.

---
+++ BossMod/Modules/Shadowbringers/Foray/CriticalEngagement/CE14VigilForLost.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BossMod.Shadowbringers.Foray.CriticalEngagement.CE14VigilForLost
+{
+    public enum OID : uint
+    {
+        Boss = 0x2DBD, // R8.000, x1
+        Helper = 0x233C, // R0.500, x11
+        MagitekBit = 0x2F58, // R0.660, spawn during fight
+    };
+
+    public enum AID : uint
+    {
+        AutoAttack = 6499, // Boss->player, no cast, single-target
+        LightLeap = 20146, // Boss->location, 4.0s cast, range 10 circle aoe
+        AreaBombardment = 20147, // Boss->self, 5.0s cast, single-target, visual (main mechanic start)
+        ChemicalMissile = 20148, // Helper->self, 4.0s cast, range 12 circle aoe
+        TailMissile = 20149, // Boss->self, 5.0s cast, single-target, visual (big aoe)
+        TailMissileAOE = 20150, // Helper->self, 8.0s cast, range 30 circle aoe
+        Shockwave = 20151, // Boss->self, 6.0s cast, range 16 circle aoe
+        ExplosiveFlare = 20152, // Helper->self, 3.0s cast, range 10 circle aoe
+        CripplingBlow = 20153, // Boss->player, 4.0s cast, single-target, tankbuster
+        PlasmaField = 20154, // Boss->self, 4.0s cast, range 60 circle, raidwide
+        Explosion = 21266, // Helper->self, 7.0s cast, range 6 circle tower
+        MassiveExplosion = 21267, // Helper->self, no cast, range 60 circle, failed tower
+        MagitekRay = 21268, // MagitekBit->self, 2.5s cast, range 50 width 4 rect
+    };
+
+    class LightLeap : Components.LocationTargetedAOEs
+    {
+        public LightLeap() : base(ActionID.MakeSpell(AID.LightLeap), 10) { }
+    }
+
+    class ChemicalMissile : Components.SelfTargetedAOEs
+    {
+        public ChemicalMissile() : base(ActionID.MakeSpell(AID.ChemicalMissile), new AOEShapeCircle(12)) { }
+    }
+
+    class TailMissile : Components.SelfTargetedAOEs
+    {
+        public TailMissile() : base(ActionID.MakeSpell(AID.TailMissileAOE), new AOEShapeCircle(30)) { }
+    }
+
+    class Shockwave : Components.SelfTargetedAOEs
+    {
+        public Shockwave() : base(ActionID.MakeSpell(AID.Shockwave), new AOEShapeCircle(16)) { }
+    }
+
+    class ExplosiveFlare : Components.SelfTargetedAOEs
+    {
+        public ExplosiveFlare() : base(ActionID.MakeSpell(AID.ExplosiveFlare), new AOEShapeCircle(10)) { }
+    }
+
+    class CripplingBlow : Components.SingleTargetCast
+    {
+        public CripplingBlow() : base(ActionID.MakeSpell(AID.CripplingBlow)) { }
+    }
+
+    class PlasmaField : Components.RaidwideCast
+    {
+        public PlasmaField() : base(ActionID.MakeSpell(AID.PlasmaField)) { }
+    }
+
+    // TODO: generalize towers
+    class Towers : BossComponent
+    {
+        private List<Actor> _towers = new();
+        private static float _radius = 6;
+
+        public override void AddHints(BossModule module, int slot, Actor actor, TextHints hints, MovementHints? movementHints)
+        {
+            if (_towers.Count > 0 && !_towers.Any(t => t.Position.InCircle(actor.Position, _radius)))
+                hints.Add("Soak the tower!", true);
+        }
+
+        public override void DrawArenaForeground(BossModule module, int pcSlot, Actor pc, MiniArena arena)
+        {
+            foreach (var t in _towers)
+                arena.AddCircle(t.Position, _radius, ArenaColor.Safe, 2);
+        }
+
+        public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if ((AID)spell.Action.ID == AID.Explosion)
+                _towers.Add(caster);
+        }
+
+        public override void OnCastFinished(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if ((AID)spell.Action.ID == AID.Explosion)
+                _towers.Remove(caster);
+        }
+    }
+
+    class MagitekRay : Components.SelfTargetedAOEs
+    {
+        public MagitekRay() : base(ActionID.MakeSpell(AID.MagitekRay), new AOEShapeRect(50, 2)) { }
+    }
+
+    class CE14VigilForLostStates : StateMachineBuilder
+    {
+        public CE14VigilForLostStates(BossModule module) : base(module)
+        {
+            TrivialPhase()
+                .ActivateOnEnter<LightLeap>()
+                .ActivateOnEnter<ChemicalMissile>()
+                .ActivateOnEnter<TailMissile>()
+                .ActivateOnEnter<Shockwave>()
+                .ActivateOnEnter<ExplosiveFlare>()
+                .ActivateOnEnter<CripplingBlow>()
+                .ActivateOnEnter<PlasmaField>()
+                .ActivateOnEnter<Towers>()
+                .ActivateOnEnter<MagitekRay>();
+        }
+    }
+
+    public class CE14VigilForLost : BossModule
+    {
+        public CE14VigilForLost(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsCircle(new(451, 830), 30)) { }
+    }
+}
---
+++ BossMod/Modules/Shadowbringers/Foray/CriticalEngagement/CE21FinalFurlong.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+
+namespace BossMod.Shadowbringers.Foray.CriticalEngagement.CE21FinalFurlong
+{
+    public enum OID : uint
+    {
+        Boss = 0x2DBB, // R3.375, x1
+        Helper = 0x233C, // R0.500, x20
+        Monoceros = 0x2DB9, // R1.800, x1
+        LlofiiTheForthright = 0x2DBA, // R0.500, x1
+        GraspingRancor = 0x2DBC, // R1.600, spawn during fight
+    };
+
+    public enum AID : uint
+    {
+        AutoAttackBoss = 6498, // Boss->player, no cast, single-target
+        GraspingRancor = 20118, // Boss->self, 4.0s cast, single-target, visual (spawn hands)
+        SpitefulGuillotine = 20119, // GraspingRancor->player, no cast, single-target, debuffs or kills if reaches tethered player
+        SpiteWave = 20124, // Boss->self, 3.0s cast, single-target, visual (stack + puddles)
+        HatefulMiasma = 20125, // Helper->players, 5.0s cast, range 6 circle stack
+        PoisonedWords = 20126, // Helper->location, 5.0s cast, range 6 circle puddles
+        TalonedGaze = 20127, // Boss->self, 4.0s cast, single-target, visual (front/back > sides)
+        // TODO: taloned wings
+        CoffinNails = 20129, // Helper->self, 4.7s cast, range 60 90-degree cone aoe
+        Stab = 20130, // Boss->player, 4.0s cast, single-target, tankbuster
+        GripOfPoison = 20131, // Boss->self, 4.0s cast, range 60 circle, raidwide
+        StepsOfDestruction = 21018, // Boss->self, 4.0s cast, single-target, visual (puddles)
+        StepsOfDestructionAOE = 21019, // Helper->location, 4.0s cast, range 6 circle puddle
+        AutoAttackMonoceros = 871, // Monoceros->Boss, no cast, single-target
+        PurifyingLight = 20132, // Monoceros->location, 12.0s cast, range 12 circle (destroys hands)
+        FabledHope = 20134, // Monoceros->self, 8.0s cast, range 10 circle, damage up on players
+        Teleport = 20135, // Monoceros->location, no cast, single-target, teleport
+        Ruin = 20142, // LlofiiTheForthright->Boss, 2.5s cast, single-target, autoattack
+        Scupper = 21334, // LlofiiTheForthright->Boss, 2.0s cast, single-target, damage down on boss
+    };
+
+    public enum TetherID : uint
+    {
+        Movable = 1, // GraspingRancor->player
+        Frozen = 2, // GraspingRancor->player
+        Unfreezable = 17, // GraspingRancor->player (appears if hand wasn't hit by aoe)
+    };
+
+    class GraspingRancor : Components.LocationTargetedAOEs
+    {
+        private List<Actor> _hands = new();
+
+        public GraspingRancor() : base(ActionID.MakeSpell(AID.PurifyingLight), 12)
+        {
+            Color = ArenaColor.SafeFromAOE;
+            Risky = false;
+        }
+
+        public override void Init(BossModule module)
+        {
+            _hands = module.Enemies(OID.GraspingRancor);
+        }
+
+        public override void AddHints(BossModule module, int slot, Actor actor, TextHints hints, MovementHints? movementHints)
+        {
+            base.AddHints(module, slot, actor, hints, movementHints);
+            if (Casters.Count > 0)
+            {
+                var hand = _hands.Find(h => h.Tether.Target == actor.InstanceID);
+                if (hand != null)
+                {
+                    bool shouldBeFrozen = Shape.Check(hand.Position, Casters[0].CastInfo!.LocXZ);
+                    bool isFrozen = hand.Tether.ID == (uint)TetherID.Frozen;
+                    hints.Add(shouldBeFrozen ? "Face the hand!" : "Look away from hand and kite into safezone!", shouldBeFrozen != isFrozen);
+                }
+            }
+        }
+
+        public override void DrawArenaForeground(BossModule module, int pcSlot, Actor pc, MiniArena arena)
+        {
+            var hand = _hands.Find(h => h.Tether.Target == pc.InstanceID);
+            if (hand != null)
+            {
+                bool isFrozen = hand.Tether.ID == (uint)TetherID.Frozen;
+                arena.Actor(hand, ArenaColor.Object, true);
+                arena.AddLine(hand.Position, pc.Position, isFrozen ? ArenaColor.Safe : ArenaColor.Danger);
+            }
+        }
+    }
+
+    class HatefulMiasma : Components.StackWithCastTargets
+    {
+        public HatefulMiasma() : base(ActionID.MakeSpell(AID.HatefulMiasma), 6) { }
+    }
+
+    class PoisonedWords : Components.LocationTargetedAOEs
+    {
+        public PoisonedWords() : base(ActionID.MakeSpell(AID.PoisonedWords), 6) { }
+    }
+
+    class CoffinNails : Components.SelfTargetedAOEs
+    {
+        public CoffinNails() : base(ActionID.MakeSpell(AID.CoffinNails), new AOEShapeCone(60, 45.Degrees()), 2) { }
+    }
+
+    class Stab : Components.SingleTargetCast
+    {
+        public Stab() : base(ActionID.MakeSpell(AID.Stab)) { }
+    }
+
+    class GripOfPoison : Components.RaidwideCast
+    {
+        public GripOfPoison() : base(ActionID.MakeSpell(AID.GripOfPoison)) { }
+    }
+
+    class StepsOfDestruction : Components.LocationTargetedAOEs
+    {
+        public StepsOfDestruction() : base(ActionID.MakeSpell(AID.StepsOfDestructionAOE), 6) { }
+    }
+
+    class CE21FinalFurlongStates : StateMachineBuilder
+    {
+        public CE21FinalFurlongStates(BossModule module) : base(module)
+        {
+            TrivialPhase()
+                .ActivateOnEnter<GraspingRancor>()
+                .ActivateOnEnter<HatefulMiasma>()
+                .ActivateOnEnter<PoisonedWords>()
+                .ActivateOnEnter<CoffinNails>()
+                .ActivateOnEnter<Stab>()
+                .ActivateOnEnter<GripOfPoison>()
+                .ActivateOnEnter<StepsOfDestruction>();
+        }
+    }
+
+    public class CE21FinalFurlong : BossModule
+    {
+        public CE21FinalFurlong(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsCircle(new(644, 228), 27)) { }
+    }
+}
