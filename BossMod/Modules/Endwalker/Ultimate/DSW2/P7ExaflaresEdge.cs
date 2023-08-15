50368c88343483c68284464c35b8850163e8c256
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2023-08-15 01:23:36 +0100

    DSR P7 WIP

--- BossMod/Modules/Endwalker/Ultimate/DSW2/P7ExaflaresEdge.cs
+++ BossMod/Modules/Endwalker/Ultimate/DSW2/P7ExaflaresEdge.cs
@@ -1,10 +1,18 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BossMod.Endwalker.Ultimate.DSW2
 {
     class P7ExaflaresEdge : Components.Exaflare
     {
         public P7ExaflaresEdge() : base(6) { }
+
+        public override void DrawArenaForeground(BossModule module, int pcSlot, Actor pc, MiniArena arena)
+        {
+            foreach (var p in SafeSpots(module))
+                arena.AddCircle(p, 1, ArenaColor.Safe);
+        }
 
         public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
         {
@@ -27,6 +35,28 @@ namespace BossMod.Endwalker.Ultimate.DSW2
                 }
                 ++NumCasts;
             }
+        }
+
+        private IEnumerable<WPos> SafeSpots(BossModule module)
+        {
+            if (NumCasts > 0 || Lines.Count < 9)
+                yield break;
+            foreach (var l in MainLines().Where(l => !MainLines().Any(o => o != l && Clips(o, l.Next))))
+                yield return l.Next;
+        }
+
+        private IEnumerable<Line> MainLines()
+        {
+            for (int i = 0; i < Lines.Count; i += 3)
+                yield return Lines[i];
+        }
+
+        private bool Clips(Line l, WPos p)
+        {
+            var off = p - l.Next;
+            var px = l.Advance.Dot(off);
+            var pz = l.Advance.OrthoL().Dot(off);
+            return Math.Abs(px) < 42 || Math.Abs(pz) < 42 && px > 0; // 42 == 6*7 (radius * advance)
         }
     }
 }
