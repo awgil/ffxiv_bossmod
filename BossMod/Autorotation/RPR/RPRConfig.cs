c87190f30dce168c9cb382593620e3e72b393dd8
lazylemo <lazylemo@hotmail.com>
2023-12-22 19:07:36 +0100

    Pre-pull tings and more changes / AOE fully implemented

--- BossMod/Autorotation/RPR/RPRConfig.cs
+++ BossMod/Autorotation/RPR/RPRConfig.cs
@@ -3,7 +3,10 @@
     [ConfigDisplay(Parent = typeof(AutorotationConfig))]
     class RPRConfig : ConfigNode
     {
-        [PropertyDisplay("Execute optimal rotations on Slice (ST) or Spinning Scythe (AOE) XD")]
+        [PropertyDisplay("Execute optimal rotations on Slice (ST) or Spinning Scythe (AOE)")]
         public bool FullRotation = true;
+
+        [PropertyDisplay("Forbid Harpe too early in prepull")]
+        public bool ForbidEarlyHarpe = true;
     }
 }
