15aac6b7926a27640ea590ae8b48387516e11236
CarnifexOptimus <Bastianfahr@pm.me>
2024-01-10 23:12:44 +0100

    split Leviathan hard module into multiple files to make it easier to read

---
+++ BossMod/Modules/RealmReborn/Trial/T09WhorleaterH/T09WhorleaterHBodyslam.cs
@@ -0,0 +1,93 @@
+using BossMod.Components;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BossMod.Modules.RealmReborn.Trial.T09WhorleaterH;
+    class BodySlamKB : Knockback
+    {
+        public float Distance;
+        public Angle Direction;
+
+        public float LeviathanZ;
+
+        public override IEnumerable<Source> Sources(BossModule module, int slot, Actor actor)
+        {
+            if (Distance > 0)
+                yield return new(new(), Distance, default, null, Direction, Kind.DirForward);
+        }
+
+        public override void Update(BossModule module)
+        {
+            base.Update(module);
+            var boss = module.Enemies(OID.Boss).FirstOrDefault();
+            if (boss != null)
+            {
+                if (LeviathanZ == default)
+                    LeviathanZ = module.Enemies(OID.Boss).First().Position.Z;
+                if (boss.Position.Z != LeviathanZ && boss.Position.Z != 0)
+                {
+                    LeviathanZ = boss.Position.Z;
+                    Distance = 25;
+                    Direction = boss.Position.Z <= 0 ? 180.Degrees() : 0.Degrees();
+                }
+            }
+        }
+
+        public override void OnEventCast(BossModule module, Actor caster, ActorCastEvent spell)
+        {
+            base.OnEventCast(module, caster, spell);
+            if ((AID)spell.Action.ID is AID.BodySlamNorth or AID.BodySlamSouth)
+            {
+                Distance = 0;
+            }
+        }
+    }
+        class BodySlamAOE : GenericAOEs
+        {
+        public float active;
+        public Angle Direction;
+        public float LeviathanZ;
+        private static AOEShapeRect rect = new(30, 5);
+
+        public override IEnumerable<AOEInstance> ActiveAOEs(BossModule module, int slot, Actor actor)
+        {
+            if (active > 0)
+                 yield return new(rect, module.PrimaryActor.Position, Direction, new());
+        }
+
+        public override void Update(BossModule module)
+        {
+            base.Update(module);
+            var boss = module.Enemies(OID.Boss).FirstOrDefault();
+            if (boss != null)
+            {
+                if (LeviathanZ == default)
+                    LeviathanZ = module.Enemies(OID.Boss).First().Position.Z;
+                if (boss.Position.Z != LeviathanZ && boss.Position.Z != 0)
+                {
+                    LeviathanZ = boss.Position.Z;
+                    if ((boss.Position.Z+boss.Position.X)<=-1f && (boss.Position.Z+boss.Position.X)>=-2f)// Leviathan head slams SW
+                    Direction = boss.Position.Z <= 0 ? 0.Degrees() : 90.Degrees(); 
+                    active = 1;
+                    if ((boss.Position.Z+boss.Position.X)<=28f && (boss.Position.Z+boss.Position.X)>=27f)// Leviathan head slams SE
+                    Direction = boss.Position.Z <= 0 ? 0.Degrees() : 270.Degrees(); 
+                    active = 1;
+                    if ((boss.Position.Z+boss.Position.X)<=-27f && (boss.Position.Z+boss.Position.X)>=-28f)// Leviathan head slams NW
+                    Direction = boss.Position.Z <= 0 ? 90.Degrees() : 0.Degrees(); 
+                    active = 1;
+                    if ((boss.Position.Z+boss.Position.X)<=2f && (boss.Position.Z+boss.Position.X)>=1f)// Leviathan head slams NE
+                    Direction = boss.Position.Z <= 0 ? 270.Degrees() : 0.Degrees(); 
+                    active = 1;
+                }
+            }
+        }
+        
+        public override void OnEventCast(BossModule module, Actor caster, ActorCastEvent spell)
+        {
+            base.OnEventCast(module, caster, spell);
+            if ((AID)spell.Action.ID == AID.BodySlamRectAOE)
+            {
+                active = 0;
+            }
+        }
+    }
---
+++ BossMod/Modules/RealmReborn/Trial/T09WhorleaterH/T09WhorleaterHSpinningDive.cs
@@ -0,0 +1,56 @@
+using BossMod.Components;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BossMod.Modules.RealmReborn.Trial.T09WhorleaterH;
+   class SpinningDive : GenericAOEs //TODO: Find out how to detect spinning dives earlier eg. the water column telegraph
+    {     
+        private float activeHelper;       
+        private static AOEShapeRect rect = new(46, 8);
+        public override IEnumerable<AOEInstance> ActiveAOEs(BossModule module, int slot, Actor actor)
+        {
+                var helper = module.Enemies(OID.SpinningDiveHelper).FirstOrDefault();      
+                 if (helper != null && activeHelper >=1)
+            yield return new(rect, helper.Position, helper.Rotation, new());
+            }
+        public override void OnActorCreated(BossModule module, Actor actor)
+        {
+            if ((OID)actor.OID == OID.SpinningDiveHelper)
+             activeHelper =1;
+        }
+        public override void OnEventCast(BossModule module, Actor caster, ActorCastEvent spell)
+        {
+            base.OnEventCast(module, caster, spell);
+            if ((AID)spell.Action.ID == AID.SpinningDiveSnapshot)
+            {
+                activeHelper = 0;
+            }
+        }
+    }
+
+        class SpinningDiveKB : Knockback //TODO: Find out how to detect spinning dives earlier eg. the water column telegraph
+    {        
+        private float activeHelper;       
+         public SpinningDiveKB() : base(ActionID.MakeSpell(AID.SpinningDiveEffect)) { }
+        private static AOEShapeRect rect = new(46, 8);
+        public override IEnumerable<Source> Sources(BossModule module, int slot, Actor actor)
+        {
+                var helper = module.Enemies(OID.SpinningDiveHelper).FirstOrDefault();  
+                if (helper != null && activeHelper >=1)
+                yield return new(helper.Position, 10, default, rect, helper.Rotation, Kind.AwayFromOrigin);
+        }
+
+        public override void OnActorCreated(BossModule module, Actor actor)
+        {
+            if ((OID)actor.OID == OID.SpinningDiveHelper)
+             activeHelper = 1;
+        }
+        public override void OnEventCast(BossModule module, Actor caster, ActorCastEvent spell)
+        {
+            base.OnEventCast(module, caster, spell);
+            if ((AID)spell.Action.ID == AID.SpinningDiveEffect)
+            {
+                activeHelper = 0;
+            }
+        }
+    }
