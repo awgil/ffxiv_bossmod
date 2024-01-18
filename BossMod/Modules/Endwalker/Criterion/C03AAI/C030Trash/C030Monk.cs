b7e3458922a723257ba0bae2980cc182f955326f
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-01-18 00:06:08 +0000

    AAI N WIP

---
+++ BossMod/Modules/Endwalker/Criterion/C03AAI/C030Trash/C030Monk.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace BossMod.Endwalker.Criterion.C03AAI.C030Trash1
+{
+    class Hydroshot : Components.Knockback
+    {
+        private Actor? _caster;
+
+        public override IEnumerable<Source> Sources(BossModule module, int slot, Actor actor)
+        {
+            if (_caster?.TargetID == actor.InstanceID)
+                yield return new(_caster.Position, 10, _caster.CastInfo?.FinishAt ?? default);
+        }
+
+        public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if ((AID)spell.Action.ID is AID.NHydroshot or AID.SHydroshot)
+                _caster = caster;
+        }
+
+        public override void OnCastFinished(BossModule module, Actor caster, ActorCastInfo spell)
+        {
+            if (_caster == caster)
+                _caster = null;
+        }
+    }
+
+    class C030MonkStates : StateMachineBuilder
+    {
+        private bool _savage;
+
+        public C030MonkStates(BossModule module, bool savage) : base(module)
+        {
+            _savage = savage;
+            DeathPhase(0, SinglePhase)
+                .ActivateOnEnter<Hydroshot>()
+                .ActivateOnEnter<NTwister>(!_savage)
+                .ActivateOnEnter<STwister>(_savage);
+        }
+
+        private void SinglePhase(uint id)
+        {
+            Hydroshot(id, 9.1f);
+            CrossAttack(id + 0x10000, 3.4f);
+            SimpleState(id + 0xFF0000, 10, "???");
+        }
+
+        private void Hydroshot(uint id, float delay)
+        {
+            Cast(id, _savage ? AID.SHydroshot : AID.NHydroshot, delay, 5, "Single-target 1");
+            Cast(id + 0x10, _savage ? AID.SHydroshot : AID.NHydroshot, 1.5f, 5, "Single-target 2");
+            Cast(id + 0x20, _savage ? AID.SHydroshot : AID.NHydroshot, 1.5f, 5, "Single-target 3");
+        }
+
+        private void CrossAttack(uint id, float delay)
+        {
+            Cast(id, _savage ? AID.SCrossAttack : AID.NCrossAttack, delay, 5, "Tankbuster")
+                .SetHint(StateMachine.StateHint.Tankbuster);
+        }
+    }
+    class C030NMonkStates : C030MonkStates { public C030NMonkStates(BossModule module) : base(module, false) { } }
+    class C030SMonkStates : C030MonkStates { public C030SMonkStates(BossModule module) : base(module, true) { } }
+
+    [ModuleInfo(PrimaryActorOID = (uint)OID.NMonk)]
+    public class C030NMonk : C030Trash1
+    {
+        public C030NMonk(WorldState ws, Actor primary) : base(ws, primary) { }
+    }
+
+    [ModuleInfo(PrimaryActorOID = (uint)OID.SMonk)]
+    public class C030SMonk : C030Trash1
+    {
+        public C030SMonk(WorldState ws, Actor primary) : base(ws, primary) { }
+    }
+}
---
+++ BossMod/Modules/Endwalker/Criterion/C03AAI/C031Ketuduke/C031Ketuduke.cs
@@ -0,0 +1,36 @@
+﻿namespace BossMod.Endwalker.Criterion.C03AAI.C031Ketuduke
+{
+    class TidalRoar : Components.CastCounter
+    {
+        public TidalRoar(AID aid) : base(ActionID.MakeSpell(aid)) { }
+    }
+    class NTidalRoar : TidalRoar { public NTidalRoar() : base(AID.NTidalRoarAOE) { } }
+    class STidalRoar : TidalRoar { public STidalRoar() : base(AID.STidalRoarAOE) { } }
+
+    class BubbleNet : Components.CastCounter
+    {
+        public BubbleNet(AID aid) : base(ActionID.MakeSpell(aid)) { }
+    }
+    class NBubbleNet1 : BubbleNet { public NBubbleNet1() : base(AID.NBubbleNet1AOE) { } }
+    class SBubbleNet1 : BubbleNet { public SBubbleNet1() : base(AID.SBubbleNet1AOE) { } }
+    class NBubbleNet2 : BubbleNet { public NBubbleNet2() : base(AID.NBubbleNet2AOE) { } }
+    class SBubbleNet2 : BubbleNet { public SBubbleNet2() : base(AID.SBubbleNet2AOE) { } }
+
+    class Hydrobomb : Components.LocationTargetedAOEs
+    {
+        public Hydrobomb(AID aid) : base(ActionID.MakeSpell(aid), 5) { }
+    }
+    class NHydrobomb : Hydrobomb { public NHydrobomb() : base(AID.NHydrobombAOE) { } }
+    class SHydrobomb : Hydrobomb { public SHydrobomb() : base(AID.SHydrobombAOE) { } }
+
+    public abstract class C031Ketuduke : BossModule
+    {
+        public C031Ketuduke(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsSquare(new(0, 0), 20)) { }
+    }
+
+    [ModuleInfo(PrimaryActorOID = (uint)OID.NBoss)]
+    public class C031NKetuduke : C031Ketuduke { public C031NKetuduke(WorldState ws, Actor primary) : base(ws, primary) { } }
+
+    [ModuleInfo(PrimaryActorOID = (uint)OID.SBoss)]
+    public class C031SKetuduke : C031Ketuduke { public C031SKetuduke(WorldState ws, Actor primary) : base(ws, primary) { } }
+}
