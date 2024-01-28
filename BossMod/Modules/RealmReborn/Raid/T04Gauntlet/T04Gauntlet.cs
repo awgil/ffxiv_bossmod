7fa3178e66770a3bf1728116836f6a4cf8f8757b
CarnifexOptimus <Bastianfahr@pm.me>
2024-01-28 11:17:44 +0100

    removed comment

--- BossMod/Modules/RealmReborn/Raid/T04Gauntlet/T04Gauntlet.cs
+++ BossMod/Modules/RealmReborn/Raid/T04Gauntlet/T04Gauntlet.cs
@@ -78,7 +78,6 @@ namespace BossMod.RealmReborn.Raid.T04Gauntlet
         }
     }
 
-    // helper actor is destroyed immediately on combat end (be it a wipe or a kill), and is not recreated immediately after wipe (only after interacting with terminal), making it ideal for wipe checks
     [ModuleInfo(PrimaryActorOID = (uint)OID.TerminalStart, CFCID = 96)]
     public class T04Gauntlet : BossModule
     {
