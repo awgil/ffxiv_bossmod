4e6748d6969c7ec060c3ad035729dc7f6d8fc1ea
CarnifexOptimus <Bastianfahr@pm.me>
2024-02-03 09:08:18 +0100

    progress with angles?

--- BossMod/Modules/Shadowbringers/Foray/CriticalEngagement/CE12BayingOfHounds.cs
+++ BossMod/Modules/Shadowbringers/Foray/CriticalEngagement/CE12BayingOfHounds.cs
@@ -37,7 +37,7 @@ namespace BossMod.Shadowbringers.Foray.CriticalEngagement.CE12BayingOfHounds
     };
     class TheDragonsBreathA : Components.SelfTargetedAOEs
     {
-        public TheDragonsBreathA() : base(ActionID.MakeSpell(AID.TheDragonsBreathA), new AOEShapeCone(60, 36.Degrees(), -10.Degrees())) { }
+        public TheDragonsBreathA() : base(ActionID.MakeSpell(AID.TheDragonsBreathA), new AOEShapeCone(60, 35.Degrees(), -10.Degrees())) { }
     }
     class TheDragonsBreathB : Components.SelfTargetedAOEs
     {
@@ -103,7 +103,7 @@ namespace BossMod.Shadowbringers.Foray.CriticalEngagement.CE12BayingOfHounds
 
     class LionsBreath : Components.SelfTargetedAOEs
     {
-        public LionsBreath() : base(ActionID.MakeSpell(AID.LionsBreathAOE), new AOEShapeCone(60, 37.5f.Degrees())) { } // TODO: verify angle
+        public LionsBreath() : base(ActionID.MakeSpell(AID.LionsBreathAOE), new AOEShapeCone(60, 39.Degrees())) { } // TODO: verify angle
     }
 
     class VoidTornado : Components.CastHint
