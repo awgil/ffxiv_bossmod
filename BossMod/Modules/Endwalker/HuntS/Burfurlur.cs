7a5df44f3a13ef9ed957b36544e992030bf2efc7
CarnifexOptimus <Bastianfahr@pm.me>
2024-02-13 09:27:01 +0100

    burfurlur update

--- BossMod/Modules/Endwalker/HuntS/Burfurlur.cs
+++ BossMod/Modules/Endwalker/HuntS/Burfurlur.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BossMod.Endwalker.HuntS.Burfurlur
 {
@@ -11,41 +12,45 @@ namespace BossMod.Endwalker.HuntS.Burfurlur
     {
         AutoAttack = 872, // Boss->player, no cast, single-target
         Trolling = 27316, // Boss->self, no cast, range 10 circle
-        QuintupleInhale1 = 27307, // Boss->self, 4.0s cast, range 27 ?-degree cone
-        QuintupleInhale24 = 27308, // Boss->self, 0.5s cast, range 27 ?-degree cone
-        QuintupleSneeze1 = 27309, // Boss->self, 5.0s cast, range 40 ?-degree cone
-        QuintupleSneeze24 = 27310, // Boss->self, 0.5s cast, range 40 ?-degree cone
-        QuintupleInhale35 = 27692, // Boss->self, 0.5s cast, range 27 ?-degree cone
-        QuintupleSneeze35 = 27693, // Boss->self, 0.5s cast, range 40 ?-degree cone
+        QuintupleInhale1 = 27307, // Boss->self, 4.0s cast, range 27 45-degree cone
+        QuintupleInhale24 = 27308, // Boss->self, 0.5s cast, range 27 45-degree cone
+        QuintupleSneeze1 = 27309, // Boss->self, 5.0s cast, range 40 45-degree cone
+        QuintupleSneeze24 = 27310, // Boss->self, 0.5s cast, range 40 45-degree cone
+        QuintupleInhale35 = 27692, // Boss->self, 0.5s cast, range 27 45-degree cone
+        QuintupleSneeze35 = 27693, // Boss->self, 0.5s cast, range 40 45-degree cone
         Uppercut = 27314, // Boss->self, 3.0s cast, range 15 120-degree cone
         RottenSpores = 27313, // Boss->location, 3.0s cast, range 6 circle
     };
 
-    class QuintupleSneeze : Components.GenericAOEs
+    class QuintupleSneeze : Components.GenericRotatingAOE
     {
         private Angle _referenceAngle;
         private List<Angle> _pendingOffsets = new();
+        private static AOEShapeCone _shape = new(40, 45.Degrees());
         private bool _sneezing;
-
-        private static AOEShapeCone _shape = new(40, 45.Degrees()); // TODO: verify angle
-
         public override IEnumerable<AOEInstance> ActiveAOEs(BossModule module, int slot, Actor actor)
         {
             if (_sneezing)
             {
-                // TODO: activation
                 if (_pendingOffsets.Count > 0)
-                    yield return new(_shape, module.PrimaryActor.Position, _referenceAngle + _pendingOffsets[0], color: ArenaColor.Danger);
+                foreach (var s in Sequences)
+                {
+                    int num = Math.Min(s.NumRemainingCasts, s.MaxShownAOEs);
+                    var time = s.NextActivation > module.WorldState.CurrentTime ? s.NextActivation : module.WorldState.CurrentTime;
+                    for (int i = 1; i < num; ++i)
+                    {
+                        time = time.AddSeconds(s.SecondsBetweenActivations);
+                        yield return new(s.Shape, s.Origin, _referenceAngle + _pendingOffsets[0], time, ImminentColor);
+                    }
+                }
                 if (_pendingOffsets.Count > 1)
-                    yield return new(_shape, module.PrimaryActor.Position, _referenceAngle + _pendingOffsets[1]);
+                foreach (var s in Sequences)
+                    if (s.NumRemainingCasts > 0)
+                        yield return new(s.Shape, s.Origin, _referenceAngle + _pendingOffsets[1], s.NextActivation, FutureColor);
             }
         }
-
         public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
         {
-            if (caster != module.PrimaryActor)
-                return;
-
             switch ((AID)spell.Action.ID)
             {
                 case AID.QuintupleInhale1:
@@ -60,6 +65,7 @@ namespace BossMod.Endwalker.HuntS.Burfurlur
                     break;
                 case AID.QuintupleSneeze1:
                     _sneezing = true;
+                    Sequences.Add(new(_shape, caster.Position, _referenceAngle, _pendingOffsets[1], spell.FinishAt, 2.2f, 6));
                     _referenceAngle = spell.Rotation;
                     break;
             }
@@ -69,6 +75,7 @@ namespace BossMod.Endwalker.HuntS.Burfurlur
         {
             if (caster == module.PrimaryActor && (AID)spell.Action.ID is AID.QuintupleSneeze1 or AID.QuintupleSneeze24 or AID.QuintupleSneeze35)
             {
+                AdvanceSequence(0, module.WorldState.CurrentTime);
                 _pendingOffsets.RemoveAt(0);
                 _sneezing = _pendingOffsets.Count > 0;
             }
