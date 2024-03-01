9734e44435eba7fcf8644a6ae66a9b7a7905b54a
CarnifexOptimus <Bastianfahr@pm.me>
2024-03-01 13:36:15 +0100

    improvements

--- BossMod/Components/CastHint.cs
+++ BossMod/Components/CastHint.cs
@@ -5,7 +5,15 @@ namespace BossMod.Components
 {
     public enum AID : uint
     {
-        SpittingSardine = 11423, //blue mage skill, this enum table is temporary until bluemages get their own autorotation enums
+        SpittingSardine = 11423, //blue mage skill
+        BombToss = 11396, //blue mage skill
+        Faze = 11403, //blue mage skill
+        TheRamsVoice = 11419, //blue mage skill
+        StickyTongue = 11412, //blue mage skill
+        PerpetualRay = 18314, //blue mage skill
+        Tatamigaeshi = 23266, //blue mage skill
+        WhiteDeath = 23268, //blue mage skill
+        TranquilizerL = 12884, //logos action in Eureka
     };
 
     // generic component that is 'active' when any actor casts specific spell
@@ -87,17 +95,30 @@ namespace BossMod.Components
             {
                 if (Active && Canbeinterrupted && actor.Role == Role.Tank && Service.ClientState.LocalPlayer?.Level >= 18)
                     hints.PlannedActions.Add((ActionID.MakeSpell(WAR.AID.Interject), caster, 1, false));
+                if (Active && Canbeinterrupted && actor.Class.GetClassCategory() == ClassCategory.PhysRanged && Service.ClientState.LocalPlayer?.Level >= 24)
+                    hints.PlannedActions.Add((ActionID.MakeSpell(BRD.AID.HeadGraze), caster, 1, false));
                 if (Active && Canbestunned && actor.Class is Class.GLA or Class.PLD && Service.ClientState.LocalPlayer?.Level >= 10)
                     hints.PlannedActions.Add((ActionID.MakeSpell(PLD.AID.ShieldBash), caster, 1, false));
                 if (Active && Canbestunned && actor.Role == Role.Tank && Service.ClientState.LocalPlayer?.Level >= 12)
+                    hints.PlannedActions.Add((ActionID.MakeSpell(WAR.AID.LowBlow), caster, 1, false));
+                if (Active && Canbestunned && actor.Class.GetClassCategory() == ClassCategory.Melee && Service.ClientState.LocalPlayer?.Level >= 10)
                     hints.PlannedActions.Add((ActionID.MakeSpell(WAR.AID.LowBlow), caster, 1, false));
                 if (Active && Canbestunned && actor.Class == Class.WHM && Service.ClientState.LocalPlayer?.Level >= 45 && Service.ClientState.LocalPlayer?.Level <= 82)
                     hints.PlannedActions.Add((ActionID.MakeSpell(WHM.AID.Holy1), caster, 1, false));
                 if (Active && Canbestunned && actor.Class == Class.WHM && Service.ClientState.LocalPlayer?.Level >= 82)
                     hints.PlannedActions.Add((ActionID.MakeSpell(WHM.AID.Holy3), caster, 1, false));
-                if (Active && Canbestunned && actor.Class == Class.BLU)
+                if (Active && Canbeinterrupted && actor.Class == Class.BLU)
                     hints.PlannedActions.Add((ActionID.MakeSpell(AID.SpittingSardine), caster, 1, false));
-                //TODO: add various missing actions
+                if (Active && Canbestunned && actor.Class == Class.BLU)
+                {
+                    hints.PlannedActions.Add((ActionID.MakeSpell(AID.BombToss), caster, 1, false));
+                    hints.PlannedActions.Add((ActionID.MakeSpell(AID.Faze), caster, 1, false));
+                    hints.PlannedActions.Add((ActionID.MakeSpell(AID.StickyTongue), caster, 1, false));
+                    hints.PlannedActions.Add((ActionID.MakeSpell(AID.PerpetualRay), caster, 1, false));
+                    hints.PlannedActions.Add((ActionID.MakeSpell(AID.Tatamigaeshi), caster, 1, false));
+                    hints.PlannedActions.Add((ActionID.MakeSpell(AID.TheRamsVoice), caster, 1, false)); //actually a deep freeze, but similar effect
+                    hints.PlannedActions.Add((ActionID.MakeSpell(AID.WhiteDeath), caster, 1, false)); //actually a deep freeze, but similar effect
+                }
             }
         }
     }
