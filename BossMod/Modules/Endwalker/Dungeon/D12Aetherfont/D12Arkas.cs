9598e0a12753f7f36cc79c0c2b79bd0a2043dcb4
mcvaxius <135515620+McVaxius@users.noreply.github.com>
2024-03-08 18:09:49 -0500

    Aetherfont tested and ready
    Aetherfont tested and ready
    even the non standard arena works perfectly.

---
+++ BossMod/Modules/Endwalker/Dungeon/D12Aetherfont/D12Arkas.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BossMod.PlanTarget;
+using Lumina.Data.Parsing.Layer;
+
+/*
+everything we need to understand the mechanics is in here
+https://www.thegamer.com/final-fantasy-14-the-aetherfont-dungeon-guide-walkthrough/
+https://ffxiv.consolegameswiki.com/wiki/The_Aetherfont
+https://gamerescape.com/2023/05/25/ffxiv-endwalker-guide-the-aetherfont/
+*/
+
+namespace BossMod.Endwalker.Dungeon.D12Aetherfont.D12Arkas
+{
+    /*
+    notes to self bnpcname has nameID, contentfindercondition has the CFC
+    iconid is a row in lockon sheet
+    tetherid is a row in channeling or w/e sheet
+    */
+    public enum OID : uint
+    {
+        Boss = 0x3EEA, // ???
+        Helper = 0x233C , // ???
+    };
+
+    public enum AID : uint
+    {
+        AutoAttackAttack = 870,      //done.(ignored) Boss->none, no cast, single-target
+        BattleCry1 = 33364,          //done. Boss->self, 5.0s cast, range 40 circle
+        BattleCry2 = 34605,          //done. Boss->self, 5.0s cast, range 40 circle
+        ElectricEruption = 33615,    //done. Boss->self, 5.0s cast, range 40 circle
+        Electrify = 33367,           //done. Helper->location, 5.5s cast, range 10 circle
+        LightningClaw1 = 33366,      //done. Boss->location, no cast, range 6 circle
+        LightningClaw2 = 34712,      //done. Boss->none, 5.0s cast, single-target 
+        LightningLeap1 = 33358,      //done. Boss->location, 4.0s cast, single-target         -> 
+        LightningLeap2 = 33359,      //done. Boss->location, 5.0s cast, single-target         -> 
+        LightningLeap3 = 33360,      //done. Helper->location, 6.0s cast, range 10 circle
+        LightningLeap4 = 34713,      //done. Helper->location, 5.0s cast, range 10 circle
+        LightningRampage1 = 34318,   //done. Boss->location, 4.0s cast, single-target         -> 
+        LightningRampage2 = 34319,   //done. Boss->location, 2.0s cast, single-target         -> 
+        LightningRampage3 = 34320,   //done. Boss->location, 2.0s cast, single-target         -> 
+        LightningRampage4 = 34321,   //done. Helper->location, 5.0s cast, range 10 circle
+        LightningRampage5 = 34714,   //done. Helper->location, 5.0s cast, range 10 circle
+        RipperClaw = 33368,          //done. Boss->none, 5.0s cast, single-target
+        Shock = 33365,               //done. Helper->location, 3.5s cast, range 6 circle
+        SpinningClaw = 33362,        //done. Boss->self, 3.5s cast, range 10 circle
+        ForkedFissures = 33361,      //done. Helper->location, 1.0s cast, width 4 rect charge
+        SpunLightning = 33363,       //done. Helper->self, 3.5s cast, range 30 width 8 rect
+    };
+
+    public enum IconID : uint
+    {
+        Icon_218 = 218, // 33CE
+        Icon_161 = 161, // 3DC2
+    };
+    class LightningRampage1 : Components.LocationTargetedAOEs
+    {
+        public LightningRampage1() : base(ActionID.MakeSpell(AID.LightningRampage1),5) { }
+    }
+    class LightningRampage2 : Components.LocationTargetedAOEs
+    {
+        public LightningRampage2() : base(ActionID.MakeSpell(AID.LightningRampage2), 5) { }
+    }
+    class LightningRampage3 : Components.LocationTargetedAOEs
+    {
+        public LightningRampage3() : base(ActionID.MakeSpell(AID.LightningRampage3), 5) { }
+    }
+    class LightningLeap1 : Components.LocationTargetedAOEs
+    {
+        public LightningLeap1() : base(ActionID.MakeSpell(AID.LightningLeap1), 5) { }
+    }
+    class LightningLeap2 : Components.LocationTargetedAOEs
+    {
+        public LightningLeap2() : base(ActionID.MakeSpell(AID.LightningLeap2), 5) { }
+    }
+    class LightningClaw2 : Components.LocationTargetedAOEs
+    {
+        public LightningClaw2() : base(ActionID.MakeSpell(AID.LightningClaw2), 5) { }
+    }
+    class SpunLightning : Components.SelfTargetedAOEs
+    {
+        public SpunLightning() : base(ActionID.MakeSpell(AID.SpunLightning), new AOEShapeRect(4,2,-50)) { }
+    }   
+    class LightningClaw1 : Components.LocationTargetedAOEs
+    {
+        public LightningClaw1() : base(ActionID.MakeSpell(AID.LightningClaw1), 6) { }
+    }
+    class ForkedFissures : Components.ChargeAOEs
+    {
+        public ForkedFissures() : base(ActionID.MakeSpell(AID.ForkedFissures), 2) { }
+    }
+    /*class ForkedFissures : Components.GenericAOEs
+     {
+         private List<AOEInstance> _aoes = new();
+         private List<Actor?>? ForkHelper;
+         private static readonly AOEShapeRect rect = new(0.5f, 0.25f, 40);
+         public override IEnumerable<AOEInstance> ActiveAOEs(BossModule module, int slot, Actor actor) => _aoes;
+         public override void OnCastStarted(BossModule module, Actor caster, ActorCastInfo spell)
+         {
+             //ForkHelper = [.. module.Enemies(OID.Helper)];
+             //ForkHelper = module.Enemies(OID.Helper).ToList();
+             if ((AID)spell.Action.ID == AID.ForkedFissures)
+             {
+                 foreach (var helper in module.Enemies(OID.Helper))
+                 {
+                     if (helper != null)
+                     {
+                         _aoes.Add(new(rect, caster.Position, helper.Rotation, spell.NPCFinishAt));
+                         _aoes.SortBy(aoe => aoe.Activation);
+                     }
+                 }
+             }
+         }
+         public override void OnCastFinished(BossModule module, Actor caster, ActorCastInfo spell)
+         {
+             if ((AID)spell.Action.ID is AID.ForkedFissures)
+                 _aoes.RemoveAll(aoe => aoe.Origin.AlmostEqual(caster.Position, 1));
+         }
+     }*/
+    /*class ForkedFissures : Components.GenericAOEs
+    {
+        private Actor? _caster;
+        private DateTime _predictedActivation;
+
+        private static readonly AOEShapeRect rect = new(0.5f, 0.25f, 40);
+
+        public override IEnumerable<AOEInstance> ActiveAOEs(BossModule module, int slot, Actor actor)
+        {
+            if (_caster?.CastInfo != null)
+                yield return new(rect, _caster.Position, _caster.CastInfo.Rotation, _caster.CastInfo.NPCFinishAt);
+        }
+    }*/
+    class ElectricEruption : Components.RaidwideCast
+    {
+        public ElectricEruption() : base(ActionID.MakeSpell(AID.ElectricEruption)) { }
+    }
+    class Electrify : Components.LocationTargetedAOEs
+    {
+        public Electrify() : base(ActionID.MakeSpell(AID.Electrify), 10) { }
+    }
+    class LightningLeap3 : Components.LocationTargetedAOEs
+    {
+        public LightningLeap3() : base(ActionID.MakeSpell(AID.LightningLeap3),10) { }
+    }
+    class LightningLeap4 : Components.LocationTargetedAOEs
+    {
+        public LightningLeap4() : base(ActionID.MakeSpell(AID.LightningLeap4), 10) { }
+    }
+    class LightningRampage5 : Components.LocationTargetedAOEs
+    {
+        public LightningRampage5() : base(ActionID.MakeSpell(AID.LightningRampage5), 10) { }
+    }
+    class LightningRampage4 : Components.LocationTargetedAOEs
+    {
+        public LightningRampage4() : base(ActionID.MakeSpell(AID.LightningRampage4), 10) { }
+    }
+    class RipperClaw : Components.SingleTargetCast
+    {
+        public RipperClaw() : base(ActionID.MakeSpell(AID.RipperClaw)) { }
+    }
+    class Shock : Components.LocationTargetedAOEs
+    {
+        public Shock() : base(ActionID.MakeSpell(AID.Shock), 6) { }
+    }
+    class SpinningClaw : Components.SelfTargetedAOEs
+    {
+        public SpinningClaw() : base(ActionID.MakeSpell(AID.SpinningClaw), new AOEShapeCircle(10)) { }
+    }
+    class BattleCry1 : Components.RaidwideCast
+    {
+        public BattleCry1() : base(ActionID.MakeSpell(AID.BattleCry1)) { }
+    }
+    class BattleCry2 : Components.RaidwideCast
+    {
+        public BattleCry2() : base(ActionID.MakeSpell(AID.BattleCry1)) { }
+    }
+    class D12ArkasStates : StateMachineBuilder
+    {
+        public D12ArkasStates(BossModule module) : base(module)
+        {
+            TrivialPhase()
+            .ActivateOnEnter<LightningClaw1>()
+            .ActivateOnEnter<LightningClaw2>()
+            .ActivateOnEnter<SpunLightning>()
+            .ActivateOnEnter<ForkedFissures>()
+            .ActivateOnEnter<ElectricEruption>()
+            .ActivateOnEnter<Electrify>()
+            .ActivateOnEnter<LightningLeap1>()
+            .ActivateOnEnter<LightningLeap2>()
+            .ActivateOnEnter<LightningLeap3>()
+            .ActivateOnEnter<LightningLeap4>()
+            .ActivateOnEnter<LightningRampage1>()
+            .ActivateOnEnter<LightningRampage2>()
+            .ActivateOnEnter<LightningRampage3>()
+            .ActivateOnEnter<LightningRampage4>()
+            .ActivateOnEnter<LightningRampage5>()
+            .ActivateOnEnter<RipperClaw>()
+            .ActivateOnEnter<Shock>()
+            .ActivateOnEnter<SpinningClaw>()
+            .ActivateOnEnter<BattleCry1>()
+            .ActivateOnEnter<BattleCry2>();
+        }
+    }
+
+    /*    notes to self bnpcname has nameID, contentfindercondition has the CFC
+    */
+    [ModuleInfo(CFCID = 822, NameID = 12337)]
+    public class D12Arkas : BossModule
+    {
+        public D12Arkas(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsCircle(new(425, -440), 12)) { }
+
+        public override void CalculateAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+        {
+            base.CalculateAIHints(slot, actor, assignment, hints);
+        }
+    }
+}
---
+++ BossMod/Modules/Endwalker/Dungeon/D12Aetherfont/D12Octomammoth.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lumina.Data.Parsing.Layer;
+using static BossMod.ActorCastEvent;
+
+/*
+everything we need to understand the mechanics is in here
+https://www.thegamer.com/final-fantasy-14-the-aetherfont-dungeon-guide-walkthrough/
+https://ffxiv.consolegameswiki.com/wiki/The_Aetherfont
+https://gamerescape.com/2023/05/25/ffxiv-endwalker-guide-the-aetherfont/
+*/
+
+namespace BossMod.Endwalker.Dungeon.D12Aetherfont.D12Octomammoth
+{
+    /*
+    notes to self bnpcname has nameID, contentfindercondition has the CFC
+    iconid is a row in lockon sheet
+    tetherid is a row in channeling or w/e sheet
+    */
+    public enum OID : uint
+    {
+        Boss = 0x3EAA, // R26.000, x1
+        MammothTentacle = 0x3EAB, // R6.000, x8, 523 type
+        Actor3eac = 0x3EAC, // R0.500, x8
+        Helper = 0x233C , // ???
+    };
+
+    public enum AID : uint
+    {
+        BossSelfUKN = 33350,      //done.(ignored) Boss->self, no cast, single-target
+        Breathstroke = 34551,     //done. Boss->self, 16.5s cast, range 35 180-degree cone
+        Clearout = 33348,         //done. MammothTentacle->self, 9.0s cast, range 16 120-degree cone
+        Octostroke = 33347,       //done.(GUESSING SIZE) Boss->self, 16.0s cast, single-target
+        SalineSpit1 = 33352,      //done. Boss->self, 3.0s cast, single-target
+        SalineSpit2 = 33353,      //done. Helper->self, 6.0s cast, range 8 circle
+        Telekinesis1 = 33349,     //done.(ignored. the helper does the telegraph) Boss->self, 5.0s cast, single-target
+        Telekinesis2 = 33351,     //done. Helper->self, 10.0s cast, range 12 circle
+        TidalBreath = 33354,      //done. Boss->self, 10.0s cast, range 35 180-degree cone
+        TidalRoar = 33356,        //done. Boss->self, 5.0s cast, range 60 circle
+        VividEyes = 33355,        //done. Boss->self, 4.0s cast, range ?-26 donut
+        WaterDrop = 34436,        //done. Helper->player, 5.0s cast, range 6 circle
+        Wallop = 33346,           //done. MammothTentacle->self, 3.0s cast, range 22 width 8 rect //idk if i did this one right
+    };
+    public enum IconID : uint
+    {
+        Icon_300 = 300, // Helper
+        Icon_139 = 139, // player/33CE/33CF/3DC2
+    };
+    public enum TetherID : uint
+    {
+        Tether_167 = 167, // Actor3eac->Boss -> Telekinesis
+    };
+
+    class Wallop : Components.SelfTargetedAOEs
+    {
+        public Wallop() : base(ActionID.MakeSpell(AID.Wallop), new AOEShapeRect(11,5.5f,16)) { }
+    }
+    class SalineSpit1 : Components.SpreadFromCastTargets
+    {
+        public SalineSpit1() : base(ActionID.MakeSpell(AID.SalineSpit1), 3) { }
+    }  
+    class VividEyes : Components.SelfTargetedAOEs
+    {
+        public VividEyes() : base(ActionID.MakeSpell(AID.VividEyes), new AOEShapeDonut(20, 26)) { }
+    }
+    class Clearout : Components.SelfTargetedAOEs
+    {
+        public Clearout() : base(ActionID.MakeSpell(AID.Clearout), new AOEShapeCone(16, 60.Degrees())) { }
+    }   
+    class TidalBreath : Components.SelfTargetedAOEs
+    {
+        public TidalBreath() : base(ActionID.MakeSpell(AID.TidalBreath), new AOEShapeCone(35, 90.Degrees())) { }
+    }
+    class Breathstroke : Components.SelfTargetedAOEs
+    {
+        public Breathstroke() : base(ActionID.MakeSpell(AID.Breathstroke), new AOEShapeCone(35, 90.Degrees())) { }
+    }
+    class Octostroke : Components.SelfTargetedAOEs
+    {
+        public Octostroke() : base(ActionID.MakeSpell(AID.Octostroke), new AOEShapeCircle(3)) { }
+    }    
+    class TidalRoar : Components.RaidwideCast
+    {
+        public TidalRoar() : base(ActionID.MakeSpell(AID.TidalRoar)) { }
+    }
+    class WaterDrop : Components.SelfTargetedAOEs
+    {
+        public WaterDrop() : base(ActionID.MakeSpell(AID.WaterDrop), new AOEShapeCircle(6)) { }
+    }
+    class SalineSpit2 : Components.SelfTargetedAOEs
+    {
+        public SalineSpit2() : base(ActionID.MakeSpell(AID.SalineSpit2), new AOEShapeCircle(8)) { }
+    } 
+    class Telekinesis2 : Components.SelfTargetedAOEs
+    {
+        public Telekinesis2() : base(ActionID.MakeSpell(AID.Telekinesis2), new AOEShapeCircle(12)) { }
+    }
+    class D12OctomammothStates : StateMachineBuilder
+    {
+        public D12OctomammothStates(BossModule module) : base(module)
+        {
+            TrivialPhase()
+            .ActivateOnEnter<D12OctomammothAI>()
+            .ActivateOnEnter<Wallop>()
+            .ActivateOnEnter<Clearout>()
+            .ActivateOnEnter<VividEyes>()
+            .ActivateOnEnter<Octostroke>()
+            .ActivateOnEnter<WaterDrop>()
+            .ActivateOnEnter<TidalRoar>()
+            .ActivateOnEnter<TidalBreath>()
+            .ActivateOnEnter<Telekinesis2>()
+            .ActivateOnEnter<Breathstroke>()
+            .ActivateOnEnter<SalineSpit1>()
+            .ActivateOnEnter<SalineSpit2>();
+        }
+    }
+
+    /*    notes to self bnpcname has nameID, contentfindercondition has the CFC
+    */
+    /*
+            public const float OctoCircleRadius = 8;
+            public const float OctoRectL = 4;
+            public const float OctoRectHW = 2;
+            public WPos OctoWposCircleA = new(-345, -368);
+            public WPos OctoWposCircleB = new(-387.678f, -350.322f);
+            public WPos OctoWposCircleC = new(-352.322f, -350.322f);
+            public WPos OctoWposCircleD = new(-370, -343);
+            public WPos OctoWposCircleE = new(-395, -368);
+            public WPos OctoWposRectA = new(-395.000f, -368.000f);  //invalid i havent figured it out yet
+            public WPos OctoWposRectB = new(-395.000f, -368.000f);  //invalid i havent figured it out yet
+            public WPos OctoWposRectC = new(-395.000f, -368.000f);  //invalid i havent figured it out yet
+            public WPos OctoWposRectD = new(-395.000f, -368.000f);  //invalid i havent figured it out yet
+            public int OctoRectAngleA = 10; //invert for other side. just guessing for now
+            public int OctoRectAngleB = 15; //invert for other side. just guessing for now
+    */
+    //    hints.AddForbiddenZone(ShapeDistance.InvertedCircle(OctoWposCircleD, OctoCircleRadius), DateTime.MaxValue);
+
+     class D12OctomammothAI : BossComponent
+    {
+        public override void AddAIHints(BossModule module, int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+        {
+            // Define circles
+            var circles = new WPos[]
+            {
+                new WPos(-345, -368),               // Circle A
+                new WPos(-387.678f, -350.322f),     // Circle B
+                new WPos(-352.322f, -350.322f),     // Circle C
+                new WPos(-370,  -343),              // Circle D
+                new WPos(-395, -368)                // Circle E
+            };
+
+            // Define smaller circles because i failed to get rectangles working
+            var smallCircles = new WPos[]
+            {
+             /*   new WPos(-348, -358),               // Small Circle A
+                new WPos(-360, -346),               // Small Circle B
+                new WPos(-379, -346),               // Small Circle C
+                new WPos(-392, -360)                // Small Circle D*/
+            new WPos(-347.4326171875f, -360.63653564453f),
+            new WPos(-347.67233276367f, -359.98715209961f),
+            new WPos(-347.85485839844f, -359.49258422852f),
+            new WPos(-348.21545410156f, -358.51571655273f),
+            new WPos(-348.66912841797f, -357.2868347168f),
+            new WPos(-359.22134399414f, -346.90921020508f),
+            new WPos(-359.98919677734f, -346.52166748047f),
+            new WPos(-361.35482788086f, -345.83236694336f),
+            new WPos(-362.4801940918f, -345.26443481445f),
+            new WPos(-377.45910644531f, -344.94708251953f),
+            new WPos(-378.73901367188f, -345.44708251953f),
+            new WPos(-379.94046020508f, -345.91647338867f),
+            new WPos(-381.08489990234f, -346.36361694336f),
+            new WPos(-391.44171142578f, -357.25454711914f),
+            new WPos(-391.79779052734f, -357.98648071289f),
+            new WPos(-392.10589599609f, -358.6198425293f),
+            new WPos(-392.50811767578f, -359.44671630859f),
+            new WPos(-392.99645996094f, -360.45059204102f)
+            };
+
+            // Define circle radius
+            var circleRadius = 8f;
+            var smallerCircleRadius = 2f;
+
+            // Define the center and radius of the giant circle
+            var giantCircleCenter = new WPos(-370, -368); // Adjust as needed
+            var giantCircleRadius = 56f;          // Adjust as needed
+
+            // Calculate the distance from a point to the nearest circle's edge
+            Func<WPos, float> distanceToNearestCircleEdge = p =>
+            {
+                var minDistance = float.MaxValue;
+                foreach (var circle in circles)
+                {
+                    var dx = p.X - circle.X;
+                    var dz = p.Z - circle.Z;
+                    var distance = MathF.Sqrt(dx * dx + dz * dz) - circleRadius;
+                    minDistance = Math.Min(minDistance, distance);
+                }
+                foreach (var circle in smallCircles)
+                {
+                    var dx = p.X - circle.X;
+                    var dz = p.Z - circle.Z;
+                    var distance = MathF.Sqrt(dx * dx + dz * dz) - smallerCircleRadius;
+                    minDistance = Math.Min(minDistance, distance);
+                }
+                return minDistance;
+            };
+
+            // Calculate the distance from a point to the edge of the giant circle
+            Func<WPos, float> distanceToGiantCircleEdge = p =>
+            {
+                var dx = p.X - giantCircleCenter.X;
+                var dz = p.Z - giantCircleCenter.Z;
+                return giantCircleRadius - MathF.Sqrt(dx * dx + dz * dz);
+            };
+
+            // Define the forbidden zone as the area outside all the circles within the giant circle
+            Func<WPos, float> forbiddenZone = p =>
+            {
+                var nearestCircleDistance = distanceToNearestCircleEdge(p);
+                var giantCircleDistance = distanceToGiantCircleEdge(p);
+                return -Math.Min(nearestCircleDistance, giantCircleDistance); // Invert the distance
+            };
+
+            // Add forbidden zone using the calculated function with current time
+            hints.AddForbiddenZone(forbiddenZone);
+        }
+    }
+
+    [ModuleInfo(CFCID = 822, NameID = 12334)]
+
+    class D12Octomammoth : BossModule
+    {
+        public D12Octomammoth(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsCircle(new(-370, -368), 36)) { }
+
+        public override void CalculateAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+        {
+            base.CalculateAIHints(slot, actor, assignment, hints);
+        }
+    
+    }
+    
+
+}
+
+
+/*
+ Arena Bounds Circles  X,Z size -> 8
+-345.000f,-368.000f
+-387.678f,-350.322f
+-352.322f,-350.322f
+-370.000f,-343.000f
+-395.000f,-368.000f
+ */
