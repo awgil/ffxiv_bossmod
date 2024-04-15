f9cfc3b16bb46032a2681828dc718aa3b0e77f34
Olga Kichakova <okichakova@gmail.com>
2024-04-15 01:41:17 +0100

    Roslyn is fucking lit.

--- BossMod/Modules/Endwalker/Savage/P12S2PallasAthena/P12S2PallasAthena.cs
+++ BossMod/Modules/Endwalker/Savage/P12S2PallasAthena/P12S2PallasAthena.cs
@@ -3,6 +3,6 @@
 [ModuleInfo(BossModuleInfo.Maturity.Verified, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 943, NameID = 12382, SortOrder = 2)]
 public class P12S2PallasAthena(WorldState ws, Actor primary) : BossModule(ws, primary, DefaultBounds)
 {
-    public static ArenaBoundsRect DefaultBounds = new ArenaBoundsRect(new(100, 95), 20, 15);
-    public static ArenaBoundsCircle SmallBounds = new ArenaBoundsCircle(new(100, 90), 7);
+    public static readonly ArenaBoundsRect DefaultBounds = new ArenaBoundsRect(new(100, 95), 20, 15);
+    public static readonly ArenaBoundsCircle SmallBounds = new ArenaBoundsCircle(new(100, 90), 7);
 }
