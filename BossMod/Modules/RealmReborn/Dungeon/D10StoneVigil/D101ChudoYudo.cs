5cc85fcff1229d18222da95057ecee35c76eef9b
CarnifexOptimus <156172553+CarnifexOptimus@users.noreply.github.com>
2024-05-16 20:19:06 +0200

    stone vigil arena rework complete

--- BossMod/Modules/RealmReborn/Dungeon/D10StoneVigil/D101ChudoYudo.cs
+++ BossMod/Modules/RealmReborn/Dungeon/D10StoneVigil/D101ChudoYudo.cs
@@ -43,9 +43,9 @@ public class D101ChudoYudo(WorldState ws, Actor primary) : BossModule(ws, primar
     private static readonly List<Shape> union = [new Square(new(0, 116), 20)];
     private static readonly List<Shape> difference = [new Square(new(-20, 136), 3, 45.Degrees()), new Square(new(20, 136), 3, 45.Degrees()), new Square(new(-20, 96), 3, 45.Degrees()),
     new Square(new(20, 96), 3, 45.Degrees()), new Rectangle(new(-4.1f, 99), 0.5f, 4, 180.Degrees()), new Circle(new(-4.5f, 96), 0.9f), new Rectangle(new(4.1f, 99), 0.5f, 4, 180.Degrees()),
-    new Circle(new(4.5f, 96), 0.9f), new Circle(new(7.6f, 96), 0.5f), new Circle(new(16.3f, 96.2f), 0.5f), new Circle(new(19.9f, 99.8f), 0.5f), new Circle(new(20.1f, 108), 0.5f),
-    new Circle(new(20.1f, 116), 0.5f), new Circle(new(20.1f, 124), 0.5f), new Circle(new(20, 132.2f), 0.5f), new Circle(new(16.2f, 136), 0.5f), new Circle(new(7.6f, 136.2f), 0.5f),
-    new Circle(new(-8f, 136.2f), 0.5f), new Circle(new(-16.2f, 136), 0.5f), new Circle(new(-20, 132.2f), 0.5f), new Circle(new(-20.1f, 124), 0.5f), new Circle(new(-20.1f, 116), 0.5f),
-    new Circle(new(-20.1f, 108), 0.5f), new Circle(new(-16.3f, 96.2f), 0.5f), new Circle(new(-19.9f, 99.8f), 0.5f), new Circle(new(-7.6f, 96), 0.5f)];
+    new Circle(new(4.5f, 96), 0.9f), new Square(new(7.7f, 95.9f), 0.5f), new Square(new(16.3f, 96.2f), 0.45f, 45.Degrees()), new Square(new(20, 99.8f), 0.5f, 45.Degrees()), new Square(new(20.1f, 108), 0.5f),
+    new Square(new(20.1f, 116), 0.5f), new Square(new(20.1f, 124), 0.5f), new Square(new(20, 132.2f), 0.5f, 45.Degrees()), new Square(new(16.2f, 136), 0.5f, 45.Degrees()), new Square(new(7.6f, 136.2f), 0.5f),
+    new Square(new(-8, 136.2f), 0.5f), new Square(new(-16.2f, 136), 0.5f, 45.Degrees()), new Square(new(-20, 132.2f), 0.5f, 45.Degrees()), new Square(new(-20.1f, 124), 0.5f), new Square(new(-20.1f, 116), 0.5f),
+    new Square(new(-20.1f, 108), 0.5f), new Square(new(-16.3f, 96.2f), 0.45f, 45.Degrees()), new Square(new(-20, 99.8f), 0.5f, 45.Degrees()), new Square(new(-7.7f, 95.9f), 0.5f)];
     public static readonly ArenaBounds arena = new ArenaBoundsComplex(union, difference);
 }
