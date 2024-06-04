0da7d23ef8a6045a1822c0652c76ba9f4877689e
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-06-04 13:56:19 -0400

    Lunar Subterrane Boss 2 AI improvement

--- BossMod/Modules/Endwalker/Dungeon/D13LunarSubterrane/D132DamcyanAntilon.cs
+++ BossMod/Modules/Endwalker/Dungeon/D13LunarSubterrane/D132DamcyanAntilon.cs
@@ -72,6 +72,15 @@ class Landslip(BossModule module) : Components.Knockback(module)
         else
             return false;
     }
+
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        foreach (var c in _casters)
+            // forbid the half of each lane that would result in the player being pushed into the death wall
+            // TODO make this work with towerfall too...not a huge priority because duty support healer can keep
+            // player alive in that case
+            hints.AddForbiddenZone(new AOEShapeRect(10, 5, 10), c.Position + c.Rotation.ToDirection() * 30);
+    }
 }
 
 class EarthenGeyser(BossModule module) : Components.StackWithCastTargets(module, ActionID.MakeSpell(AID.EarthenGeyser2), 10);
