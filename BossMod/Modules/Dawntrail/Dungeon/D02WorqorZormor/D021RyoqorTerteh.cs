e760c8f18f479fa3c063a650ae9773cd9326b437
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-07-10 18:54:20 -0400

    names cleanup

--- BossMod/Modules/Dawntrail/Dungeon/D02WorqorZormor/D021RyoqorTerteh.cs
+++ BossMod/Modules/Dawntrail/Dungeon/D02WorqorZormor/D021RyoqorTerteh.cs
@@ -2,41 +2,26 @@
 
 public enum OID : uint
 {
-    _Gen_Actor1e8f2f = 0x1E8F2F, // R0.500, x1, EventObj type
-    _Gen_Actor1e8fb8 = 0x1E8FB8, // R2.000, x2, EventObj type
-    _Gen_QorrlohTeh = 0x43A2, // R0.500, x4
     Boss = 0x4159, // R5.280, x1
     Helper = 0x233C,
-    _Gen_RyoqorTerteh = 0x233C, // R0.500, x4, 523 type
-    _Gen_RorrlohTeh = 0x415B, // R1.500, x0 (spawn during fight)
-    _Gen_QorrlohTeh1 = 0x415A, // R3.000, x0 (spawn during fight)
-    _Gen_Snowball = 0x415C, // R2.500, x0 (spawn during fight)
 }
 
 public enum AID : uint
 {
-    _AutoAttack_Attack = 872, // Boss->player, no cast, single-target
-    _Spell_FrostingFracas = 36280, // 233C->self, 5.0s cast, range 60 circle
-    _Spell_FrostingFracas2 = 36279, // Boss->self, 5.0s cast, single-target
-    _Spell_FluffleUp = 36265, // Boss->self, 4.0s cast, single-target
-    _Spell_ColdFeat = 36266, // Boss->self, 4.0s cast, single-target
-    _Spell_IceScream = 36270, // 415B->self, 12.0s cast, range 20 width 20 rect
-    _Spell_FrozenSwirl = 36272, // 43A2->self, 12.0s cast, range 15 circle
-    _Spell_FrozenSwirl2 = 36271, // 415A->self, 12.0s cast, single-target
-    _Spell_Snowscoop = 36275, // Boss->self, 4.0s cast, single-target
-    _Spell_SnowBoulder = 36278, // 415C->self, 4.0s cast, range 50 width 6 rect
-    _Spell_SparklingSprinkling = 36713, // Boss->self, 5.0s cast, single-target
-    _Spell_SparklingSprinkling2 = 36281, // 233C->player, 5.0s cast, range 5 circle
+    FrostingFracas = 36280, // 233C->self, 5.0s cast, range 60 circle
+    IceScream = 36270, // 415B->self, 12.0s cast, range 20 width 20 rect
+    FrozenSwirl = 36272, // 43A2->self, 12.0s cast, range 15 circle
+    SnowBoulder = 36278, // 415C->self, 4.0s cast, range 50 width 6 rect
+    SparklingSprinkling = 36281, // 233C->player, 5.0s cast, range 5 circle
 }
 
 public enum SID : uint
 {
-    _Gen_1 = 2056, // none->_Gen_RorrlohTeh, extra=0x2C4
-    _Gen_2 = 3944, // none->_Gen_RorrlohTeh/_Gen_QorrlohTeh1, extra=0x0
-    _Gen_3 = 3445, // none->_Gen_RorrlohTeh/_Gen_QorrlohTeh1/_Gen_QorrlohTeh, extra=0xFFF6
+    // frozen adds always get both 3944 and 3445, no idea what the difference is. i just picked my favorite
+    Frozen = 3944, // none->_Gen_RorrlohTeh/_Gen_QorrlohTeh1, extra=0x0
 }
 
-class FrostingFracas(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID._Spell_FrostingFracas));
+class FrostingFracas(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID.FrostingFracas));
 
 abstract class FreezableAOEs(BossModule module, ActionID action, AOEShape shape) : Components.GenericAOEs(module)
 {
@@ -79,21 +64,18 @@ abstract class FreezableAOEs(BossModule module, ActionID action, AOEShape shape)
 
     public override void OnStatusGain(Actor actor, ActorStatus status)
     {
-        if ((SID)status.ID is SID._Gen_1 or SID._Gen_2 or SID._Gen_3)
+        if ((SID)status.ID is SID.Frozen && _casters.ContainsKey(actor))
         {
-            if (_casters.ContainsKey(actor))
-            {
-                _casters[actor] = true;
-                _numFrozen += 1;
-            }
+            _casters[actor] = true;
+            _numFrozen += 1;
         }
     }
 }
 
-class IceScream(BossModule module) : FreezableAOEs(module, ActionID.MakeSpell(AID._Spell_IceScream), new AOEShapeRect(20, 10));
-class FrozenSwirl(BossModule module) : FreezableAOEs(module, ActionID.MakeSpell(AID._Spell_FrozenSwirl), new AOEShapeCircle(15));
-class SparklingSprinkling(BossModule module) : Components.SpreadFromCastTargets(module, ActionID.MakeSpell(AID._Spell_SparklingSprinkling2), 5);
-class SnowBoulder(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Spell_SnowBoulder), new AOEShapeRect(50, 3), maxCasts: 6);
+class IceScream(BossModule module) : FreezableAOEs(module, ActionID.MakeSpell(AID.IceScream), new AOEShapeRect(20, 10));
+class FrozenSwirl(BossModule module) : FreezableAOEs(module, ActionID.MakeSpell(AID.FrozenSwirl), new AOEShapeCircle(15));
+class SparklingSprinkling(BossModule module) : Components.SpreadFromCastTargets(module, ActionID.MakeSpell(AID.SparklingSprinkling), 5);
+class SnowBoulder(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.SnowBoulder), new AOEShapeRect(50, 3), maxCasts: 6);
 
 class D021RyoqorTertehStates : StateMachineBuilder
 {
