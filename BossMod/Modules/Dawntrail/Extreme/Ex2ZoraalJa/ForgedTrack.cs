8630cf0e197eafb1066a44ba777edc0d1a700d65
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-07-10 23:46:23 +0100

    Formatting fix.

--- BossMod/Modules/Dawntrail/Extreme/Ex2ZoraalJa/ForgedTrack.cs
+++ BossMod/Modules/Dawntrail/Extreme/Ex2ZoraalJa/ForgedTrack.cs
@@ -40,7 +40,7 @@ class ForgedTrack(BossModule module) : Components.GenericAOEs(module)
             if (rightIsWide == laneRight)
             {
                 // wide
-                WideAOEs.Add(new(_shapeWide, Module.Center + rightDir *  adjustedLaneOffset, spell.Rotation, spell.NPCFinishAt.AddSeconds(1.4f)));
+                WideAOEs.Add(new(_shapeWide, Module.Center + rightDir * adjustedLaneOffset, spell.Rotation, spell.NPCFinishAt.AddSeconds(1.4f)));
             }
             else
             {
