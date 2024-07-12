4b3df8ddef0528f6e4797376051604a61cde7f63
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-07-12 17:14:44 -0400

    reset here

--- BossMod/BossModule/AIHints.cs
+++ BossMod/BossModule/AIHints.cs
@@ -68,7 +68,6 @@ public sealed class AIHints
     // clear all stored data
     public void Clear()
     {
-        ForceMovementIn = float.MaxValue;
         Center = default;
         Bounds = DefaultBounds;
         PotentialTargets.Clear();
