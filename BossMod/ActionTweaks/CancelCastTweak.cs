ac8d2efb36c2f55ce258de89309b6d4852b2f21a
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-07-26 00:38:32 +0100

    Do not cancel raise spells on dead targets lol - instead, cancel them on raised ones.

--- BossMod/ActionTweaks/CancelCastTweak.cs
+++ BossMod/ActionTweaks/CancelCastTweak.cs
@@ -13,11 +13,28 @@ public sealed class CancelCastTweak(WorldState ws)
         if (currentTime < _nextCancelAllowed)
             return false;
 
-        var cancel = force || _config.CancelCastOnDeadTarget && (_ws.Actors.Find(_ws.Party.Player()?.CastInfo?.TargetID ?? 0)?.IsDead ?? false);
-        if (!cancel)
+        if (!force && !WantCancel())
             return false;
 
         _nextCancelAllowed = currentTime.AddSeconds(0.2f);
         return true;
+    }
+
+    private bool WantCancel()
+    {
+        if (!_config.CancelCastOnDeadTarget)
+            return false;
+
+        var cast = _ws.Party.Player()?.CastInfo;
+        var target = _ws.Actors.Find(cast?.TargetID ?? 0);
+        if (target == null)
+            return false;
+
+        var isRaise = Service.LuminaRow<Lumina.Excel.GeneratedSheets.Action>(cast?.Action.SpellId() ?? 0)?.Unknown24 == 1;
+        if (!isRaise)
+            return target.IsDead;
+
+        // for raise spells, we want to cancel them if target becomes alive or gains 'raise' status
+        return !target.IsDead || target.Statuses.Any(s => s.ID is 148 or 1140);
     }
 }
