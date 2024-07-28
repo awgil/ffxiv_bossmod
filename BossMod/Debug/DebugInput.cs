b3be09769de9991af207f2ccf2f1211ce95ea87e
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-07-28 08:11:39 +0100

    Unbroken what was broken.

--- BossMod/Debug/DebugInput.cs
+++ BossMod/Debug/DebugInput.cs
@@ -74,7 +74,7 @@ unsafe sealed class DebugInput : IDisposable
     private delegate ref int GetRefValueDelegate(int vkCode);
     private readonly GetRefValueDelegate _getKeyRef;
 
-    private readonly PlayerController* _playerController;
+    //private readonly PlayerController* _playerController;
 
     //private delegate void RMIWalkDelegate(PlayerMoveControllerWalk* self, float* sumLeft, float* sumForward, float* sumTurnLeft, byte* haveBackwardOrStrafe, byte* a6, byte bAdditiveUnk);
     //private readonly Hook<RMIWalkDelegate> _rmiWalkHook;
@@ -113,8 +113,9 @@ unsafe sealed class DebugInput : IDisposable
         //_amex = autorot.ActionManager;
         //_navi = new(_amex);
 
-        _playerController = (PlayerController*)Service.SigScanner.GetStaticAddressFromSig("48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 3C 01 75 1E 48 8D 0D");
-        Service.Log($"[DebugInput] playerController addess: 0x{(nint)_playerController:X}");
+        //_playerController = (PlayerController*)Service.SigScanner.GetStaticAddressFromSig("48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 3C 01 75 1E 48 8D 0D");
+        //Service.Log($"[DebugInput] playerController addess: 0x{(nint)_playerController:X}");
+        Service.Log("[DebugInput] fix me");
 
         //_rmiWalkHook = Service.Hook.HookFromSignature<RMIWalkDelegate>("E8 ?? ?? ?? ?? 80 7B 3E 00 48 8D 3D", RMIWalkDetour);
         //Service.Log($"[DebugInput] rmiwalk addess: 0x{_rmiWalkHook.Address:X}");
