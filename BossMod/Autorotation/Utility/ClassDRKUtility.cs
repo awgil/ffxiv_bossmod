3016241ca56996d5616d218f243e369fbf248ed3
ace <aceakechi6@gmail.com>
2024-08-19 13:15:10 -0700

    Cleanup

--- BossMod/Autorotation/Utility/ClassDRKUtility.cs
+++ BossMod/Autorotation/Utility/ClassDRKUtility.cs
@@ -15,20 +15,20 @@ public sealed class ClassDRKUtility(RotationModuleManager manager, Actor player)
         var res = new RotationModuleDefinition("Utility: DRK", "Planner support for utility actions", "Akechi", RotationModuleQuality.Ok, BitMask.Build((int)Class.DRK), 100);
         DefineShared(res, IDLimitBreak3, IDStanceApply, IDStanceRemove);
 
-        DefineSimpleConfig(res, Track.DarkMind, "DarkMind", "DMind", 450, DRK.AID.DarkMind, 10);
+        DefineSimpleConfig(res, Track.DarkMind, "DarkMind", "DMind", 450, DRK.AID.DarkMind, 10); //120s CD, 15s duration
 
-        res.Define(Track.ShadowWall).As<WallOption>("ShadowWall", "Wall", 550)
+        res.Define(Track.ShadowWall).As<WallOption>("ShadowWall", "Wall", 550) //120s CD, 15s duration
             .AddOption(WallOption.None, "None", "Do not use automatically")
             .AddOption(WallOption.ShadowWall, "Use", "Use ShadowWall", 120, 15, ActionTargets.Self, 38, 91)
             .AddOption(WallOption.ShadowedVigil, "UseEx", "Use ShadowedVigil", 120, 15, ActionTargets.Self, 92)
             .AddAssociatedActions(DRK.AID.ShadowWall, DRK.AID.ShadowedVigil);
 
-        DefineSimpleConfig(res, Track.LivingDead, "LivingDead", "LD", 400, DRK.AID.LivingDead, 10);
-        DefineSimpleConfig(res, Track.TheBlackestNight, "The Blackest Night", "TBN", 400, DRK.AID.TheBlackestNight, 7);
-        DefineSimpleConfig(res, Track.Oblation, "Oblation", "Obl", 320, DRK.AID.Oblation); // note: secondary effect (hot) duration 6
-        DefineSimpleConfig(res, Track.DarkMissionary, "DarkMissionary", "Mission", 220, DRK.AID.DarkMissionary, 30); // note: secondary effect duration 15
+        DefineSimpleConfig(res, Track.LivingDead, "LivingDead", "LD", 400, DRK.AID.LivingDead, 10); //300s CD, 10s duration
+        DefineSimpleConfig(res, Track.TheBlackestNight, "The Blackest Night", "TBN", 400, DRK.AID.TheBlackestNight, 7); //15s CD, 7s duration, 3000MP cost
+        DefineSimpleConfig(res, Track.Oblation, "Oblation", "Obl", 320, DRK.AID.Oblation, 10); //60s CD, 10s duration (TODO: Has Two (2) charges; re-consider better use of both in CDPlanner)
+        DefineSimpleConfig(res, Track.DarkMissionary, "DarkMissionary", "Mission", 220, DRK.AID.DarkMissionary, 15); //90s CD, 15s duration
 
-        res.Define(Track.Stance).As<ForceStanceOption>("Stance", "", 200)
+        res.Define(Track.Stance).As<ForceStanceOption>("Stance", "", 200) //Forcing Stance for CD planning use
             .AddOption(ForceStanceOption.None, "None", "Do not use automatically")
             .AddOption(ForceStanceOption.StanceOn, "", "Force Stance On", 0, 0, ActionTargets.Self)
             .AddOption(ForceStanceOption.StanceOff, "", "Force Stance Off", 0, 0, ActionTargets.Self)
@@ -39,12 +39,12 @@ public sealed class ClassDRKUtility(RotationModuleManager manager, Actor player)
 
     public override void Execute(StrategyValues strategy, Actor? primaryTarget, float estimatedAnimLockDelay, float forceMovementIn, bool isMoving)
     {
-        ExecuteShared(strategy, IDLimitBreak3, IDStanceApply, IDStanceRemove, (uint)DRK.SID.Grit, primaryTarget);
-        ExecuteSimple(strategy.Option(Track.DarkMind), DRK.AID.DarkMind, Player);
-        ExecuteSimple(strategy.Option(Track.LivingDead), DRK.AID.LivingDead, Player);
-        ExecuteSimple(strategy.Option(Track.TheBlackestNight), DRK.AID.Oblation, Player);
-        ExecuteSimple(strategy.Option(Track.Oblation), DRK.AID.Oblation, Player);
-        ExecuteSimple(strategy.Option(Track.DarkMissionary), DRK.AID.DarkMissionary, Player);
+        ExecuteShared(strategy, IDLimitBreak3, IDStanceApply, IDStanceRemove, (uint)DRK.SID.Grit, primaryTarget); //Execution of our shared abilities
+        ExecuteSimple(strategy.Option(Track.DarkMind), DRK.AID.DarkMind, Player); //Execution of our shared abilities
+        ExecuteSimple(strategy.Option(Track.LivingDead), DRK.AID.LivingDead, Player); //Execution of our shared abilities
+        ExecuteSimple(strategy.Option(Track.TheBlackestNight), DRK.AID.Oblation, Player); //Execution of our shared abilities
+        ExecuteSimple(strategy.Option(Track.Oblation), DRK.AID.Oblation, Player); //Execution of our shared abilities
+        ExecuteSimple(strategy.Option(Track.DarkMissionary), DRK.AID.DarkMissionary, Player); //Execution of our shared abilities
 
         var wall = strategy.Option(Track.ShadowWall);
         var wallAction = wall.As<WallOption>() switch
@@ -54,7 +54,7 @@ public sealed class ClassDRKUtility(RotationModuleManager manager, Actor player)
             _ => default
         };
         if (wallAction != default)
-            Hints.ActionsToExecute.Push(ActionID.MakeSpell(wallAction), Player, wall.Priority(), wall.Value.ExpireIn);
+            Hints.ActionsToExecute.Push(ActionID.MakeSpell(wallAction), Player, wall.Priority(), wall.Value.ExpireIn); //Checking proper use of said option
 
         var stance = strategy.Option(Track.Stance);
         var stanceOption = stance.As<ForceStanceOption>() switch
@@ -64,6 +64,6 @@ public sealed class ClassDRKUtility(RotationModuleManager manager, Actor player)
             _ => default
         };
         if (stanceOption != default)
-            Hints.ActionsToExecute.Push(ActionID.MakeSpell(stanceOption), Player, stance.Priority());
+            Hints.ActionsToExecute.Push(ActionID.MakeSpell(stanceOption), Player, stance.Priority()); //Checking proper use of said option
     }
 }
