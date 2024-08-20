c4f9d835a4ad3e770a37bb534738db021d02911d
ace <aceakechi6@gmail.com>
2024-08-19 23:08:32 -0700

    Cleanup

--- BossMod/Autorotation/Utility/ClassPLDUtility.cs
+++ BossMod/Autorotation/Utility/ClassPLDUtility.cs
@@ -5,39 +5,39 @@ namespace BossMod.Autorotation;
 
 public sealed class ClassPLDUtility(RotationModuleManager manager, Actor player) : RoleTankUtility(manager, player)
 {
-    public enum Track { Sheltron = SharedTrack.Count, Sentinel, Cover, Bulwark, DivineVeil, PassageOfArms, HallowedGround }
-    public enum ShelOption { None, Sheltron, HolySheltron, Intervention }
-    public enum SentOption { None, Sentinel, Guardian }
+    public enum Track { Sheltron = SharedTrack.Count, Sentinel, Cover, Bulwark, DivineVeil, PassageOfArms, HallowedGround } //What we're tracking
+    public enum ShelOption { None, Sheltron, HolySheltron, Intervention } //Sheltron Options
+    public enum SentOption { None, Sentinel, Guardian } //Sentinel enhancement
 
-    public static readonly ActionID IDLimitBreak3 = ActionID.MakeSpell(PLD.AID.LastBastion);
-    public static readonly ActionID IDStanceApply = ActionID.MakeSpell(PLD.AID.IronWill);
-    public static readonly ActionID IDStanceRemove = ActionID.MakeSpell(PLD.AID.ReleaseIronWill);
+    public static readonly ActionID IDLimitBreak3 = ActionID.MakeSpell(PLD.AID.LastBastion); //LB
+    public static readonly ActionID IDStanceApply = ActionID.MakeSpell(PLD.AID.IronWill); //StanceOn
+    public static readonly ActionID IDStanceRemove = ActionID.MakeSpell(PLD.AID.ReleaseIronWill); //StanceOff
 
     public static RotationModuleDefinition Definition()
     {
-        var res = new RotationModuleDefinition("Utility: PLD", "Planner support for utility actions", "veyn, Akechi", RotationModuleQuality.WIP, BitMask.Build((int)Class.PLD), 100);
+        var res = new RotationModuleDefinition("Utility: PLD", "Planner support for utility actions", "veyn, Akechi", RotationModuleQuality.Good, BitMask.Build((int)Class.PLD), 100);
 
         DefineShared(res, IDLimitBreak3, IDStanceApply, IDStanceRemove);
 
-        res.Define(Track.Sheltron).As<ShelOption>("Sheltron", "Shel", 350)
+        res.Define(Track.Sheltron).As<ShelOption>("Sheltron", "Shel", 350) //Sheltron defintion for CD plans
             .AddOption(ShelOption.None, "None", "Do not use automatically")
             .AddOption(ShelOption.Sheltron, "Sheltron", "Use Sheltron", 0, 4, ActionTargets.Self, 35, 81)
             .AddOption(ShelOption.HolySheltron, "HolySheltron", "Use Holy Sheltron", 0, 4, ActionTargets.Self, 82)
             .AddOption(ShelOption.Intervention, "Intervention", "Use Intervention", 0, 4, ActionTargets.Party, 62)
             .AddAssociatedActions(PLD.AID.Sheltron, PLD.AID.HolySheltron, PLD.AID.Intervention);
 
-        res.Define(Track.Sentinel).As<SentOption>("Sentinel", "Sent", 550)
+        res.Define(Track.Sentinel).As<SentOption>("Sentinel", "Sent", 550) //Sentinel defintion for CD plans
             .AddOption(SentOption.None, "None", "Do not use automatically")
             .AddOption(SentOption.Sentinel, "Use", "Use Sentinel", 120, 15, ActionTargets.Self, 38, 91)
             .AddOption(SentOption.Guardian, "UseEx", "Use Guardian", 120, 15, ActionTargets.Self, 92)
             .AddAssociatedActions(PLD.AID.Sentinel, PLD.AID.Guardian);
 
-        DefineSimpleConfig(res, Track.Cover, "Cover", "", 320, PLD.AID.Cover, 12);
-        DefineSimpleConfig(res, Track.Bulwark, "Bulwark", "Bul", 450, PLD.AID.Bulwark, 10);
-        DefineSimpleConfig(res, Track.DivineVeil, "DivineVeil", "Veil", 220, PLD.AID.DivineVeil, 30);
+        DefineSimpleConfig(res, Track.Cover, "Cover", "", 320, PLD.AID.Cover, 12); //Cover defintion for CD plan 
+        DefineSimpleConfig(res, Track.Bulwark, "Bulwark", "Bul", 450, PLD.AID.Bulwark, 10); //Bulwark deifnition
+        DefineSimpleConfig(res, Track.DivineVeil, "DivineVeil", "Veil", 220, PLD.AID.DivineVeil, 30); //DivineVeil defintion for CD plan
+        DefineSimpleConfig(res, Track.PassageOfArms, "PassageOfArms", "Arms", 470, PLD.AID.PassageOfArms, 3); //PassageOfArms defintion for CD plan
+        DefineSimpleConfig(res, Track.HallowedGround, "HallowedGround", "Inv", 400, PLD.AID.HallowedGround, 10); //HallowedGround defintion for CD plan
         //DefineSimpleConfig(res, Track.Clemency, "Clemency", "Clem", 420, PLD.AID.Clemency); TODO: we don't really care, do we? maybe later
-        DefineSimpleConfig(res, Track.PassageOfArms, "PassageOfArms", "Arms", 470, PLD.AID.PassageOfArms, 3);
-        DefineSimpleConfig(res, Track.HallowedGround, "HallowedGround", "Inv", 400, PLD.AID.HallowedGround, 10);
 
         return res;
     }
@@ -45,12 +45,12 @@ public sealed class ClassPLDUtility(RotationModuleManager manager, Actor player)
     public override void Execute(StrategyValues strategy, Actor? primaryTarget, float estimatedAnimLockDelay, float forceMovementIn, bool isMoving)
     {
         ExecuteShared(strategy, IDLimitBreak3, IDStanceApply, IDStanceRemove, (uint)PLD.SID.IronWill, primaryTarget);
-        ExecuteSimple(strategy.Option(Track.Cover), PLD.AID.Cover, Player);
-        ExecuteSimple(strategy.Option(Track.Bulwark), PLD.AID.Bulwark, Player);
-        ExecuteSimple(strategy.Option(Track.DivineVeil), PLD.AID.DivineVeil, Player);
-        //ExecuteSimple(strategy.Option(Track.Clemency), PLD.AID.Clemency, Player); TODO: we don't really care, do we? maybe later
-        ExecuteSimple(strategy.Option(Track.PassageOfArms), PLD.AID.PassageOfArms, Player);
-        ExecuteSimple(strategy.Option(Track.HallowedGround), PLD.AID.HallowedGround, Player);
+        ExecuteSimple(strategy.Option(Track.Cover), PLD.AID.Cover, Player); //Cover execution
+        ExecuteSimple(strategy.Option(Track.Bulwark), PLD.AID.Bulwark, Player); //Bulwark execution
+        ExecuteSimple(strategy.Option(Track.DivineVeil), PLD.AID.DivineVeil, Player); //DivineVeil execution
+        ExecuteSimple(strategy.Option(Track.PassageOfArms), PLD.AID.PassageOfArms, Player); //PassageOfArms execution
+        ExecuteSimple(strategy.Option(Track.HallowedGround), PLD.AID.HallowedGround, Player); //HallowedGround execution
+        //DefineSimpleConfig(res, Track.Clemency, "Clemency", "Clem", 420, PLD.AID.Clemency); TODO: we don't really care, do we? maybe later
 
         var shel = strategy.Option(Track.Sheltron);
         var shelAction = shel.As<ShelOption>() switch
@@ -61,7 +61,7 @@ public sealed class ClassPLDUtility(RotationModuleManager manager, Actor player)
             _ => default
         };
         if (shelAction != default)
-            Hints.ActionsToExecute.Push(ActionID.MakeSpell(shelAction), shelAction == PLD.AID.Intervention ? ResolveTargetOverride(shel.Value) ?? CoTank() : Player, shel.Priority(), shel.Value.ExpireIn);
+            Hints.ActionsToExecute.Push(ActionID.MakeSpell(shelAction), shelAction == PLD.AID.Intervention ? ResolveTargetOverride(shel.Value) ?? CoTank() : Player, shel.Priority(), shel.Value.ExpireIn); //Sheltron & Intervention execution
 
         var sent = strategy.Option(Track.Sentinel);
         var sentAction = sent.As<SentOption>() switch
@@ -71,6 +71,6 @@ public sealed class ClassPLDUtility(RotationModuleManager manager, Actor player)
             _ => default
         };
         if (sentAction != default)
-            Hints.ActionsToExecute.Push(ActionID.MakeSpell(sentAction), Player, sent.Priority(), sent.Value.ExpireIn);
+            Hints.ActionsToExecute.Push(ActionID.MakeSpell(sentAction), Player, sent.Priority(), sent.Value.ExpireIn); //Sentinel execution
     }
 }
