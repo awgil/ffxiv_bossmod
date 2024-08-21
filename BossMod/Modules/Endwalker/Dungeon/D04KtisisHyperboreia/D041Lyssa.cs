bfe9c37b9639846c3060e6a9cac8dfe9bfc8e56a
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-08-21 16:06:56 -0400

    ktisis

---
+++ BossMod/Modules/Endwalker/Dungeon/D04KtisisHyperboreia/D041Lyssa.cs
@@ -0,0 +1,77 @@
+﻿namespace BossMod.Endwalker.Dungeon.D04KtisisHyperboreia.D041Lyssa;
+
+public enum OID : uint
+{
+    Boss = 0x3323, // R4.000, x1
+}
+
+public enum AID : uint
+{
+    SkullDasher = 25182, // Boss->none, 5.0s cast, single-target
+    PunishingSlice = 25177, // _Gen_Lyssa->self, 2.0s cast, range 50 width 50 rect
+    IcePillar = 25179, // _Gen_IcePillar->self, 3.0s cast, range 4 circle
+    FrigidStomp = 25181, // Boss->self, 5.0s cast, range 50 circle
+    PillarPierce = 25375, // _Gen_IcePillar->self, 5.0s cast, range 80 width 4 rect
+    HeavySmash = 25180, // Boss->none, 5.0s cast, range 6 circle
+}
+
+class SkullDasher(BossModule module) : Components.SingleTargetCast(module, ActionID.MakeSpell(AID.SkullDasher));
+
+class PunishingSlice(BossModule module) : Components.GenericAOEs(module, ActionID.MakeSpell(AID.PunishingSlice))
+{
+    private AOEInstance? _aoe;
+    private static readonly AOEShapeRect shape = new(50, 25);
+
+    private static readonly Dictionary<(byte index, uint state), Angle> aoeSources = new()
+    {
+        {(0x00, 0x00020001), default},
+        {(0x00, 0x00200010), 60.Degrees()},
+        {(0x00, 0x01000080), 120.Degrees()},
+        {(0x01, 0x00200010), 180.Degrees()},
+        {(0x01, 0x01000080), 240.Degrees()},
+        {(0x01, 0x00020001), 300.Degrees()}
+    };
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+        => Utils.ZeroOrOne(_aoe);
+
+    public override void OnEventEnvControl(byte index, uint state)
+    {
+        if (aoeSources.TryGetValue((index, state), out var rotation))
+        {
+            var activation = NumCasts == 0 ? Module.WorldState.FutureTime(13) : Module.WorldState.FutureTime(16);
+            var aoeSource = Arena.Center - rotation.ToDirection() * 12.5f;
+            _aoe = new AOEInstance(shape, aoeSource, rotation, activation);
+        }
+    }
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if (spell.Action == WatchedAction)
+        {
+            ++NumCasts;
+            _aoe = null;
+        }
+    }
+}
+
+class FrigidStomp(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID.FrigidStomp));
+class IcePillar(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.IcePillar), new AOEShapeCircle(4));
+class PillarPierce(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.PillarPierce), new AOEShapeRect(80, 2));
+class HeavySmash(BossModule module) : Components.StackWithCastTargets(module, ActionID.MakeSpell(AID.HeavySmash), 6, 4);
+
+class D041LyssaStates : StateMachineBuilder
+{
+    public D041LyssaStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<SkullDasher>()
+            .ActivateOnEnter<PunishingSlice>()
+            .ActivateOnEnter<FrigidStomp>()
+            .ActivateOnEnter<IcePillar>()
+            .ActivateOnEnter<PillarPierce>()
+            .ActivateOnEnter<HeavySmash>();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.Contributed, Contributors = "xan", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 787, NameID = 10396)]
+public class D041Lyssa(WorldState ws, Actor primary) : BossModule(ws, primary, new(-144, 49), new ArenaBoundsCircle(19.5f));
---
+++ BossMod/Modules/Endwalker/Dungeon/D04KtisisHyperboreia/D043Hermes.cs
@@ -0,0 +1,133 @@
+namespace BossMod.Endwalker.Dungeon.D04KtisisHyperboreia.D043Hermes;
+
+public enum OID : uint
+{
+    Boss = 0x348A, // R4.200, x?
+    Meteor = 0x348C, // R2.400, x?
+}
+
+public enum AID : uint
+{
+    Trismegistos = 25886, // Boss->self, 5.0s cast, range 40 circle
+    TrueAeroIV = 25889, // Karukeion->self, 4.0s cast, range 50 width 10 rect
+    TrueTornado = 25902, // Boss->self, 5.0s cast, single-target
+    CosmicKiss = 25891, // Meteor->self, 5.0s cast, range 40 circle
+    TrueAeroIV1 = 27836, // Karukeion->self, 4.0s cast, range 50 width 10 rect
+    TrueAero = 25899, // Boss->self, 5.0s cast, single-target
+    TrueAero2 = 25901, // Hermes->self, 2.5s cast, range 40 width 6 rect
+    TrueAeroIV2 = 27837, // Karukeion->self, 10.0s cast, range 50 width 10 rect
+    TrueAeroII = 25897, // Hermes->player, 5.0s cast, range 6 circle
+    TrueAeroII2 = 25898, // 233C->location, 3.5s cast, range 6 circle
+    TrueTornado2 = 25906, // Hermes->location, 2.5s cast, range 4 circle
+    TrueBravery = 25907, // Boss->self, 5.0s cast, single-target
+}
+
+class Trismegistos(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID.Trismegistos));
+class TrueTornado(BossModule module) : Components.SingleTargetCast(module, ActionID.MakeSpell(AID.TrueTornado));
+class TrueTornado2(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID.TrueTornado2), 4);
+class CosmicKiss(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.CosmicKiss), new AOEShapeCircle(10));
+
+class TrueAeroIV(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.TrueAeroIV), new AOEShapeRect(50, 5));
+class TrueAeroIV2(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.TrueAeroIV1), new AOEShapeRect(50, 5), maxCasts: 4);
+class TrueAeroIV3(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.TrueAeroIV2), new AOEShapeRect(50, 5), maxCasts: 4);
+class WindSafe(BossModule module) : Components.GenericAOEs(module)
+{
+    private IEnumerable<Actor> Meteors => Module.Enemies(OID.Meteor);
+    private readonly List<(Actor source, AOEInstance aoe)> SafeZones = [];
+
+    // naively, just the same width as meteor hitbox - seemed accurate during my testing, needs double-check though
+    private readonly float SafeZoneWidth = 2.4f;
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor) => SafeZones.Take(4).Select(x => x.aoe);
+
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        var shapes = SafeZones.Take(4).Select(s => s.aoe.Shape.Distance(s.aoe.Origin, s.aoe.Rotation)).ToList();
+        if (shapes.Count == 0)
+            return;
+
+        hints.AddForbiddenZone(p => -shapes.Min(e => e(p)), SafeZones[0].aoe.Activation);
+    }
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.TrueAeroIV1)
+        {
+            NumCasts++;
+            var meteorBlocking = Meteors.FirstOrDefault(x => x.Position.InRect(caster.Position, spell.Rotation, 50, 0, 5) && (NumCasts <= 4 || !WillBreak(x)));
+            if (meteorBlocking != null)
+                SafeZones.Add((caster, new AOEInstance(new AOEShapeRect(50, SafeZoneWidth), meteorBlocking.Position, spell.Rotation, Module.CastFinishAt(spell), ArenaColor.SafeFromAOE, false)));
+        }
+    }
+
+    public override void DrawArenaForeground(int pcSlot, Actor pc)
+    {
+        foreach (var meteor in Module.Enemies(OID.Meteor))
+        {
+            if (WillBreak(meteor))
+                Arena.ActorOutsideBounds(meteor.Position, meteor.Rotation, ArenaColor.Object);
+            else
+                Arena.Actor(meteor.Position, meteor.Rotation, ArenaColor.Object);
+        }
+    }
+
+    private bool WillBreak(Actor meteor) => meteor.ModelState.AnimState2 == 1;
+
+    public override void OnCastFinished(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.TrueAeroIV1)
+            SafeZones.RemoveAll(x => x.source == caster);
+    }
+
+    public override void OnActorDestroyed(Actor actor)
+    {
+        // reset cast counter for next iteration of mechanic
+        if (!Meteors.Any())
+            NumCasts = 0;
+    }
+}
+
+class TrueAero(BossModule module) : Components.GenericBaitAway(module, ActionID.MakeSpell(AID.TrueAero))
+{
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if (spell.Action == WatchedAction)
+            foreach (var player in WorldState.Party.WithoutSlot())
+                CurrentBaits.Add(new(caster, player, new AOEShapeRect(40, 3), Module.CastFinishAt(spell)));
+    }
+
+    public override void OnCastFinished(Actor caster, ActorCastInfo spell)
+    {
+        if (spell.Action == WatchedAction)
+            CurrentBaits.Clear();
+    }
+}
+
+class TrueAero2(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.TrueAero2), new AOEShapeRect(40, 3));
+class TrueBravery(BossModule module) : Components.CastInterruptHint(module, ActionID.MakeSpell(AID.TrueBravery));
+class TrueAeroII(BossModule module) : Components.SpreadFromCastTargets(module, ActionID.MakeSpell(AID.TrueAeroII), 6);
+class TrueAeroII2(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID.TrueAeroII2), 6);
+
+class D043HermesStates : StateMachineBuilder
+{
+    public D043HermesStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<Trismegistos>()
+            .ActivateOnEnter<TrueTornado>()
+            .ActivateOnEnter<TrueTornado2>()
+            .ActivateOnEnter<CosmicKiss>()
+            .ActivateOnEnter<TrueAeroIV>()
+            .ActivateOnEnter<TrueAeroIV3>()
+            .ActivateOnEnter<WindSafe>()
+            .ActivateOnEnter<TrueAero>()
+            .ActivateOnEnter<TrueAero2>()
+            .ActivateOnEnter<TrueBravery>()
+            .ActivateOnEnter<TrueAeroII>()
+            .ActivateOnEnter<TrueAeroII2>()
+            ;
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.Contributed, Contributors = "xan", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 787, NameID = 10399)]
+public class D043Hermes(WorldState ws, Actor primary) : BossModule(ws, primary, new(0, -50), new ArenaBoundsCircle(20));
