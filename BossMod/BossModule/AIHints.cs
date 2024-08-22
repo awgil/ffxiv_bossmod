32985ae8ce0c8fb80991563901555d4753ab919e
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-08-22 19:05:57 -0400

    reverte

--- BossMod/BossModule/AIHints.cs
+++ BossMod/BossModule/AIHints.cs
@@ -10,7 +10,7 @@ public sealed class AIHints
         public const int PriorityForbidFully = -2; // attacking this enemy is forbidden both by ai or player (e.g. invulnerable, or attacking/killing might lead to a wipe)
 
         public Actor Actor = actor;
-        public int Priority = PriorityForbidAI; // <0 means damaging is actually forbidden, 0 is default (TODO: revise default...)
+        public int Priority = actor.InCombat ? 0 : PriorityForbidAI; // <0 means damaging is actually forbidden, 0 is default (TODO: revise default...)
         //public float TimeToKill;
         public float AttackStrength = 0.05f; // target's predicted HP percent is decreased by this amount (0.05 by default)
         public WPos DesiredPosition = actor.Position; // tank AI will try to move enemy to this position
