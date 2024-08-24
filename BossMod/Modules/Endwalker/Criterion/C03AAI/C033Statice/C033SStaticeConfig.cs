6d09bbf0060211c28a1118ba86a6935e3f5e335a
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-08-24 05:38:16 +0100

    AAI B3: customizable strat.

---
+++ BossMod/Modules/Endwalker/Criterion/C03AAI/C033Statice/C033SStaticeConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace BossMod.Endwalker.Criterion.C03AAI.C033Statice;
+
+[ConfigDisplay(Order = 0x333, Parent = typeof(EndwalkerConfig))]
+public class C033SStaticeConfig() : ConfigNode()
+{
+    [PropertyDisplay("Darts 2: supports relative west, dd relative east")]
+    public bool Fireworks2Invert = false;
+}
