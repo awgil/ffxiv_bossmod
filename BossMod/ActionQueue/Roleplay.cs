dd0d94e1ec37c47ab676f2a1baf369a2ca356bcf
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-08-25 15:40:04 -0400

    delete rogue AIDs

--- BossMod/ActionQueue/Roleplay.cs
+++ BossMod/ActionQueue/Roleplay.cs
@@ -7,25 +7,6 @@ public enum AID : uint
     PhotonStream = 7620, // range 10 width 4 rect aoe
     DiffractiveMagitekCannon = 7621, // range 30 radius 10 ground targeted aoe
     HighPoweredMagitekCannon = 7622, // range 42 width 8 rect aoe
-
-    // Alphinaud - I forgot the name of this quest and it needs a module
-    RuinIII = 11191,
-    Physick = 11192,
-    TriShackle = 11482,
-
-    // Y'shtola - Will of the Moon (StB)
-    StoneIVSeventhDawn = 13423,
-    AeroIISeventhDawn = 13424,
-    CureIISeventhDawn = 13425,
-    Aetherwell = 13426,
-
-    // Hien - Requiem for Heroes (StB)
-    Kyokufu = 14840,
-    Gofu = 19046,
-    Yagetsu = 19047,
-    Ajisai = 14841,
-    HissatsuGyoten = 14842,
-    SecondWind = 15375,
 }
 
 public enum TraitID : uint { }
@@ -33,9 +14,6 @@ public enum TraitID : uint { }
 public enum SID : uint
 {
     RolePlaying = 1534,
-
-    // Hien
-    Ajisai = 1779,
 }
 
 public sealed class Definitions : IDisposable
@@ -46,22 +24,6 @@ public sealed class Definitions : IDisposable
         d.RegisterSpell(AID.PhotonStream);
         d.RegisterSpell(AID.DiffractiveMagitekCannon);
         d.RegisterSpell(AID.HighPoweredMagitekCannon);
-
-        d.RegisterSpell(AID.RuinIII);
-        d.RegisterSpell(AID.Physick);
-        d.RegisterSpell(AID.TriShackle);
-
-        d.RegisterSpell(AID.StoneIVSeventhDawn);
-        d.RegisterSpell(AID.AeroIISeventhDawn);
-        d.RegisterSpell(AID.CureIISeventhDawn);
-        d.RegisterSpell(AID.Aetherwell);
-
-        d.RegisterSpell(AID.Kyokufu);
-        d.RegisterSpell(AID.Gofu);
-        d.RegisterSpell(AID.Yagetsu);
-        d.RegisterSpell(AID.Ajisai);
-        d.RegisterSpell(AID.HissatsuGyoten);
-        d.RegisterSpell(AID.SecondWind);
     }
 
     public void Dispose() { }
