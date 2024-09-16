dae1a4810f3768a6dca5da8e4ab3bf3b3f57253e
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-09-16 15:12:59 -0400

    more things

---
+++ BossMod/Util/DateTime.cs
@@ -0,0 +1,20 @@
+﻿namespace BossMod;
+
+public static class DateTimeExtensions
+{
+    public static DateTime Floor(this DateTime dt, TimeSpan interval)
+        => dt.AddTicks(-(dt.Ticks % interval.Ticks));
+
+    public static DateTime Ceiling(this DateTime dt, TimeSpan interval)
+    {
+        var overflow = dt.Ticks % interval.Ticks;
+        return overflow == 0 ? dt : dt.AddTicks(interval.Ticks - overflow);
+    }
+
+    public static DateTime Round(this DateTime dt, TimeSpan interval)
+    {
+        var halfIntervalTicks = (interval.Ticks + 1) >> 1;
+
+        return dt.AddTicks(halfIntervalTicks - (dt.Ticks + halfIntervalTicks) % interval.Ticks);
+    }
+}
