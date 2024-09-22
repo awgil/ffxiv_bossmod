70cf8d8c93428c50b4839c014c7ca88326353d35
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-09-22 19:16:27 +0100

    Origenics bugfixes.

--- BossMod/Modules/Dawntrail/Dungeon/D05Origenics/D052Deceiver.cs
+++ BossMod/Modules/Dawntrail/Dungeon/D05Origenics/D052Deceiver.cs
@@ -14,7 +14,8 @@ public enum AID : uint
     AutoAttackAdd = 873, // SentryReal->player, no cast, single-target
     Teleport = 36362, // Boss->location, no cast, single-target
     Electrowave = 36371, // Boss->self, 5.0s cast, range 72 circle, raidwide
-    BionicThrash = 36369, // Boss->self, 7.0s cast, single-target, visual (two quadrant cleave)
+    BionicThrash1 = 36368, // Boss->self, 7.0s cast, single-target, visual (two quadrant cleave)
+    BionicThrash2 = 36369, // Boss->self, 7.0s cast, single-target, visual (two quadrant cleave)
     BionicThrashAOE = 36370, // Helper->self, 8.0s cast, range 30 90-degree cone
     InitializeAndroids = 36363, // Boss->self, 4.0s cast, single-target, visual (sentries)
     SynchroshotFake = 36373, // SentryFake->self, 5.0s cast, range 40 width 4 rect
@@ -44,29 +45,34 @@ class Surge(BossModule module) : Components.Knockback(module, ActionID.MakeSpell
 {
     private readonly List<(WPos origin, WDir dir)> _realTurrets = [];
     private readonly List<(WPos origin, WDir dir)> _fakeTurrets = [];
-    private DateTime _activation;
+    public DateTime Activation;
 
     public override IEnumerable<Source> Sources(int slot, Actor actor)
     {
-        if (_activation != default)
+        if (Activation != default)
         {
             float distance = 30;
             if (_realTurrets.Any(t => (t.origin.X < Module.Center.X) == (actor.Position.X < Module.Center.X) && Math.Abs(t.origin.Z - actor.Position.Z) <= 5))
                 distance = Math.Max(0, 15.5f - MathF.Abs(actor.Position.X - Module.Center.X));
-            yield return new(new(Module.Center.X, actor.Position.Z), distance, _activation);
+            yield return new(new(Module.Center.X, actor.Position.Z), distance, Activation);
         }
     }
 
     public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
     {
-        if (_activation != default)
+        if (Activation != default)
+        {
             foreach (var t in _fakeTurrets)
-                hints.AddForbiddenZone(ShapeDistance.Rect(t.origin, t.dir, 20, 0, 5), _activation);
+                hints.AddForbiddenZone(ShapeDistance.Rect(t.origin, t.dir, 20, 0, 5), Activation);
+            // this is paired with spread (electray), which resolve right after knockback - avoid taking same lanes as other party members
+            foreach (var p in Raid.WithoutSlot().Exclude(actor))
+                hints.AddForbiddenZone(ShapeDistance.Rect(new(Module.Center.X, p.Position.Z), new WDir(p.Position.X < Module.Center.X ? -1 : 1, 0), 16, 0, 5), Activation);
+        }
     }
 
     public override void DrawArenaBackground(int pcSlot, Actor pc)
     {
-        if (_activation != default)
+        if (Activation != default)
             foreach (var t in _realTurrets)
                 Arena.ZoneRect(t.origin, t.dir, 20, 0, 5, ArenaColor.SafeFromAOE);
     }
@@ -82,7 +88,7 @@ class Surge(BossModule module) : Components.Knockback(module, ActionID.MakeSpell
                 _fakeTurrets.Add((caster.Position, spell.Rotation.ToDirection()));
                 break;
             case AID.Surge:
-                _activation = Module.CastFinishAt(spell);
+                Activation = Module.CastFinishAt(spell);
                 break;
         }
     }
@@ -93,12 +99,22 @@ class Surge(BossModule module) : Components.Knockback(module, ActionID.MakeSpell
         {
             _realTurrets.Clear();
             _fakeTurrets.Clear();
-            _activation = default;
+            Activation = default;
         }
     }
 }
 
-class Electray(BossModule module) : Components.SpreadFromCastTargets(module, ActionID.MakeSpell(AID.Electray), 5);
+class Electray(BossModule module) : Components.SpreadFromCastTargets(module, ActionID.MakeSpell(AID.Electray), 5)
+{
+    private readonly Surge? _surge = module.FindComponent<Surge>();
+
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        // do not add spread hints before knockback is resolved
+        if (_surge == null || _surge.Activation == default)
+            base.AddAIHints(slot, actor, assignment, hints);
+    }
+}
 
 class D052DeceiverStates : StateMachineBuilder
 {
