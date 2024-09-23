3c69d105cf0c307d8c7290b7cef141704a69f00a
AceAkechi123 <aceakechi6@gmail.com>
2024-09-23 04:07:00 -0700

    BLM dash

--- BossMod/Autorotation/Utility/ClassBLMUtility.cs
+++ BossMod/Autorotation/Utility/ClassBLMUtility.cs
@@ -3,6 +3,12 @@
 public sealed class ClassBLMUtility(RotationModuleManager manager, Actor player) : RoleMagicalUtility(manager, player)
 {
     public enum Track { Manaward = SharedTrack.Count, AetherialManipulation }
+    public enum DashStrategy { Automatic, Force, GapClose } //GapCloser strategy
+    public float CDleft => World.Client.Cooldowns[ActionDefinitions.GCDGroup].Remaining;
+    public bool InMeleeRange(Actor? target) => Player.DistanceToHitbox(target) <= 3; //Checks if we're inside melee range
+
+    public const float DashMinCD = 0.8f; //Triple-weaving dash is not a good idea, since it might delay gcd for longer than normal anim lock
+
     public static readonly ActionID IDLimitBreak3 = ActionID.MakeSpell(BLM.AID.Meteor);
 
     public static RotationModuleDefinition Definition()
@@ -11,7 +17,12 @@ public sealed class ClassBLMUtility(RotationModuleManager manager, Actor player)
         DefineShared(res, IDLimitBreak3);
 
         DefineSimpleConfig(res, Track.Manaward, "Manaward", "", 600, BLM.AID.Manaward, 20);
-        DefineSimpleConfig(res, Track.AetherialManipulation, "Aetherial Manipulation", "Dash", 500, BLM.AID.AetherialManipulation);
+
+        res.Define(Track.AetherialManipulation).As<DashStrategy>("Dash", uiPriority: 20)
+            .AddOption(DashStrategy.Automatic, "Automatic", "No use")
+            .AddOption(DashStrategy.Force, "Force", "Use ASAP")
+            .AddOption(DashStrategy.GapClose, "GapClose", "Use as gapcloser if outside melee range")
+            .AddAssociatedActions(BLM.AID.AetherialManipulation);
 
         return res;
     }
@@ -20,6 +31,17 @@ public sealed class ClassBLMUtility(RotationModuleManager manager, Actor player)
     {
         ExecuteShared(strategy, IDLimitBreak3);
         ExecuteSimple(strategy.Option(Track.Manaward), BLM.AID.Manaward, Player);
-        ExecuteSimple(strategy.Option(Track.AetherialManipulation), BLM.AID.AetherialManipulation, null);
+
+        var dash = strategy.Option(Track.AetherialManipulation);
+        var dashStrategy = strategy.Option(Track.AetherialManipulation).As<DashStrategy>();
+        if (ShouldUseDash(dashStrategy, null))
+            Hints.ActionsToExecute.Push(ActionID.MakeSpell(BLM.AID.AetherialManipulation), null, dash.Priority());
     }
+    private bool ShouldUseDash(DashStrategy strategy, Actor? primaryTarget) => strategy switch
+    {
+        DashStrategy.Automatic => false,
+        DashStrategy.Force => CDleft >= DashMinCD,
+        DashStrategy.GapClose => !InMeleeRange(primaryTarget),
+        _ => false,
+    };
 }
