38d25c67e590acc1606e5515d81fcf9fb3dad604
AceAkechi123 <aceakechi6@gmail.com>
2024-09-23 03:13:04 -0700

    AST BLM BRD DNC DRG

--- BossMod/Autorotation/Utility/ClassDRGUtility.cs
+++ BossMod/Autorotation/Utility/ClassDRGUtility.cs
@@ -2,21 +2,22 @@
 
 public sealed class ClassDRGUtility(RotationModuleManager manager, Actor player) : RoleMeleeUtility(manager, player)
 {
-    //public enum Track { ElusiveJump = SharedTrack.Count }
+    public enum Track { WingedGlide = SharedTrack.Count }
 
     public static readonly ActionID IDLimitBreak3 = ActionID.MakeSpell(DRG.AID.DragonsongDive);
 
     public static RotationModuleDefinition Definition()
     {
-        var res = new RotationModuleDefinition("Utility: DRG", "Planner support for utility actions", "veyn", RotationModuleQuality.WIP, BitMask.Build((int)Class.DRG), 100);
+        var res = new RotationModuleDefinition("Utility: DRG", "Planner support for utility actions", "Akechi", RotationModuleQuality.Excellent, BitMask.Build((int)Class.DRG), 100);
         DefineShared(res, IDLimitBreak3);
 
-        // TODO: elusive jump (not sure how it can be planned really...)
+        DefineSimpleConfig(res, Track.WingedGlide, "Winged Glide", "Glide", 500, DRG.AID.WingedGlide);
         return res;
     }
 
     public override void Execute(StrategyValues strategy, Actor? primaryTarget, float estimatedAnimLockDelay, float forceMovementIn, bool isMoving)
     {
         ExecuteShared(strategy, IDLimitBreak3, primaryTarget);
+        ExecuteSimple(strategy.Option(Track.WingedGlide), DRG.AID.WingedGlide, primaryTarget);
     }
 }
