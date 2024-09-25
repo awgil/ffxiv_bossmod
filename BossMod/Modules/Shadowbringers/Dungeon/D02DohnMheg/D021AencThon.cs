98e3089989dfd1e921619224e1932c7c4c0e9fce
Adam Bennett <adam-bennett@live.com>
2024-09-24 22:38:02 -0700

    Ported DohnMeg from BMR, Minus B3 Chasm cause WTF!

---
+++ BossMod/Modules/Shadowbringers/Dungeon/D02DohnMheg/D021AencThon.cs
@@ -0,0 +1,110 @@
+﻿namespace BossMod.Shadowbringers.Dungeon.D02DohnMheg.D021AencThon;
+
+public enum OID : uint
+{
+    Boss = 0x3F2, // R=2.0
+    GeyserHelper1 = 0x1EAAA1, // controls animations for 2 geysers
+    GeyserHelper2 = 0x1EAAA2, // controls animations for 3 geysers
+    Helper = 0x233C
+}
+
+public enum AID : uint
+{
+    AutoAttack = 872, // Boss->player, no cast, single-target
+    CandyCane = 8857, // Boss->player, 4.0s cast, single-target
+    Hydrofall1 = 8871, // Boss->self, 3.0s cast, single-target
+    Hydrofall2 = 8893, // Helper->location, 3.0s cast, range 6 circle
+    LaughingLeap1 = 8852, // Boss->location, 4.0s cast, range 4 circle
+    LaughingLeap2 = 8840, // Boss->players, no cast, range 4 circle
+    Landsblood1 = 7822, // Boss->self, 3.0s cast, range 40 circle
+    Landsblood2 = 7899, // Boss->self, no cast, range 40 circle
+    Geyser = 8800 // Helper->self, no cast, range 6 circle
+}
+
+public enum IconID : uint
+{
+    Tankbuster = 198, // player
+    Stackmarker = 62 // player
+}
+
+class Landsblood(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID.Landsblood1), "Raidwides + Geysers");
+class CandyCane(BossModule module) : Components.SingleTargetCast(module, ActionID.MakeSpell(AID.CandyCane));
+class Hydrofall(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID.Hydrofall2), 6);
+class LaughingLeap(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID.LaughingLeap1), 4);
+class LaughingLeapStack(BossModule module) : Components.StackWithIcon(module, (uint)IconID.Stackmarker, ActionID.MakeSpell(AID.LaughingLeap2), 4, 5.15f, 4, 4);
+
+class Geyser(BossModule module) : Components.GenericAOEs(module)
+{
+    private static readonly AOEShapeCircle circle = new(6);
+
+    private static readonly Dictionary<OID, Dictionary<Angle, List<WPos>>> GeyserPositions = new()
+    {
+        {
+            OID.GeyserHelper1, new Dictionary<Angle, List<WPos>>
+            {
+                { 0.Degrees(), [new(0, 14.16f), new(-9, 45.16f)] },
+                { 180.Degrees(), [new(9, 15.16f), new(0, 46.16f)] },
+                { -90.Degrees(), [new(-15, 21.16f), new(16, 30.16f)] },
+                { 90.Degrees(), [new(-16, 30.16f), new(15, 39.16f)] }
+            }
+        },
+        {
+            OID.GeyserHelper2, new Dictionary<Angle, List<WPos>>
+            {
+                { 0.Degrees(), [new(0, 35.16f), new(-9, 15.16f), new(7, 23.16f)] },
+                { 90.Degrees(),  [new(-15, 39.16f), new(-7, 23.16f), new(5, 30.16f)] },
+                { 180.Degrees(), [new(9, 45.16f), new(-7, 37.16f), new(0, 25.16f)] },
+                { -90.Degrees(), [new(7, 37.16f), new(15, 21.16f), new(-5, 30.16f)] }
+            }
+        }
+    };
+
+    private readonly List<AOEInstance> _geysers = [];
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        foreach (var g in _geysers)
+            yield return new(g.Shape, g.Origin, default, g.Activation, g.Activation == _geysers[0].Activation ? ArenaColor.Danger : ArenaColor.AOE, g.Activation == _geysers[0].Activation);
+    }
+
+    public override void OnActorEAnim(Actor actor, uint state)
+    {
+        if (state == 0x00100020)
+        {
+            if (GeyserPositions.TryGetValue((OID)actor.OID, out var positionsByRotation))
+            {
+                var activation = WorldState.FutureTime(5.1f);
+                foreach (var (rotation, positions) in positionsByRotation)
+                    if (actor.Rotation.AlmostEqual(rotation, Angle.DegToRad))
+                    {
+                        foreach (var pos in positions)
+                            _geysers.Add(new(circle, pos, default, activation));
+                        break;
+                    }
+            }
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID == AID.Geyser && _geysers.Count > 0)
+            _geysers.RemoveAt(0);
+    }
+}
+
+class D021AencThonStates : StateMachineBuilder
+{
+    public D021AencThonStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<CandyCane>()
+            .ActivateOnEnter<Landsblood>()
+            .ActivateOnEnter<Hydrofall>()
+            .ActivateOnEnter<LaughingLeap>()
+            .ActivateOnEnter<LaughingLeapStack>()
+            .ActivateOnEnter<Geyser>();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.Verified, Contributors = "The Combat Reborn Team (Malediktus), Ported by Herculezz", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 649, NameID = 8141)]
+public class D021AencThon(WorldState ws, Actor primary) : BossModule(ws, primary, new(0, 30), new ArenaBoundsCircle(19.5f));
---
+++ BossMod/Modules/Shadowbringers/Dungeon/D02DohnMheg/D022Griaule.cs
@@ -0,0 +1,99 @@
+﻿namespace BossMod.Shadowbringers.Dungeon.D02DohnMheg.D022Griaule;
+
+public enum OID : uint
+{
+    Boss = 0x98E, // R=3.18-12.72
+    PaintedRoot = 0xF08, // R=1.48
+    PaintedSapling = 0xEFB // R=0.9
+}
+
+public enum AID : uint
+{
+    AutoAttack = 870, // Boss->player, no cast, single-target
+    Rake = 10355, // Boss->player, no cast, single-target
+    Swinge = 8906, // Boss->self, 4.0s cast, range 50+R 60-degree cone
+    Fodder = 8897, // Boss->self, 5.0s cast, single-target
+    Tiiimbeeer = 8915, // Boss->self, 6.0s cast, range 50 circle
+    FeedingTime = 8899, // PaintedSapling->player/Boss, no cast, single-target
+    CoilingIvy = 8901 // Boss->self, 3.0s cast, single-target
+}
+
+public enum TetherID : uint
+{
+    GrowthTether = 84 // EFB->Boss/player
+}
+
+class FeedingTime(BossModule module) : Components.InterceptTether(module, ActionID.MakeSpell(AID.FeedingTime), (uint)TetherID.GrowthTether)
+{
+    private DateTime _activation;
+    public override void OnActorCreated(Actor actor)
+    {
+        if ((OID)actor.OID == OID.PaintedSapling)
+            _activation = WorldState.FutureTime(10.9f);
+    }
+
+    //TODO: consider moving this logic to the component
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        if (Active)
+        {
+            var source = Module.Enemies(OID.PaintedSapling)[slot].Position;
+            var direction = (Module.PrimaryActor.Position - source).Normalized();
+            hints.AddForbiddenZone(ShapeDistance.InvertedRect(Module.PrimaryActor.Position - (Module.PrimaryActor.HitboxRadius + 0.1f) * Angle.FromDirection(direction).ToDirection(), source, 1), _activation);
+        }
+    }
+}
+
+class Tiiimbeeer(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID.Tiiimbeeer));
+class Swinge(BossModule module) : Components.GenericAOEs(module)
+{
+    private AOEInstance? _aoe;
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor) => Utils.ZeroOrOne(_aoe);
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.Swinge)
+            _aoe = new(new AOEShapeCone(50 + caster.HitboxRadius, 30.Degrees()), caster.Position, spell.Rotation, Module.CastFinishAt(spell));
+    }
+
+    public override void OnCastFinished(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.Swinge)
+            _aoe = null;
+    }
+}
+
+class D022GriauleStates : StateMachineBuilder
+{
+    public D022GriauleStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<FeedingTime>()
+            .ActivateOnEnter<Tiiimbeeer>()
+            .ActivateOnEnter<Swinge>();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.Verified, Contributors = "The Combat Reborn Team (Malediktus), Ported by Herculezz", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 649, NameID = 8143)]
+public class D022Griaule(WorldState ws, Actor primary) : BossModule(ws, primary, new(7.17f, -339), new ArenaBoundsCircle(24.6f))
+{
+    protected override void DrawEnemies(int pcSlot, Actor pc)
+    {
+        Arena.Actor(PrimaryActor, ArenaColor.Enemy);
+        Arena.Actors(Enemies(OID.PaintedRoot), ArenaColor.Enemy);
+    }
+
+    protected override void CalculateModuleAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        foreach (var e in hints.PotentialTargets)
+        {
+            e.Priority = (OID)e.Actor.OID switch
+            {
+                OID.PaintedRoot => 2,
+                OID.Boss => 1,
+                _ => 0
+            };
+        }
+    }
+}
