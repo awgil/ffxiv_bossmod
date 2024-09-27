9bfc33b4ccf7ece6ba5c20089e6e2c453f958ad8
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-09-27 20:00:51 +0100

    Function order consistency.

--- BossMod/BossModule/SimpleBossModule.cs
+++ BossMod/BossModule/SimpleBossModule.cs
@@ -4,7 +4,6 @@
 // these always center map around PC
 public abstract class SimpleBossModule(WorldState ws, Actor primary) : BossModule(ws, primary, primary.Position, new ArenaBoundsCircle(30))
 {
-    protected override void UpdateModule() => Arena.Center = WorldState.Party.Player()?.Position ?? default;
-
     public override bool CheckReset() => !PrimaryActor.InCombat;
+    protected override void UpdateModule() => Arena.Center = WorldState.Party.Player()?.Position ?? default;
 }
