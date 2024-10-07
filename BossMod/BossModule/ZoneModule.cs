bab9f8944f437755fb85ed54a45836eb49c6d75e
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-10-07 21:33:07 +0100

    Zone module example.

--- BossMod/BossModule/ZoneModule.cs
+++ BossMod/BossModule/ZoneModule.cs
@@ -1,4 +1,7 @@
-﻿namespace BossMod;
+﻿using Dalamud.Interface.Utility.Raii;
+using ImGuiNET;
+
+namespace BossMod;
 
 public abstract class ZoneModule(WorldState ws) : IDisposable
 {
@@ -15,5 +18,18 @@ public abstract class ZoneModule(WorldState ws) : IDisposable
     }
 
     public virtual void Update() { }
+    public virtual bool WantToBeDrawn() => false; // return true if it wants to be drawn (higher priority than inactive boss modules, but lower priority than active)
+    public virtual List<string> CalculateGlobalHints() => [];
     public virtual void CalculateAIHints(Actor player, AIHints hints) { } // note: this is called after framework automatically fills auto-detected hints
+
+    public void DrawGlobalHints()
+    {
+        using var color = ImRaii.PushColor(ImGuiCol.Text, 0xffffff00);
+        foreach (var hint in CalculateGlobalHints())
+        {
+            ImGui.TextUnformatted(hint);
+            //ImGui.SameLine();
+        }
+        //ImGui.NewLine();
+    }
 }
