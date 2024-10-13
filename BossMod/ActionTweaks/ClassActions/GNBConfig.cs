7145218275395d7de9dd30e241fd35a731a633e1
AceAkechi123 <aceakechi6@gmail.com>
2024-10-12 22:22:51 -0700

    remove GNBConfig options

--- BossMod/ActionTweaks/ClassActions/GNBConfig.cs
+++ BossMod/ActionTweaks/ClassActions/GNBConfig.cs
@@ -3,19 +3,6 @@ namespace BossMod;
 [ConfigDisplay(Parent = typeof(ActionTweaksConfig))]
 class GNBConfig : ConfigNode
 {
-    [PropertyDisplay("Forbid 'Lightning Shot' too early in prepull")]
+    [PropertyDisplay("Prevent use of 'Lightning Shot' too early when in pre-pull")]
     public bool ForbidEarlyLightningShot = true;
-
-    // TODO: things below should be strategy options rather than global options
-    [PropertyDisplay("Use both Rough Divide charges in No Mercy")]
-    public bool NoMercyRoughDivide = true;
-
-    [PropertyDisplay("<= 2.47 sks rotation")]
-    public bool Skscheck = true;
-
-    [PropertyDisplay("Early No Mercy in Opener (NOTE: This will break Lv30-53 rotation)")]
-    public bool EarlyNoMercy = true;
-
-    [PropertyDisplay("Early Sonic Break in Opener (NOTE: This will break Lv30-53 rotation)")]
-    public bool EarlySonicBreak = true;
 }
