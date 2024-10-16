9b62b9455634e6f0717a0c340dceebce6e96818e
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-10-16 21:20:56 +0100

    Fix jittery movement on outdoor bosses.

--- BossMod/BossModule/SimpleBossModule.cs
+++ BossMod/BossModule/SimpleBossModule.cs
@@ -4,6 +4,16 @@
 // these always center map around PC
 public abstract class SimpleBossModule(WorldState ws, Actor primary) : BossModule(ws, primary, primary.Position, new ArenaBoundsCircle(30))
 {
+    private WPos _prevFramePathfindCenter;
+
     public override bool CheckReset() => !PrimaryActor.InCombat;
     protected override void UpdateModule() => Arena.Center = WorldState.Party.Player()?.Position ?? default;
+
+    protected override void CalculateModuleAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        // we don't want to change pathfinding map origin every time player slightly moves, it makes movement jittery
+        // instead, (a) quantize origin and (b) only update it when player moves sufficiently far away
+        hints.PathfindMapCenter = _prevFramePathfindCenter.AlmostEqual(Arena.Center, 5) ? _prevFramePathfindCenter : (_prevFramePathfindCenter = Arena.Center.Rounded());
+        hints.PathfindMapBounds = AIHints.DefaultBounds;
+    }
 }
