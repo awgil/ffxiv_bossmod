4220885cdf539b7826de55b3bcecdbe8024e1dcd
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-11-05 23:22:04 -0500

    sohr khai

---
+++ BossMod/Modules/Heavensward/Dungeon/D13SohrKhai/D131ChieftainMoglin.cs
@@ -0,0 +1,163 @@
+﻿namespace BossMod.Heavensward.Dungeon.D13SohrKhai.D131ChieftainMoglin;
+
+public enum OID : uint
+{
+    _Gen_EnchantedFan = 0x1605, // R3.000, x1
+    _Gen_ChieftainMoglin = 0x1B2, // R0.500, x23
+    Boss = 0x15FB, // R1.800, x1
+    _Gen_PomguardPompincher = 0x1602, // R0.900, x0 (spawn during fight)
+    _Gen_PomguardPomchopper = 0x15FD, // R0.900, x0 (spawn during fight)
+    _Gen_CaptainMogsun = 0x15FC, // R0.900, x0 (spawn during fight)
+    _Gen_PomguardPomcrier = 0x1601, // R0.900, x1
+    _Gen_PomguardPomfluffer = 0x15FE, // R0.900, x1
+    _Gen_PomguardPomfryer = 0x1600, // R0.900, x1
+    _Gen_PomguardPompiercer = 0x15FF, // R0.900, x1
+    _Gen_DemoniacalMogcane = 0x1603, // R5.000, x0 (spawn during fight)
+}
+
+public enum AID : uint
+{
+    _AutoAttack_Attack = 872, // Boss->player, no cast, single-target
+    _Weaponskill_1000KuponzeCharge = 6019, // Boss->self, no cast, range 8+R ?-degree cone
+    _AutoAttack_Attack1 = 870, // 15FC/1602->player, no cast, single-target
+    _Weaponskill_PoisonNeedle = 4432, // 1602->player, no cast, single-target
+    _Spell_PomHoly = 6020, // Boss->location, 3.0s cast, range 50 circle
+    _Spell_PomPraise = 6016, // 1B2->self, 13.0s cast, range 4 circle
+    _Spell_PomPraise1 = 6015, // Boss->self, 13.0s cast, single-target
+    _Weaponskill_100KuponzeSwipe = 4429, // 15FD->self, 3.0s cast, range 20+R 90-degree cone
+    _Spell_DemoniacalMogcane = 6017, // Boss->self, 2.0s cast, single-target
+    _Weaponskill_MoogleEyeShot = 4427, // 15FF->player, no cast, single-target
+    _Ability_PomBom = 6018, // 1603->self, no cast, single-target
+    _Ability_PomBom1 = 6212, // _Gen_ChieftainMoglin->self, no cast, range 40+R width 4 rect
+    _Weaponskill_SpinningMogshield = 4425, // _Gen_CaptainMogsun->self, 3.0s cast, range 6+R circle
+    _Spell_MarchOfTheMoogles = 4431, // _Gen_PomguardPomcrier->self, 5.0s cast, range 10 circle
+    _Spell_PomCure = 4426, // _Gen_PomguardPomfluffer->Boss, 3.0s cast, single-target
+    _Spell_PomFlare = 4428, // _Gen_PomguardPomfryer->self, 6.0s cast, range 21+R circle
+}
+
+public enum SID : uint
+{
+    _Gen_Invincibility = 325, // none->Boss, extra=0x0
+    _Gen_OffBalance = 1064, // none->_Gen_CaptainMogsun/_Gen_PomguardPomchopper/_Gen_PomguardPompincher, extra=0x0
+    _Gen_Poison = 18, // _Gen_PomguardPompincher->player, extra=0x0
+}
+
+class Mogshield(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_SpinningMogshield), new AOEShapeCircle(6.9f));
+class ThousandKuponzeCharge(BossModule module) : Components.GenericBaitAway(module)
+{
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID == AID._Spell_DemoniacalMogcane)
+            CurrentBaits.Add(new(caster, WorldState.Actors.Find(caster.TargetID)!, new AOEShapeCone(9.8f, 60.Degrees()), WorldState.FutureTime(4.2f)));
+
+        if ((AID)spell.Action.ID == AID._Weaponskill_1000KuponzeCharge)
+            CurrentBaits.Clear();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        // sanity check: clear baits if cleave is delayed enough (haven't seen this happen in any replays)
+        CurrentBaits.RemoveAll(b => (WorldState.CurrentTime - b.Activation).TotalSeconds > 6);
+    }
+}
+class PomFlare(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Spell_PomFlare), new AOEShapeCircle(21.9f));
+class PomHoly(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID._Spell_PomHoly));
+class Swipe(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_100KuponzeSwipe), new AOEShapeCone(20.9f, 45.Degrees()));
+// pombom 6.4f
+class PomBom(BossModule module) : Components.GenericAOEs(module)
+{
+    private record struct CaneObj(Actor Cane, DateTime Activation);
+    private CaneObj? Cane;
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor) => Utils.ZeroOrOne(Cane).Select(c => new AOEInstance(new AOEShapeCross(20.25f, 2), c.Cane.Position, c.Cane.Rotation, c.Activation));
+
+    public override void OnActorCreated(Actor actor)
+    {
+        if ((OID)actor.OID == OID._Gen_DemoniacalMogcane)
+        {
+            Cane = new(actor, WorldState.FutureTime(6.4f));
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID == AID._Ability_PomBom1)
+            Cane = null;
+    }
+}
+class PomPraise(BossModule module) : BossComponent(module)
+{
+    private readonly List<Actor> Casters = [];
+
+    private IEnumerable<Actor> RaisedMoogles => Casters.Count == 0 ? [] : WorldState.Actors.Where(e => e.FindStatus(SID._Gen_OffBalance) != null && WillRaise(e));
+
+    private bool WillRaise(Actor moogle) => Casters.Any(c => moogle.Position.InCircle(c.Position, 4));
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID._Spell_PomPraise)
+            Casters.Add(caster);
+    }
+
+    public override void OnCastFinished(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID._Spell_PomPraise)
+            Casters.Remove(caster);
+    }
+
+    public override void DrawArenaForeground(int pcSlot, Actor pc)
+    {
+        foreach (var e in WorldState.Actors.Exclude(Module.PrimaryActor).Where(x => !x.IsAlly && x.IsTargetable))
+            Arena.Actor(e, e.FindStatus(SID._Gen_OffBalance) == null ? ArenaColor.Enemy : WillRaise(e) ? ArenaColor.Object : ArenaColor.PlayerGeneric);
+    }
+
+    public override void DrawArenaBackground(int pcSlot, Actor pc)
+    {
+        foreach (var c in Casters)
+            Arena.ZoneCircle(c.Position, 4, ArenaColor.AOE);
+    }
+
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        if (Casters.Count == 0)
+            return;
+
+        foreach (var r in hints.PotentialTargets)
+            if (r.Actor.FindStatus(SID._Gen_OffBalance) != null && WillRaise(r.Actor))
+                r.Priority = 5;
+    }
+
+    public override void AddHints(int slot, Actor actor, TextHints hints)
+    {
+        if (RaisedMoogles.Any())
+            hints.Add("Knock moogles out of raise!");
+    }
+}
+
+class ChieftainMoglinStates : StateMachineBuilder
+{
+    public ChieftainMoglinStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<PomHoly>()
+            .ActivateOnEnter<Swipe>()
+            .ActivateOnEnter<PomPraise>()
+            .ActivateOnEnter<PomFlare>()
+            .ActivateOnEnter<PomBom>()
+            .ActivateOnEnter<ThousandKuponzeCharge>()
+            .ActivateOnEnter<Mogshield>();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 171, NameID = 4943)]
+public class ChieftainMoglin(WorldState ws, Actor primary) : BossModule(ws, primary, new(-400, -158), new ArenaBoundsCircle(20))
+{
+    protected override void CalculateModuleAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        foreach (var h in hints.PotentialTargets)
+            h.Priority = h.Actor.FindStatus(SID._Gen_Invincibility) == null ? 1 : 0;
+    }
+}
+
---
+++ BossMod/Modules/Heavensward/Dungeon/D13SohrKhai/D133Hraesvelgr.cs
@@ -0,0 +1,123 @@
+﻿namespace BossMod.Heavensward.Dungeon.D13SohrKhai.D133Hraesvelgr;
+
+public enum OID : uint
+{
+    Boss = 0x3D17,
+    Helper = 0x233C,
+}
+
+public enum AID : uint
+{
+    _AutoAttack_ = 32142, // Boss->player, no cast, single-target
+    _Weaponskill_Wyrmclaw = 32141, // Boss->player, 5.0s cast, single-target
+    _Weaponskill_HallowedWings = 32136, // Boss->self, 6.0s cast, range 50 width 22 rect, regular aoe (with offset)
+    _Weaponskill_HallowedWings1 = 32137, // Boss->self, 6.0s cast, range 50 width 22 rect, regular aoe (with offset)
+    _Weaponskill_HolyStorm = 32127, // Boss->self, 5.0s cast, range 40 circle, raidwide
+    _Weaponskill_HallowedDive = 32131, // Boss->self, 6.0s cast, range 40 width 20 rect, regular aoe
+    _Weaponskill_HolyOrb = 32129, // Helper->self, 5.0s cast, range 6 circle, exaflares
+    _Weaponskill_HolyOrb1 = 32130, // Helper->self, no cast, range 6 circle
+    _Weaponskill_AkhMorn = 32132, // Boss->players, 5.0s cast, range 6 circle, stack 1
+    _Weaponskill_AkhMorn1 = 32133, // Boss->players, no cast, range 6 circle, stack 2
+    _Weaponskill_HolyBreath = 32138, // Boss->self, 5.0+1.0s cast, single-target
+    _Weaponskill_HolyBreath1 = 32139, // Helper->player, 6.0s cast, range 6 circle, spread
+    _Weaponskill_DiamondStorm = 32128, // Boss->self, 5.0s cast, range 40 circle, raidwide + ice
+    _Weaponskill_FrigidDive = 32134, // Boss->self, 6.0s cast, range 40 width 20 rect, regular aoe
+    _Weaponskill_FrostedOrb = 32135, // Helper->self, 5.0s cast, range 6 circle, regular aoe
+}
+
+class HolyStorm(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID._Weaponskill_HolyStorm));
+class DiamondStorm(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID._Weaponskill_DiamondStorm));
+class FrigidDive(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_FrigidDive), new AOEShapeRect(40, 10));
+class HallowedDive(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_HallowedDive), new AOEShapeRect(40, 10));
+class Wyrmclaw(BossModule module) : Components.SingleTargetCast(module, ActionID.MakeSpell(AID._Weaponskill_Wyrmclaw));
+class FrostedOrb(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_FrostedOrb), new AOEShapeCircle(6));
+class HolyBreath(BossModule module) : Components.SpreadFromIcon(module, 311, ActionID.MakeSpell(AID._Weaponskill_HolyBreath1), 6, 6);
+class HallowedWings(BossModule module) : Components.GenericAOEs(module)
+{
+    private readonly List<Actor> Casters = [];
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor) => Casters.Select(c => new AOEInstance(new AOEShapeRect(50, 11), c.CastInfo!.LocXZ, c.CastInfo!.Rotation, Module.CastFinishAt(c.CastInfo)));
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID is AID._Weaponskill_HallowedWings or AID._Weaponskill_HallowedWings1)
+            Casters.Add(caster);
+    }
+
+    public override void OnCastFinished(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID is AID._Weaponskill_HallowedWings or AID._Weaponskill_HallowedWings1)
+            Casters.Remove(caster);
+    }
+}
+class AkhMorn(BossModule module) : Components.UniformStackSpread(module, 6, 0)
+{
+    private int Counter;
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID._Weaponskill_AkhMorn)
+            AddStack(WorldState.Actors.Find(spell.TargetID)!, Module.CastFinishAt(spell));
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID is AID._Weaponskill_AkhMorn or AID._Weaponskill_AkhMorn1)
+            if (++Counter >= 6)
+                Stacks.Clear();
+    }
+}
+class HolyOrb(BossModule module) : Components.Exaflare(module, new AOEShapeCircle(6))
+{
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID._Weaponskill_HolyOrb)
+            Lines.Add(new Line()
+            {
+                Next = caster.Position,
+                Advance = caster.Rotation.ToDirection() * 6.8f,
+                NextExplosion = Module.CastFinishAt(spell),
+                TimeToMove = 0.95f,
+                ExplosionsLeft = 5,
+                MaxShownExplosions = 3
+            });
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID is AID._Weaponskill_HolyOrb or AID._Weaponskill_HolyOrb1)
+        {
+            int index = Lines.FindIndex(item => item.Next.AlmostEqual(caster.Position, 1));
+            if (index == -1)
+            {
+                ReportError($"Failed to find entry for {caster.InstanceID:X}");
+                return;
+            }
+
+            AdvanceLine(Lines[index], caster.Position);
+            if (Lines[index].ExplosionsLeft == 0)
+                Lines.RemoveAt(index);
+        }
+    }
+}
+
+class HraesvelgrStates : StateMachineBuilder
+{
+    public HraesvelgrStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<Wyrmclaw>()
+            .ActivateOnEnter<HallowedWings>()
+            .ActivateOnEnter<FrigidDive>()
+            .ActivateOnEnter<HallowedDive>()
+            .ActivateOnEnter<HolyStorm>()
+            .ActivateOnEnter<DiamondStorm>()
+            .ActivateOnEnter<AkhMorn>()
+            .ActivateOnEnter<FrostedOrb>()
+            .ActivateOnEnter<HolyBreath>()
+            .ActivateOnEnter<HolyOrb>();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 171, NameID = 4954)]
+public class Hraesvelgr(WorldState ws, Actor primary) : BossModule(ws, primary, new(400, -400), new ArenaBoundsCircle(20));
+
