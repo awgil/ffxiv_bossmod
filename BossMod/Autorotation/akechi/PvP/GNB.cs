e389a0b25ad8cb692933fa98e049ebc67381d7a9
Akechi-kun <167795370+Akechi-kun@users.noreply.github.com>
2024-12-01 05:16:18 -0800

    cleanup GNB PvP

--- BossMod/Autorotation/akechi/PvP/GNB.cs
+++ BossMod/Autorotation/akechi/PvP/GNB.cs
@@ -1,5 +1,4 @@
 ﻿using FFXIVClientStructs.FFXIV.Client.Game.Gauge;
-using FFXIVClientStructs.FFXIV.Component.GUI;
 using AID = BossMod.GNB.AID;
 using SID = BossMod.GNB.SID;
 
@@ -10,24 +9,23 @@ namespace BossMod.Autorotation.akechi.PvP;
 public sealed class GNB(RotationModuleManager manager, Actor player) : RotationModule(manager, player)
 {
     #region Enums: Abilities / Strategies
-    //Actions tracked for Cooldown Planner execution
     public enum Track
     {
-        Burst,           //Burst tracking
-        Combo,           //Solid Barrel combo tracking
-        LimitBreak,      //Limit Break tracking
-        GnashingFang,    //Gnashing Fang action tracking
-        FatedCircle,     //Fated Circle ability tracking
-        RoughDivide,     //Rough Divide ability tracking
-        Zone,            //Blasting Zone ability tracking
-        Corundum,        //Heart of Corundum ability tracking
+        Burst,
+        Combo,
+        LimitBreak,
+        GnashingFang,
+        FatedCircle,
+        RoughDivide,
+        Zone,
+        Corundum,
     }
 
     public enum BurstStrategy
     {
-        Automatic,        //Automatically execute based on conditions
-        Force,            //Force burst actions regardless of conditions
-        Hold              //Conserve resources and cooldowns
+        Automatic,
+        Force,
+        Hold
     }
 
     public enum ComboStrategy
@@ -56,30 +54,28 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
 
     public enum OffensiveStrategy
     {
-        Automatic,      //Automatically decide when to use offensive abilities
-        Force,          //Force the use of offensive abilities regardless of conditions
-        Delay           //Delay the use of offensive abilities for strategic reasons
+        Automatic,
+        Force,
+        Delay
     }
 
     #endregion
 
     public static RotationModuleDefinition Definition()
     {
-        //Module title & signature
         var res = new RotationModuleDefinition("GNB (PvP)", "PvP Rotation Module", "Standard rotation (Akechi)", "Akechi", RotationModuleQuality.Basic, BitMask.Build((int)Class.GNB), 100);
 
         #region Custom strategies
-        //Burst strategy
         res.Define(Track.Burst).As<BurstStrategy>("Burst", uiPriority: 190)
             .AddOption(BurstStrategy.Automatic, "Automatic", "Use everything optimally")
             .AddOption(BurstStrategy.Force, "Force", "Force everything")
             .AddOption(BurstStrategy.Hold, "Hold", "Hold everything");
-        //Combo strategy
+
         res.Define(Track.Combo).As<ComboStrategy>("Combo", uiPriority: 190)
             .AddOption(ComboStrategy.Automatic, "Automatic", "Use combo optimally")
             .AddOption(ComboStrategy.Force, "Force", "Force combo")
             .AddOption(ComboStrategy.Hold, "Hold", "Hold combo");
-        //Limit Break strategy
+
         res.Define(Track.LimitBreak).As<LimitBreakStrategy>("Limit Break", uiPriority: 190)
             .AddOption(LimitBreakStrategy.Automatic, "Automatic", "Use Limit Break optimally")
             .AddOption(LimitBreakStrategy.Force, "Force", "Force Limit Break")
@@ -92,21 +88,25 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
             .AddOption(OffensiveStrategy.Force, "Force", "Force", 0, 0, ActionTargets.Hostile, 30)
             .AddOption(OffensiveStrategy.Delay, "Delay", "Delay", 0, 0, ActionTargets.None, 30)
             .AddAssociatedActions(AID.GnashingFangPvP);
+
         res.Define(Track.FatedCircle).As<OffensiveStrategy>("Fated Circle", uiPriority: 150)
             .AddOption(OffensiveStrategy.Automatic, "Automatic", "Use normally")
             .AddOption(OffensiveStrategy.Force, "Force", "Force", 0, 0, ActionTargets.Hostile, 30)
             .AddOption(OffensiveStrategy.Delay, "Delay", "Delay", 0, 0, ActionTargets.None, 30)
             .AddAssociatedActions(AID.FatedCirclePvP);
+
         res.Define(Track.RoughDivide).As<OffensiveStrategy>("Rough Divide", uiPriority: 150)
             .AddOption(OffensiveStrategy.Automatic, "Automatic", "Use normally")
             .AddOption(OffensiveStrategy.Force, "Force", "Force", 0, 0, ActionTargets.Hostile, 30)
             .AddOption(OffensiveStrategy.Delay, "Delay", "Delay", 0, 0, ActionTargets.None, 30)
             .AddAssociatedActions(AID.RoughDividePvP);
+
         res.Define(Track.Zone).As<OffensiveStrategy>("Zone", uiPriority: 150)
             .AddOption(OffensiveStrategy.Automatic, "Automatic", "Use normally")
             .AddOption(OffensiveStrategy.Force, "Force", "Force", 0, 0, ActionTargets.Hostile, 30)
             .AddOption(OffensiveStrategy.Delay, "Delay", "Delay", 0, 0, ActionTargets.None, 30)
             .AddAssociatedActions(AID.BlastingZonePvP);
+
         res.Define(Track.Corundum).As<OffensiveStrategy>("Corundum", uiPriority: 150)
             .AddOption(OffensiveStrategy.Automatic, "Automatic", "Use normally")
             .AddOption(OffensiveStrategy.Force, "Force", "Force", 0, 0, ActionTargets.Hostile, 30)
@@ -115,24 +115,22 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
         #endregion
 
         return res;
-
     }
 
     #region Priorities
-    //Priority for GCDs used
     public enum GCDPriority
     {
         None = 0,
-        KeenEdge = 325,    //1
-        BrutalShell = 320, //2
-        SolidBarrel = 315, //3
-        BurstStrike = 340, //4
+        KeenEdge = 325,
+        BrutalShell = 320,
+        SolidBarrel = 315,
+        BurstStrike = 340,
         Combo = 350,
         GnashingFang = 400,
         FatedCircle = 450,
         ForcedGCD = 900,
     }
-    //Priority for oGCDs used
+
     public enum OGCDPriority
     {
         None = 0,
@@ -146,70 +144,57 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
     #endregion
 
     #region Placeholders for Variables
-    //Cooldown Related
-    private float nmLeft; //Time left on No Mercy buff (20s base)
-    private float rdCD; //Time left on No Mercy cooldown (60s base)
-    private bool hasNM; //Checks self for No Mercy buff
-    private bool hasBlast; //Checks self for Ready To Blast buff
-    private bool hasRaze; //Checks self for Ready To Raze buff
-    private bool hasRip; //Checks self for Ready To Rip buff
-    private bool hasTear; //Checks self for Ready To Tear buff
-    private bool hasGouge; //Checks self for Ready To Gouge buff
-    private bool can1; //can Keen Edge
-    private bool can2; //can Brutal Shell
-    private bool can3; //can Solid Barrel
-    private bool can4; //can Burst Strike
-    private bool canGF; //can Gnashing Fang
-    private bool canFC; //can Fated Circle
-    private bool canZone; //can Blasting Zone
-    private bool canHyper; //can Hypervelocity
-    private bool canBrand; //can Fated Brand
-    private bool canRip; //can Jugular Rip
-    private bool canTear; //can Abdomen Tear
-    private bool canGouge; //can Eye Gouge
+    private float nmLeft;
+    private float rdCD;
+    private bool hasNM;
+    private bool hasBlast;
+    private bool hasRaze;
+    private bool hasRip;
+    private bool hasTear;
+    private bool hasGouge;
+    private bool canGF;
+    private bool canFC;
+    private bool canZone;
+    private bool canHyper;
+    private bool canBrand;
+    private bool canRip;
+    private bool canTear;
+    private bool canGouge;
 
-    //Misc
-    public bool LBready; //Checks if Limit Break is ready
-    public float GFcomboStep; //Current Gnashing Fang combo step (0-3)
-    public float comboStep; //Current combo step (0-4)
-    public bool inCombo; //Checks if in combo
-    public bool inGF; //Checks if in Gnashing Fang combo
-    public float GCDLength; //Current GCD length, adjusted by skill speed/haste (2.5s baseline)
-    public AID NextGCD; //Next global cooldown action to be used (needed for cartridge management)
-    private GCDPriority NextGCDPrio; //Priority of the next GCD, used for decision making on cooldowns
+    public bool LBready;
+    public float GFcomboStep;
+    public float comboStep;
+    public bool inCombo;
+    public bool inGF;
+    public float GCDLength;
+    public AID NextGCD;
+    private GCDPriority NextGCDPrio;
     #endregion
 
     #region Module Helpers
-    private float CD(AID aid) => World.Client.Cooldowns[ActionDefinitions.Instance.Spell(aid)!.MainCooldownGroup].Remaining; //Get remaining cooldown time for the specified action
-    private bool CanFitGCD(float deadline, int extraGCDs = 0) => GCD + GCDLength * extraGCDs < deadline; //Check if we can fit an additional GCD within the provided deadline
-    private AID ComboLastMove => (AID)World.Client.ComboState.Action; //Get the last action used in the combo sequence
-    private bool In3y(Actor? target) => Player.DistanceToHitbox(target) <= 2.9; //Check if the target is within melee range (3 yalms)
-    private bool In5y(Actor? target) => Player.DistanceToHitbox(target) <= 4.9; //Check if the target is within 5 yalms
-    private bool In20y(Actor? target) => Player.DistanceToHitbox(target) <= 19.9; //Check if the target is within 20 yalms
-    private bool In30y(Actor? target) => Player.DistanceToHitbox(target) <= 29.9; //Check if the target is within 30 yalms
-    private bool IsOffCooldown(AID aid) => World.Client.Cooldowns[ActionDefinitions.Instance.Spell(aid)!.MainCooldownGroup].Remaining < 0.6f; //Check if the desired action is ready (cooldown less than 0.6 seconds)
-    private int NumTargetsHitByAoE() => Hints.NumPriorityTargetsInAOECircle(Player.Position, 5); //Returns the number of targets hit by AoE within a 5-yalm radius around the player
-    public bool HasEffect(SID sid) => SelfStatusLeft(sid) > 0; //Check if the player has the specified status effect
-    public bool TargetHasEffect(SID sid, Actor? target) => StatusDetails(target, sid, Player.InstanceID, 1000).Left > 0; //Check if the target has the specified status effect
+    private float CD(AID aid) => World.Client.Cooldowns[ActionDefinitions.Instance.Spell(aid)!.MainCooldownGroup].Remaining;
+    private AID ComboLastMove => (AID)World.Client.ComboState.Action;
+    private bool In5y(Actor? target) => Player.DistanceToHitbox(target) <= 4.9;
+    private bool IsOffCooldown(AID aid) => World.Client.Cooldowns[ActionDefinitions.Instance.Spell(aid)!.MainCooldownGroup].Remaining < 0.6f;
+    public bool HasEffect(SID sid) => SelfStatusLeft(sid) > 0;
+    public bool TargetHasEffect(SID sid, Actor? target) => StatusDetails(target, sid, Player.InstanceID, 1000).Left > 0;
     public AID LimitBreak => HasEffect(SID.RelentlessRushPvP) ? AID.TerminalTriggerPvP : AID.RelentlessRushPvP;
     #endregion
 
-    public override void Execute(StrategyValues strategy, Actor? primaryTarget, float estimatedAnimLockDelay, bool isMoving) //Executes our actions
+    public override void Execute(StrategyValues strategy, Actor? primaryTarget, float estimatedAnimLockDelay, bool isMoving)
     {
         #region Variables
-        var gauge = World.Client.GetGauge<GunbreakerGauge>(); //Retrieve Gunbreaker gauge
+        var gauge = World.Client.GetGauge<GunbreakerGauge>();
         var GunStep = gauge.AmmoComboStep;
-
-        rdCD = CD(AID.RoughDividePvP); //Rough Divide cooldown (14s)
-        nmLeft = SelfStatusLeft(SID.NoMercyPvP, 7); //Remaining time for No Mercy buff (7s)
-        hasNM = nmLeft > 0; //Checks if No Mercy is active
-        hasBlast = HasEffect(SID.ReadyToBlastPvP); //Checks for Ready To Blast buff
-        hasRaze = HasEffect(SID.ReadyToRazePvP); //Checks for Ready To Raze buff
-        hasRip = HasEffect(SID.ReadyToRipPvP) || GunStep == 1; //Checks for Ready To Rip buff
-        hasTear = HasEffect(SID.ReadyToTearPvP) || GunStep == 2; //Checks for Ready To Tear buff
-        hasGouge = HasEffect(SID.ReadyToGougePvP); //Checks for Ready To Gouge buff
-        LBready = World.Party.LimitBreakLevel >= 1; //Checks if Limit Break is ready
-        //Misc
+        rdCD = CD(AID.RoughDividePvP);
+        nmLeft = SelfStatusLeft(SID.NoMercyPvP, 7);
+        hasNM = nmLeft > 0;
+        hasBlast = HasEffect(SID.ReadyToBlastPvP);
+        hasRaze = HasEffect(SID.ReadyToRazePvP);
+        hasRip = HasEffect(SID.ReadyToRipPvP) || GunStep == 1;
+        hasTear = HasEffect(SID.ReadyToTearPvP) || GunStep == 2;
+        hasGouge = HasEffect(SID.ReadyToGougePvP);
+        LBready = World.Party.LimitBreakLevel >= 1;
         GFcomboStep = ComboLastMove switch
         {
             AID.WickedTalonPvP => 3,
@@ -227,23 +212,19 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
         };
         inCombo = comboStep > 0;
         inGF = GFcomboStep > 0;
-        GCDLength = ActionSpeed.GCDRounded(World.Client.PlayerStats.SkillSpeed, World.Client.PlayerStats.Haste, Player.Level); //GCD based on skill speed and haste
-        NextGCD = AID.None; //Next global cooldown action to be used
-        NextGCDPrio = GCDPriority.None; //Priority of the next GCD, used for decision making on cooldowns
+        GCDLength = ActionSpeed.GCDRounded(World.Client.PlayerStats.SkillSpeed, World.Client.PlayerStats.Haste, Player.Level);
+        NextGCD = AID.None;
+        NextGCDPrio = GCDPriority.None;
 
         #region Minimal Requirements
-        can1 = ComboLastMove is AID.BurstStrikePvP; //KeenEdge conditions
-        can2 = ComboLastMove is AID.KeenEdgePvP; //BrutalShell conditions
-        can3 = ComboLastMove is AID.BrutalShellPvP; //SolidBarrel conditions
-        can4 = ComboLastMove is AID.SolidBarrelPvP; //BurstStrike conditions
-        canGF = IsOffCooldown(AID.GnashingFangPvP); //GnashingFang conditions
-        canFC = IsOffCooldown(AID.GnashingFangPvP); //FatedCircle conditions
-        canZone = IsOffCooldown(AID.BlastingZonePvP); //Zone conditions
-        canHyper = hasBlast && In5y(primaryTarget); //Hypervelocity conditions
-        canBrand = hasRaze && In5y(primaryTarget); //Fated Brand conditions
-        canRip = hasRip && In5y(primaryTarget); //Jugular Rip conditions
-        canTear = hasTear && In5y(primaryTarget); //Abdomen conditions
-        canGouge = hasGouge && In5y(primaryTarget); //Eye Gouge conditions
+        canGF = IsOffCooldown(AID.GnashingFangPvP);
+        canFC = IsOffCooldown(AID.GnashingFangPvP);
+        canZone = IsOffCooldown(AID.BlastingZonePvP);
+        canHyper = hasBlast && In5y(primaryTarget);
+        canBrand = hasRaze && In5y(primaryTarget);
+        canRip = hasRip && In5y(primaryTarget);
+        canTear = hasTear && In5y(primaryTarget);
+        canGouge = hasGouge && In5y(primaryTarget);
         #endregion
         #endregion
 
@@ -251,11 +232,10 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
         var burstStrategy = burst.As<BurstStrategy>();
         var hold = burstStrategy == BurstStrategy.Hold;
 
-        if (strategy.Option(Track.Combo).As<ComboStrategy>() == ComboStrategy.Force) //ST (without overcap protection)
+        if (strategy.Option(Track.Combo).As<ComboStrategy>() == ComboStrategy.Force)
             QueueGCD(NextCombo(), primaryTarget, GCDPriority.ForcedGCD);
 
         #region Rotation Execution
-        //Determine and queue combo actions
         if (!hold && !inGF)
             QueueGCD(NextCombo(), primaryTarget, GCDPriority.Combo);
 
@@ -299,14 +279,12 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
 
         #endregion
 
-        //Limit Break execution
         var lbStrat = strategy.Option(Track.LimitBreak).As<LimitBreakStrategy>();
         if (ShouldUseLimitBreak(lbStrat, primaryTarget))
             QueueOGCD(LimitBreak, primaryTarget, lbStrat == LimitBreakStrategy.Force ? OGCDPriority.ForcedOGCD : OGCDPriority.LB);
     }
 
     #region Core Execution Helpers
-    //QueueGCD execution
     private void QueueGCD(AID aid, Actor? target, GCDPriority prio)
     {
         if (prio != GCDPriority.None)
@@ -319,8 +297,6 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
             }
         }
     }
-
-    //QueueOGCD execution
     private void QueueOGCD(AID aid, Actor? target, OGCDPriority prio, float basePrio = ActionQueue.Priority.Medium)
     {
         if (prio != OGCDPriority.None)
@@ -328,22 +304,19 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
             Hints.ActionsToExecute.Push(ActionID.MakeSpell(aid), target, basePrio + (int)prio);
         }
     }
-
     #endregion
 
     #region Single-Target Helpers
-    private AID NextCombo() => ComboLastMove switch //Determines the next single-target action based on the last action used
+    private AID NextCombo() => ComboLastMove switch
     {
-        AID.SolidBarrelPvP => AID.BurstStrikePvP, //4, defaults back to 1
-        AID.BrutalShellPvP => AID.SolidBarrelPvP, //3
-        AID.KeenEdgePvP => AID.BrutalShellPvP, //2
-        _ => AID.KeenEdgePvP, //1
+        AID.SolidBarrelPvP => AID.BurstStrikePvP,
+        AID.BrutalShellPvP => AID.SolidBarrelPvP,
+        AID.KeenEdgePvP => AID.BrutalShellPvP,
+        _ => AID.KeenEdgePvP,
     };
     #endregion
 
     #region Cooldown Helpers
-
-    //Determines when to use No Mercy
     private bool ShouldUseRoughDivide(OffensiveStrategy strategy, Actor? target) => strategy switch
     {
         OffensiveStrategy.Automatic =>
@@ -354,7 +327,6 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
         _ => false
     };
 
-    //Determines when to use Zone
     private bool ShouldUseZone(OffensiveStrategy strategy, Actor? target) => strategy switch
     {
         OffensiveStrategy.Automatic =>
@@ -368,7 +340,6 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
         _ => false
     };
 
-    //Determines when to use Gnashing Fang
     private bool ShouldUseGnashingFang(OffensiveStrategy strategy, Actor? target) => strategy switch
     {
         OffensiveStrategy.Automatic =>
@@ -382,7 +353,6 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : RotationM
         _ => false
     };
 
-    //Determines when to use Fated Circle
     private bool ShouldUseFatedCircle(OffensiveStrategy strategy, Actor? target) => strategy switch
     {
         OffensiveStrategy.Automatic =>
