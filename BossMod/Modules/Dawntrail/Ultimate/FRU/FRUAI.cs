f7fed990c336a9e2aa82ee85fdd5439bd78bd03e
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-12-15 14:38:30 +0000

    FRU AI stuffs.

--- BossMod/Modules/Dawntrail/Ultimate/FRU/FRUAI.cs
+++ BossMod/Modules/Dawntrail/Ultimate/FRU/FRUAI.cs
@@ -1,5 +1,6 @@
 ﻿using BossMod.AI;
 using BossMod.Autorotation;
+using BossMod.Pathfinding;
 
 namespace BossMod.Dawntrail.Ultimate.FRU;
 
@@ -7,19 +8,22 @@ namespace BossMod.Dawntrail.Ultimate.FRU;
 sealed class FRUAI(RotationModuleManager manager, Actor player) : AIRotationModule(manager, player)
 {
     public enum Track { Movement }
-    public enum MovementStrategy { None, Prepull, DragToCenter, MaxMeleeNearest, ClockSpot }
+    public enum MovementStrategy { None, Pathfind, Prepull, DragToCenter, MaxMeleeNearest, ClockSpot }
 
     public static RotationModuleDefinition Definition()
     {
         var res = new RotationModuleDefinition("AI Experiment", "Experimental encounter-specific rotation", "Encounter AI", "veyn", RotationModuleQuality.WIP, new(~1ul), 100, 1, typeof(FRU));
         res.Define(Track.Movement).As<MovementStrategy>("Movement", "Movement")
             .AddOption(MovementStrategy.None, "None", "No automatic movement")
+            .AddOption(MovementStrategy.Pathfind, "Pathfind", "Use standard pathfinding to move")
             .AddOption(MovementStrategy.Prepull, "Prepull", "Pre-pull position: as close to the clock-spot as possible")
             .AddOption(MovementStrategy.DragToCenter, "DragToCenter", "Drag boss to the arena center")
             .AddOption(MovementStrategy.MaxMeleeNearest, "MaxMeleeNearest", "Move to nearest spot in max-melee")
             .AddOption(MovementStrategy.ClockSpot, "ClockSpot", "Move to role-based clock-spot");
         return res;
     }
+
+    private readonly FRUConfig _config = Service.Config.Get<FRUConfig>();
 
     public override void Execute(StrategyValues strategy, Actor? primaryTarget, float estimatedAnimLockDelay, bool isMoving)
     {
@@ -31,6 +35,7 @@ sealed class FRUAI(RotationModuleManager manager, Actor player) : AIRotationModu
 
     private WPos CalculateDestination(FRU module, Actor? primaryTarget, MovementStrategy strategy, PartyRolesConfig.Assignment assignment) => strategy switch
     {
+        MovementStrategy.Pathfind => PathfindPosition(),
         MovementStrategy.Prepull => PrepullPosition(module, assignment),
         MovementStrategy.DragToCenter => DragToCenterPosition(module),
         MovementStrategy.MaxMeleeNearest => primaryTarget != null ? primaryTarget.Position + 7.5f * (Player.Position - primaryTarget.Position).Normalized() : Player.Position,
@@ -38,12 +43,25 @@ sealed class FRUAI(RotationModuleManager manager, Actor player) : AIRotationModu
         _ => Player.Position
     };
 
-    // assumption: pull range is 12; hitbox is 5, so maxmelee is 8, meaning we have approx 4m to move during pull - with sprint, speed is 7.8, accel is 30 => over 0.26s accel period we move 1.014m, then need another 0.38s to reach boss (but it also moves)
-    // TODO: R1/R2 can stand close to clockspots, H1/H2 can stand directly on clockspots
-    private WPos PrepullPosition(FRU module, PartyRolesConfig.Assignment assignment) => assignment switch
+    // TODO: account for leeway for casters
+    private WPos PathfindPosition()
     {
-        _ => module.PrimaryActor.Position + new WDir(0, 12.5f)
-    };
+        var res = NavigationDecision.Build(NavigationContext, World, Hints, Player, Speed());
+        return res.Destination ?? Player.Position;
+    }
+
+    // assumption: pull range is 12; hitbox is 5, so maxmelee is 8, meaning we have approx 4m to move during pull - with sprint, speed is 7.8, accel is 30 => over 0.26s accel period we move 1.014m, then need another 0.38s to reach boss (but it also moves)
+    private WPos PrepullPosition(FRU module, PartyRolesConfig.Assignment assignment)
+    {
+        var safeRange = 12.5f;
+        var pos = ClockSpotPosition(module, assignment, assignment is PartyRolesConfig.Assignment.MT or PartyRolesConfig.Assignment.MT or PartyRolesConfig.Assignment.M1 or PartyRolesConfig.Assignment.M2 ? 5 : 10);
+        var off = pos - module.PrimaryActor.Position;
+        var distSq = off.LengthSq();
+        if (distSq >= safeRange * safeRange)
+            return pos;
+        off /= MathF.Sqrt(distSq);
+        return module.PrimaryActor.Position + off * safeRange;
+    }
 
     // notes on boss movement: boss top speed is ~8.5m, it moves up to distance 7.5m (both hitboxes + 2m)
     // empyrically, if i stand still, i can start moving when boss is ~11m away and it will still be dragged to intended spot
@@ -59,16 +77,9 @@ sealed class FRUAI(RotationModuleManager manager, Actor player) : AIRotationModu
         return GCD > timeToMelee + 0.1f ? dragSpot : module.PrimaryActor.Position + meleeDistance * dragDir;
     }
 
-    private WPos ClockSpotPosition(FRU module, PartyRolesConfig.Assignment assignment, float range) => assignment switch
+    private WPos ClockSpotPosition(FRU module, PartyRolesConfig.Assignment assignment, float range)
     {
-        PartyRolesConfig.Assignment.MT => module.Center + range * 180.Degrees().ToDirection(),
-        PartyRolesConfig.Assignment.OT => module.Center + range * 0.Degrees().ToDirection(),
-        PartyRolesConfig.Assignment.H1 => module.Center + range * (-90).Degrees().ToDirection(),
-        PartyRolesConfig.Assignment.H2 => module.Center + range * 90.Degrees().ToDirection(),
-        PartyRolesConfig.Assignment.M1 => module.Center + range * (-45).Degrees().ToDirection(),
-        PartyRolesConfig.Assignment.M2 => module.Center + range * 45.Degrees().ToDirection(),
-        PartyRolesConfig.Assignment.R1 => module.Center + range * (-135).Degrees().ToDirection(),
-        PartyRolesConfig.Assignment.R2 => module.Center + range * 135.Degrees().ToDirection(),
-        _ => Player.Position
-    };
+        var dir = _config.P1CyclonicBreakSpots[assignment];
+        return dir >= 0 ? module.Center + range * (180 - 45 * dir).Degrees().ToDirection() : Player.Position;
+    }
 }
