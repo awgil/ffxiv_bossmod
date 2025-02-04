cbc864a0fdb0433b152d63a6ee841cc00dd7d3f2
ace <aceakechi6@gmail.com>
2025-02-04 13:55:50 -0800

    NIN cleanup

--- BossMod/Autorotation/Utility/ClassNINUtility.cs
+++ BossMod/Autorotation/Utility/ClassNINUtility.cs
@@ -16,7 +16,7 @@ public sealed class ClassNINUtility(RotationModuleManager manager, Actor player)
 
         res.Define(Track.Shukuchi).As<DashStrategy>("Shukuchi", "Dash", 20)
             .AddOption(DashStrategy.None, "Automatic", "No use.")
-            .AddOption(DashStrategy.GapClose, "GapClose", "Use as gapcloser if outside melee range", 60, 0, ActionTargets.Hostile, 45)
+            .AddOption(DashStrategy.GapClose, "GapClose", "Use as gapcloser if outside melee range", 60, 0, ActionTargets.Party | ActionTargets.Hostile, 45)
             .AddOption(DashStrategy.GapCloseHold1, "GapCloseHold1", "Use as gapcloser if outside melee range; conserves 1 charge for manual usage", 60, 0, ActionTargets.Hostile, 74)
             .AddAssociatedActions(NIN.AID.Shukuchi);
 
@@ -29,6 +29,8 @@ public sealed class ClassNINUtility(RotationModuleManager manager, Actor player)
         ExecuteSimple(strategy.Option(Track.ShadeShift), NIN.AID.ShadeShift, Player);
 
         // TODO: revise, this doesn't look correct (shukuchi is area targeted, so it should use that; probably should expose options to use regardless of melee distance...)
+        //These should work with all Area targeting options? Able to target Waymarks, Center, AbsolutePoint, etc.
+        //Figure out how to make compatible with WPos
         var dash = strategy.Option(Track.Shukuchi);
         var dashStrategy = strategy.Option(Track.Shukuchi).As<DashStrategy>();
         var dashTarget = ResolveTargetOverride(dash.Value) ?? primaryTarget; //Smart-Targeting
@@ -38,10 +40,10 @@ public sealed class ClassNINUtility(RotationModuleManager manager, Actor player)
         {
             DashStrategy.None => false,
             DashStrategy.GapClose => distance is > 3f and <= 20f,
-            DashStrategy.GapCloseHold1 => distance is > 3f and <= 20f && cd <= 60.5f, // TODO: this condition doesn't look correct...
+            DashStrategy.GapCloseHold1 => distance is > 3f and <= 20f && cd < 0.6f,
             _ => false,
         };
         if (shouldDash && dashTarget != null)
-            Hints.ActionsToExecute.Push(ActionID.MakeSpell(NIN.AID.Shukuchi), null, dash.Priority(), dash.Value.ExpireIn, 0, 0, dashTarget.PosRot.XYZ());
+            Hints.ActionsToExecute.Push(ActionID.MakeSpell(NIN.AID.Shukuchi), null, dash.Priority(), dash.Value.ExpireIn, targetPos: dashTarget.PosRot.XYZ());
     }
 }
