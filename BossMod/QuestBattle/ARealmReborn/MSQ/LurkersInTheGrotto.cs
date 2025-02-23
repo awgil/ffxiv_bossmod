5c89c18570410ab643229370200ff5deaeb4c566
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-02-23 12:46:22 -0500

    lurks u

---
+++ BossMod/QuestBattle/ARealmReborn/MSQ/LurkersInTheGrotto.cs
@@ -0,0 +1,11 @@
+﻿namespace BossMod.QuestBattle.ARealmReborn.MSQ;
+
+[ZoneModuleInfo(BossModuleInfo.Maturity.Contributed, 332)]
+public class LurkersInTheGrotto(WorldState ws) : QuestBattle(ws)
+{
+    public override void CalculateAIHints(int playerSlot, Actor player, AIHints hints)
+    {
+        foreach (var p in hints.PotentialTargets)
+            p.Priority = p.Actor.OID == 0x386 ? 0 : 1;
+    }
+}
