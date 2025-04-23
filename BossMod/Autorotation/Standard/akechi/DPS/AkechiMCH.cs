7150c00fbfb4d71a46d5408a7373788d5de738a8
ace <167795370+Akechi-kun@users.noreply.github.com>
2025-04-22 22:44:31 -0700

    1-2-3 improvements

--- BossMod/Autorotation/Standard/akechi/DPS/AkechiMCH.cs
+++ BossMod/Autorotation/Standard/akechi/DPS/AkechiMCH.cs
@@ -449,7 +449,7 @@ public sealed class AkechiMCH(RotationModuleManager manager, Actor player) : Ake
         (BestSplashTargets, NumSplashTargets) = GetBestTarget(primaryTarget, 25, IsSplashTarget);
         (BestChainsawTargets, NumChainsawTargets) = GetBestTarget(primaryTarget, 25, Is25yRectTarget);
         NumFlamethrowerTargets = Hints.NumPriorityTargetsInAOECone(Player.Position, 12, Player.Rotation.ToDirection(), 45.Degrees());
-        ShouldUseAOE = Unlocked(AID.SpreadShot) && NumConeTargets > 1;
+        ShouldUseAOE = Unlocked(AID.SpreadShot) && (ComboLastMove is AID.HeatedSlugShot ? NumConeTargets > 3 : ComboLastMove is AID.HeatedSplitShot ? NumConeTargets > 2 : NumConeTargets > 1);
         ShouldUseRangedAOE = Unlocked(AID.Ricochet) && NumSplashTargets > 1;
         ShouldUseSaw = Unlocked(AID.ChainSaw) && NumChainsawTargets > 1;
         ShouldFlamethrower = Unlocked(AID.Flamethrower) && NumFlamethrowerTargets > 2;
