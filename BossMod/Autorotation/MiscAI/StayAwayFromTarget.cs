7d27ed86e5f7140a0651c9ee545351fad1edccb0
Erisen <203582669+Erisen8974@users.noreply.github.com>
2025-05-03 15:25:07 -0400

    Module to stay out of the targets hitbox

---
+++ BossMod/Autorotation/MiscAI/StayAwayFromTarget.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace BossMod.Autorotation.MiscAI;
+
+public sealed class StayAwayFromTarget(RotationModuleManager manager, Actor player) : RotationModule(manager, player)
+{
+    public enum Tracks
+    {
+        Distance
+    }
+
+    public enum DistanceDefinition
+    {
+    }
+
+    public static RotationModuleDefinition Definition()
+    {
+        RotationModuleDefinition def = new("Misc AI: Stay away from target", "Preset module to maintain distance from target and avoid dodging to the center pixel.", "AI", "Erisen", RotationModuleQuality.Basic, new(~0ul), 100);
+
+        var configRef = def.Define(Tracks.Distance).As<DistanceDefinition>("Distance");
+
+        for (float f = 0; f <= 5f; f = MathF.Round(f + 0.1f, 1))
+        {
+            configRef.AddOption((DistanceDefinition)(f * 10f), f.ToString(CultureInfo.InvariantCulture));
+        }
+
+        return def;
+    }
+
+    public override void Execute(StrategyValues strategy, ref Actor? primaryTarget, float estimatedAnimLockDelay, bool isMoving)
+    {
+        if (primaryTarget != null)
+        {
+            var realDistance = strategy.Option(Tracks.Distance).Value.Option / 10f + primaryTarget.HitboxRadius;
+            var sqDist = 4 * realDistance * realDistance; // 4x to make .25 prio
+            var tagetPosition = primaryTarget.Position;
+            // No hard cut because this can conflict with stay close to target.
+            // by assigning a smaller scaling amount to the middle it will still attempt to get near the edge.
+            Hints.GoalZones.Add(p => MathF.Min(.25f, (tagetPosition - p).LengthSq() / sqDist));
+        }
+    }
+}
