36891b0dbe73ccd1a39d6b6441619cc75bb2a0ff
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-05-08 14:09:27 -0400

    keeping it around just for the heck of it

--- BossMod/Modules/Dawntrail/Dungeon/D10Underkeep/D103ValiaPira.cs
+++ BossMod/Modules/Dawntrail/Dungeon/D10Underkeep/D103ValiaPira.cs
@@ -4,29 +4,29 @@ public enum OID : uint
 {
     Boss = 0x478E,
     Helper = 0x233C,
-    _Gen_ = 0x47B5, // R1.000, x9
-    RedExplodey = 0x478F, // R1.600, x6
-    _Gen_CoordinateBit = 0x4789, // R1.000, x2
-    _Gen_CoordinateTurret = 0x4793, // R1.000, x4
+    ExplodingOrb = 0x478F, // R1.600, x6
+    TetherSource = 0x47B5, // R1.000, x9
+    CoordinateBit = 0x4789, // R1.000, x2
+    CoordinateTurret = 0x4793, // R1.000, x4
 }
 
 public enum AID : uint
 {
-    _AutoAttack_ = 42526, // Boss->player, no cast, single-target
-    _Spell_EntropicSphere = 42525, // Boss->self, 5.0s cast, range 40 circle
-    _Ability_CoordinateMarch = 42513, // Boss->self, 4.0s cast, single-target
-    Collision = 42514, // 478F->4789/47B5, no cast, single-target
-    _Ability_EnforcementRay = 42737, // 4789->self, 0.5s cast, range 36 width 9 cross
-    _Ability_OrderedFire = 42508, // Boss->self, no cast, single-target
-    _Ability_OrderedFire2 = 42509, // Helper->Boss, 1.0s cast, single-target
-    _Ability_Electray = 43130, // 4793->self, 7.0s cast, range 40 width 9 rect
-    _Spell_ElectricField = 42519, // Boss->self, 6.6+0.7s cast, single-target
-    _Spell_ElectricField1 = 43261, // Helper->self, no cast, range 26 50-degree cone
-    _Spell_ConcurrentField = 42521, // Helper->self, 7.3s cast, range 26 50-degree cone
-    _Ability_NeutralizeFrontLines = 42738, // Boss->self, 5.0s cast, range 30 180-degree cone
-    _Spell_HyperchargedLight = 42524, // Helper->player, 5.0s cast, range 5 circle
-    _Ability_Bloodmarch = 42739, // Boss->self, 5.0s cast, single-target
-    _Ability_DeterrentPulse = 42540, // Boss->self/players, 5.0s cast, range 40 width 8 rect
+    AutoAttack = 42526, // Boss->player, no cast, single-target
+    EntropicSphere = 42525, // Boss->self, 5.0s cast, range 40 circle
+    CoordinateMarch = 42513, // Boss->self, 4.0s cast, single-target
+    Collision = 42514, // ExplodingOrb->CoordinateBit/TetherSource, no cast, single-target
+    EnforcementRay = 42737, // CoordinateBit->self, 0.5s cast, range 36 width 9 cross
+    OrderedFire = 42508, // Boss->self, no cast, single-target
+    OrderedFire2 = 42509, // Helper->Boss, 1.0s cast, single-target
+    Electray = 43130, // CoordinateTurret->self, 7.0s cast, range 40 width 9 rect
+    ElectricFieldVisual = 42519, // Boss->self, 6.6+0.7s cast, single-target
+    ElectricFieldSpread = 43261, // Helper->self, no cast, range 26 ?-degree cone
+    ConcurrentField = 42521, // Helper->self, 7.3s cast, range 26 ?-degree cone
+    NeutralizeFrontLines = 42738, // Boss->self, 5.0s cast, range 30 180-degree cone
+    HyperchargedLight = 42524, // Helper->player, 5.0s cast, range 5 circle
+    Bloodmarch = 42739, // Boss->self, 5.0s cast, single-target
+    DeterrentPulse = 42540, // Boss->self/players, 5.0s cast, range 40 width 8 rect
 }
 
 public enum IconID : uint
@@ -37,17 +37,17 @@ public enum IconID : uint
 
 public enum TetherID : uint
 {
-    _Gen_Tether_322 = 322, // _Gen_->Boss
-    _Gen_Tether_321 = 321, // _Gen_->Boss
-    OrbTeleport = 282, // _Gen_->_Gen_1
+    OrbTether = 322, // TetherSource->Boss
+    BitTether = 321, // TetherSource->Boss
+    OrbTeleport = 282, // TetherSource->ExplodingOrb
 }
 
-class EntropicSphere(BossModule module) : Components.RaidwideCast(module, AID._Spell_EntropicSphere);
-class Electray(BossModule module) : Components.StandardAOEs(module, AID._Ability_Electray, new AOEShapeRect(40, 4.5f));
-class ConcurrentField(BossModule module) : Components.StandardAOEs(module, AID._Spell_ConcurrentField, new AOEShapeCone(26, 25.Degrees()));
-class ElectricField(BossModule module) : Components.BaitAwayIcon(module, new AOEShapeCone(26, 25.Degrees()), (uint)IconID.ElectricField, AID._Spell_ElectricField1, 7.4f);
-class NeutralizeFrontLines(BossModule module) : Components.StandardAOEs(module, AID._Ability_NeutralizeFrontLines, new AOEShapeCone(30, 90.Degrees()));
-class DeterrentPulse(BossModule module) : Components.GenericWildCharge(module, 4, AID._Ability_DeterrentPulse, 40)
+class EntropicSphere(BossModule module) : Components.RaidwideCast(module, AID.EntropicSphere);
+class Electray(BossModule module) : Components.StandardAOEs(module, AID.Electray, new AOEShapeRect(40, 4.5f));
+class ConcurrentField(BossModule module) : Components.StandardAOEs(module, AID.ConcurrentField, new AOEShapeCone(26, 25.Degrees()));
+class ElectricField(BossModule module) : Components.BaitAwayIcon(module, new AOEShapeCone(26, 25.Degrees()), (uint)IconID.ElectricField, AID.ElectricFieldSpread, 7.4f);
+class NeutralizeFrontLines(BossModule module) : Components.StandardAOEs(module, AID.NeutralizeFrontLines, new AOEShapeCone(30, 90.Degrees()));
+class DeterrentPulse(BossModule module) : Components.GenericWildCharge(module, 4, AID.DeterrentPulse, 40)
 {
     public override void OnEventIcon(Actor actor, uint iconID, ulong targetID)
     {
@@ -70,16 +70,195 @@ class DeterrentPulse(BossModule module) : Components.GenericWildCharge(module, 4
         }
     }
 }
-class HyperchargedLight(BossModule module) : Components.SpreadFromCastTargets(module, AID._Spell_HyperchargedLight, 5);
+class HyperchargedLight(BossModule module) : Components.SpreadFromCastTargets(module, AID.HyperchargedLight, 5);
 
-class Coordinates(BossModule module) : Components.GenericAOEs(module)
+class CoordinateMarch(BossModule module) : Components.GenericAOEs(module, AID.EnforcementRay)
 {
-    private readonly List<Actor> _orbs = [];
-    private readonly List<(Actor From, Actor To)> _links = [];
+    public int BossCasts;
 
-    public readonly List<(int Tile, DateTime Activation)> Crosses = [];
-    record struct Bit(Actor Actor, DateTime MoveStart, List<int> Path);
-    private readonly List<Bit> _bits = [];
+    private readonly List<Actor> _orbs = [];
+    private readonly List<(Actor Bit, WPos Spawn, DateTime MoveStart)> _bits = [];
+    private readonly List<(WPos Source, DateTime Activation)> _crosses = [];
+
+    private SourceSide _sourceSide;
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor) => _crosses.Take(1).Select(c => new AOEInstance(new AOEShapeCross(36, 4.5f), c.Source, Activation: c.Activation));
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.CoordinateMarch)
+            BossCasts++;
+    }
+
+    public override void OnTethered(Actor source, ActorTetherInfo tether)
+    {
+        if ((TetherID)tether.ID == TetherID.OrbTeleport)
+        {
+            _orbs.Add(source);
+            _orbs.RemoveAll(o => o.InstanceID == tether.Target);
+            Predict();
+        }
+    }
+
+    private WPos ClampToArena(WPos p) => new(Math.Clamp(p.X, Arena.Center.X - 17.5f, Arena.Center.X + 17.5f), Math.Clamp(p.Z, Arena.Center.Z - 17.5f, Arena.Center.Z + 17.5f));
+
+    public override void OnActorPlayActionTimelineEvent(Actor actor, ushort id)
+    {
+        switch (((OID)actor.OID, id))
+        {
+            case (OID.ExplodingOrb, 0x11DA):
+                _orbs.Add(actor);
+                break;
+            case (OID.CoordinateBit, 0x11D5):
+                if (_sourceSide == SourceSide.None)
+                    _sourceSide = actor.Position.Z < -347 ? SourceSide.North : SourceSide.East;
+                _bits.Add((actor, ClampToArena(actor.Position), default));
+                break;
+            case (OID.CoordinateBit, 0x24E1):
+                _bits.RemoveAll(b => b.Bit == actor);
+                break;
+        }
+    }
+
+    public override void OnStatusGain(Actor actor, ActorStatus status)
+    {
+        if ((OID)actor.OID == OID.CoordinateBit && status.ID == 2056)
+        {
+            var bit = _bits.FindIndex(b => b.Bit == actor);
+            if (bit >= 0)
+            {
+                _bits.Ref(bit).MoveStart = WorldState.CurrentTime;
+                Predict();
+            }
+        }
+    }
+
+    enum SourceSide
+    {
+        None,
+        North,
+        East
+    }
+
+    // arrays of tile indices, indexed by starting tile
+    public static readonly byte[][] PathsFirstN = [
+        [], [1,5,9,8,12], [2,6,10,11,15], [],
+        [],[],[],[],
+        [],[],[],[],
+        [],[],[],[]
+    ];
+    public static readonly byte[][] PathsFirstE = [
+        [],[],[],[],
+        [],[],[], Rotate(PathsFirstN[1]),
+        [],[],[], Rotate(PathsFirstN[2]),
+        [],[],[],[]
+    ];
+    public static readonly byte[][] PathsRestN = [
+        [0,1,5,4,8,9,13,12], [1,0,4,5,9,8,12,13], [2,3,7,6,10,11,15,14], [3,2,6,7,11,10,14,15],
+        [],[],[],[],
+        [],[],[],[],
+        [],[],[],[]
+    ];
+    public static readonly byte[][] PathsRestE = [
+        [],[],[],Rotate(PathsRestN[0]),
+        [],[],[],Rotate(PathsRestN[1]),
+        [],[],[],Rotate(PathsRestN[2]),
+        [],[],[],Rotate(PathsRestN[3])
+    ];
+
+    private static byte[] Rotate(byte[] tiles)
+    {
+        var ret = new byte[tiles.Length];
+        for (var i = 0; i < tiles.Length; i++)
+        {
+            var t = tiles[i];
+            ret[i] = (byte)(3 - (t / 4) + (4 * (t % 4)));
+        }
+        return ret;
+    }
+
+    private byte FindTile(WPos p)
+    {
+        var off = (p - Arena.Center) / 9f + new WDir(2, 2);
+        var col = (int)off.X;
+        var row = (int)off.Z;
+        return (byte)(row * 4 + col);
+    }
+
+    private void Predict()
+    {
+        _crosses.Clear();
+
+        if (_bits.Any(b => b.MoveStart == default))
+            // second bit hasn't moved yet, we need to use that timestamp to predict activations
+            return;
+
+        var collisionCounter = new int[16]; // indexed by path
+
+        const float travelBase = 1.75f; // estimate
+
+        var curPaths = (BossCasts, _sourceSide) switch
+        {
+            ( <= 1, SourceSide.North) => PathsFirstN,
+            ( <= 1, SourceSide.East) => PathsFirstE,
+            _ => _sourceSide == SourceSide.North ? PathsRestN : PathsRestE
+        };
+
+        List<(Actor orb, Actor bit, DateTime bitSpawn, int path, int distance)> collisions = [];
+
+        foreach (var o in _orbs)
+        {
+            var thisTile = FindTile(o.Position);
+            var thisBit = _bits.First(b => curPaths[FindTile(b.Spawn)].Contains(thisTile));
+            var thisPathIx = FindTile(thisBit.Spawn);
+
+            var dist = Array.IndexOf(curPaths[thisPathIx], thisTile);
+            collisions.Add((o, thisBit.Bit, thisBit.MoveStart, thisPathIx, dist));
+        }
+
+        collisions.SortBy(c => c.distance);
+
+        foreach (var coll in collisions)
+        {
+            var travelTime = travelBase;
+            travelTime += coll.distance * 2; // bits travel about half a tile per second
+            travelTime += collisionCounter[coll.path] * 2.5f; // bits pause about 2.5s during collision
+
+            _crosses.Add((coll.orb.Position, coll.bitSpawn.AddSeconds(travelTime)));
+            collisionCounter[coll.path]++;
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID == AID.Collision)
+        {
+            _sourceSide = SourceSide.None;
+            _orbs.RemoveAll(o => o.Position.AlmostEqual(caster.Position, 1));
+        }
+
+        if (spell.Action == WatchedAction)
+        {
+            NumCasts++;
+            var ix = _crosses.FindIndex(c => c.Source.AlmostEqual(caster.Position, 1));
+            if (ix >= 0)
+            {
+#if DEBUG
+                var cross = _crosses[ix];
+                Service.Log($"predicted activation {cross.Activation}, actual activation {WorldState.CurrentTime}, difference {(cross.Activation - WorldState.CurrentTime).TotalSeconds:f3}");
+#endif
+                _crosses.RemoveAt(ix);
+            }
+        }
+    }
+}
+
+class Debugger(BossModule module) : BossComponent(module)
+{
+#if DEBUG
+    private readonly List<Actor> _orbs = [];
+    private readonly List<Actor> _bits = [];
+    private readonly List<(Actor From, Actor To)> _links = [];
 
     [Flags]
     public enum Tile
@@ -94,8 +273,6 @@ class Coordinates(BossModule module) : Components.GenericAOEs(module)
     // starting at top left from 0
     // bit 0 = N, bit 1 = E, bit 2 = S, bit 3 = W
     public readonly Tile[] Tiles = new Tile[16];
-
-    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor) => Crosses.Take(1).Select(c => new AOEInstance(new AOEShapeCross(36, 4.5f), TileCenter(c.Tile), default, c.Activation));
 
     public override void OnEventEnvControl(byte index, uint state)
     {
@@ -125,157 +302,31 @@ class Coordinates(BossModule module) : Components.GenericAOEs(module)
     {
         switch (((OID)actor.OID, id))
         {
-            case (OID.RedExplodey, 0x11DA):
+            case (OID.ExplodingOrb, 0x11DA):
                 _orbs.Add(actor);
                 _links.RemoveAll(t => t.From.Position.AlmostEqual(actor.Position, 0.1f));
                 break;
-            case (OID._Gen_CoordinateBit, 0x11D5):
-                _bits.Add(new(actor, default, FindPath(actor)));
+            case (OID.CoordinateBit, 0x11D5):
+                _bits.Add(actor);
                 break;
-            case (OID._Gen_CoordinateBit, 0x24E1):
-                _bits.RemoveAll(b => b.Actor == actor);
+            case (OID.CoordinateBit, 0x24E1):
+                _bits.Remove(actor);
                 break;
         }
-    }
-
-    private List<int> FindPath(Actor bit)
-    {
-        var t1 = FindTile(bit.Position);
-        List<int> path = [];
-        Tile prevConn;
-        if (bit.Position.Z < Arena.Center.Z - 17.5f)
-            prevConn = Tile.N;
-        else if (bit.Position.X < Arena.Center.X - 17.5f)
-            prevConn = Tile.W;
-        else if (bit.Position.X > Arena.Center.X + 17.5f)
-            prevConn = Tile.E;
-        else if (bit.Position.Z > Arena.Center.Z + 17.5f)
-            prevConn = Tile.S;
-        else
-        {
-            prevConn = Tile.N | Tile.E | Tile.S | Tile.W;
-            ReportError("don't know connection");
-        }
-
-        while (t1 >= 0)
-        {
-            if (path.Contains(t1))
-            {
-                ReportError($"loop in path at tile {t1}");
-                return [];
-            }
-            path.Add(t1);
-            var (t2, nc2) = NextTile(t1, prevConn);
-            prevConn = nc2;
-            t1 = t2;
-        }
-
-        return path;
-    }
-
-    private (int, Tile) NextTile(int prevTile, Tile prevDirection)
-    {
-        var t = Tiles[prevTile];
-        t &= ~prevDirection;
-        if (t.HasFlag(Tile.N) && prevTile > 3)
-            return (prevTile - 4, Tile.S);
-        if (t.HasFlag(Tile.E) && prevTile % 4 != 3)
-            return (prevTile + 1, Tile.W);
-        if (t.HasFlag(Tile.W) && prevTile % 4 != 0)
-            return (prevTile - 1, Tile.E);
-        if (t.HasFlag(Tile.S) && prevTile < 12)
-            return (prevTile + 4, Tile.N);
-        return (-1, default);
-    }
-
-    private int FindTile(WPos p)
-    {
-        var off = (p - Arena.Center) / 9f + new WDir(2, 2);
-        var col = (int)off.X;
-        var row = (int)off.Z;
-        return row * 4 + col;
-    }
-
-    public override void Update()
-    {
-        var update = false;
-        for (var i = 0; i < _bits.Count; i++)
-        {
-            if (_bits[i].MoveStart == default && _bits[i].Actor.LastFrameMovement != default)
-            {
-                _bits.Ref(i).MoveStart = WorldState.CurrentTime;
-                update = true;
-            }
-        }
-
-        if (update && _bits.All(b => b.MoveStart != default))
-            UpdatePredictions();
-    }
-
-    private void UpdatePredictions()
-    {
-        Crosses.Clear();
-        var orbTiles = new BitMask();
-        foreach (var (f, _) in _links)
-            orbTiles.Set(FindTile(f.Position));
-
-        foreach (var o in _orbs)
-        {
-            if (_links.Any(l => l.To == o))
-                continue;
-            orbTiles.Set(FindTile(o.Position));
-        }
-
-        for (var i = 0; i < _bits.Count; i++)
-        {
-            var bit = _bits[i];
-            var travelTime = 0.5f;
-            foreach (var p in bit.Path)
-            {
-                travelTime += 1f;
-                if (orbTiles[p])
-                {
-                    // estimated 2 seconds of being stationary during AOE activation, plus ~0.5s of acceleration
-                    Crosses.Add((p, bit.MoveStart.AddSeconds(travelTime)));
-                    travelTime += 2.5f;
-                }
-                travelTime += 1f;
-            }
-        }
-
-        Crosses.SortBy(c => c.Activation);
-    }
-
-    public override void OnEventCast(Actor caster, ActorCastEvent spell)
-    {
-        if ((AID)spell.Action.ID == AID.Collision)
-        {
-            _orbs.Remove(caster);
-        }
-
-        if ((AID)spell.Action.ID == AID._Ability_EnforcementRay)
-            Crosses.RemoveAll(c => c.Tile == FindTile(caster.Position));
     }
 
     public override void OnTethered(Actor source, ActorTetherInfo tether)
     {
         if ((TetherID)tether.ID == TetherID.OrbTeleport)
-        {
             _links.Add((source, WorldState.Actors.Find(tether.Target)!));
-            if (_bits.All(b => b.MoveStart != default))
-                UpdatePredictions();
-        }
     }
 
-    private WPos TileCenter(int tile)
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
     {
-        var col = tile % 4;
-        var row = tile / 4;
-        var off = new WDir(-13.5f, -13.5f) + new WDir(9 * col, 9 * row);
-        return Arena.Center + off;
+        if ((AID)spell.Action.ID == AID.Collision)
+            _orbs.Remove(caster);
     }
 
-#if DEBUG
     public override void DrawArenaForeground(int pcSlot, Actor pc)
     {
         for (var i = 0; i < 16; i++)
@@ -299,15 +350,18 @@ class Coordinates(BossModule module) : Components.GenericAOEs(module)
         }
 
         foreach (var b in _bits)
-            Arena.ZoneRect(b.Actor.Position, new WDir(0, 1), 1, 1, 1, 0xFFFF0080);
+            Arena.ZoneRect(b.Position, new WDir(0, 1), 1, 1, 1, 0xFFFF0080);
 
         foreach (var b in _orbs)
-            Arena.ZoneRect(b.Position, new WDir(0, 1), 1, 1, 1, 0xFF8000FF);
+            Arena.ZoneCircle(b.Position, 1, 0xFF8000FF);
     }
 
-    public override void AddGlobalHints(GlobalHints hints)
+    private WPos TileCenter(int tile)
     {
-        hints.Add(string.Join(", ", Crosses.Select(a => $"{a.Tile} @ +{(a.Activation - WorldState.CurrentTime).TotalSeconds:f3}")));
+        var col = tile % 4;
+        var row = tile / 4;
+        var off = new WDir(-13.5f, -13.5f) + new WDir(9 * col, 9 * row);
+        return Arena.Center + off;
     }
 #endif
 }
@@ -324,10 +378,11 @@ class D103ValiaPiraStates : StateMachineBuilder
             .ActivateOnEnter<NeutralizeFrontLines>()
             .ActivateOnEnter<DeterrentPulse>()
             .ActivateOnEnter<HyperchargedLight>()
-            .ActivateOnEnter<Coordinates>();
+            .ActivateOnEnter<CoordinateMarch>()
+            .ActivateOnEnter<Debugger>();
     }
 }
 
-[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1027, NameID = 13749)]
+[ModuleInfo(BossModuleInfo.Maturity.Verified, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1027, NameID = 13749)]
 public class D103ValiaPira(WorldState ws, Actor primary) : BossModule(ws, primary, new(0, -331), new ArenaBoundsSquare(17.5f));
 
