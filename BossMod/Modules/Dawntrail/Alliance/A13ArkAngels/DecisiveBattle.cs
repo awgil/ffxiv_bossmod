65d163f9ca0939d08a6c74f5662dad99e493a8e4
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-05-14 13:15:04 -0400

    jeuno: use status instead of tether (not everyone gets a tether???)

--- BossMod/Modules/Dawntrail/Alliance/A13ArkAngels/DecisiveBattle.cs
+++ BossMod/Modules/Dawntrail/Alliance/A13ArkAngels/DecisiveBattle.cs
@@ -2,31 +2,83 @@
 
 class DecisiveBattle(BossModule module) : BossComponent(module)
 {
-    private readonly Actor?[] _assignedBoss = new Actor?[PartyState.MaxAllianceSize];
+    enum Buff
+    {
+        None,
+        Epic,
+        Fated,
+        Vaunted
+    }
+
+    private readonly Actor?[] _bosses = new Actor?[4];
+    private readonly Buff[] _buffs = new Buff[PartyState.MaxAllianceSize];
+
+    public override void OnStatusGain(Actor actor, ActorStatus status)
+    {
+        var slot = Raid.FindSlot(actor.InstanceID);
+
+        switch ((SID)status.ID)
+        {
+            case SID.EpicHero:
+                if (slot >= 0)
+                    _buffs[slot] = Buff.Epic;
+                break;
+            case SID.FatedHero:
+                if (slot >= 0)
+                    _buffs[slot] = Buff.Fated;
+                break;
+            case SID.VauntedHero:
+                if (slot >= 0)
+                    _buffs[slot] = Buff.Vaunted;
+                break;
+            case SID.EpicVillain:
+                _bosses[(int)Buff.Epic] = actor;
+                break;
+            case SID.FatedVillain:
+                _bosses[(int)Buff.Fated] = actor;
+                break;
+            case SID.VauntedVillain:
+                _bosses[(int)Buff.Vaunted] = actor;
+                break;
+        }
+    }
+
+    public override void OnStatusLose(Actor actor, ActorStatus status)
+    {
+        switch ((SID)status.ID)
+        {
+            case SID.EpicHero:
+            case SID.FatedHero:
+            case SID.VauntedHero:
+                var slot = Raid.FindSlot(actor.InstanceID);
+                if (slot >= 0)
+                    _buffs[slot] = Buff.None;
+                break;
+        }
+    }
 
     public override void AddHints(int slot, Actor actor, TextHints hints)
     {
-        if (slot < _assignedBoss.Length && _assignedBoss[slot] != null)
+        var assigned = AssignedBoss(slot);
+        if (assigned != null)
         {
             var target = WorldState.Actors.Find(actor.TargetID);
-            if (target != null && target != _assignedBoss[slot] && (OID)target.OID is OID.BossMR or OID.BossTT or OID.BossGK)
+            if (target != null && !target.IsAlly && target != assigned)
                 hints.Add("Target correct boss!");
         }
     }
 
     public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
     {
-        if (slot < _assignedBoss.Length && _assignedBoss[slot] != null)
-            foreach (var enemy in hints.PotentialTargets)
-                if (enemy.Actor != _assignedBoss[slot])
-                    enemy.Priority = AIHints.Enemy.PriorityInvincible;
+        var buff = GetBuff(slot);
+        if (buff == Buff.None)
+            return;
+
+        for (var b = Buff.Epic; b <= Buff.Vaunted; b++)
+            if (b != buff)
+                hints.SetPriority(_bosses[(int)b], AIHints.Enemy.PriorityInvincible);
     }
 
-    public override void OnTethered(Actor source, ActorTetherInfo tether)
-    {
-        if (tether.ID == (uint)TetherID.DecisiveBattle && Raid.FindSlot(source.InstanceID) is var slot && slot >= 0)
-        {
-            _assignedBoss[slot] = WorldState.Actors.Find(tether.Target);
-        }
-    }
+    private Buff GetBuff(int slot) => slot < _buffs.Length ? _buffs[slot] : Buff.None;
+    private Actor? AssignedBoss(int slot) => _bosses[(int)GetBuff(slot)];
 }
