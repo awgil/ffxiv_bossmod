5fc8ab1bc0fad24a84e78d17806625e3f7581d09
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-05-21 01:09:20 -0400

    thanks vbm003

--- BossMod/Modules/Shadowbringers/Alliance/A15WalkingFortress/BallisticFloor.cs
+++ BossMod/Modules/Shadowbringers/Alliance/A15WalkingFortress/BallisticFloor.cs
@@ -1,5 +1,4 @@
-﻿
-namespace BossMod.Shadowbringers.Alliance.A15WalkingFortress;
+﻿namespace BossMod.Shadowbringers.Alliance.A15WalkingFortress;
 
 class BallisticFloor(BossModule module) : Components.GenericAOEs(module, AID.BallisticImpactFloor)
 {
