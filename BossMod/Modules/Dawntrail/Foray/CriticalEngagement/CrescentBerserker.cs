dfd7e99434005895542e775dcb331b388d6778e4
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-05-29 14:47:04 -0400

    reorganize some CEs

---
+++ BossMod/Modules/Dawntrail/Foray/CriticalEngagement/CrescentBerserker.cs
@@ -0,0 +1,21 @@
+﻿namespace BossMod.Dawntrail.Foray.CriticalEngagement.CrescentBerserker;
+
+public enum OID : uint
+{
+    Boss = 0x4703,
+    Helper = 0x233C,
+}
+
+class CrescentBerserkerStates : StateMachineBuilder
+{
+    public CrescentBerserkerStates(BossModule module) : base(module)
+    {
+        TrivialPhase();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1018, NameID = 13759)]
+public class CrescentBerserker(WorldState ws, Actor primary) : BossModule(ws, primary, new(100, 100), new ArenaBoundsCircle(20));
+
+
+
--- BossMod/Modules/Dawntrail/Foray/CriticalEngagement/OccultKnight.cs
+++ BossMod/Modules/Dawntrail/Foray/CriticalEngagement/OccultKnight.cs
@@ -4,16 +4,19 @@ public enum OID : uint
 {
     Boss = 0x471E,
     Helper = 0x233C,
+    Megaloknight = 0x4720,
 }
 
 class OccultKnightStates : StateMachineBuilder
 {
     public OccultKnightStates(BossModule module) : base(module)
     {
-        TrivialPhase();
+        SimplePhase(0, id => Timeout(id, 9999), "P1")
+            .Raw.Update = () => Module.Enemies(OID.Megaloknight).Any(m => m.IsTargetable);
+        SimplePhase(1, id => Timeout(id, 9999), "P2")
+            .Raw.Update = () => Module.Enemies(OID.Megaloknight).All(m => m.IsDeadOrDestroyed);
     }
 }
 
-// TODO: fix state machine
-//[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1018, NameID = 13728)]
-// public class OccultKnight(WorldState ws, Actor primary) : BossModule(ws, primary, new(100, 100), new ArenaBoundsCircle(20));
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1018, NameID = 13728)]
+public class OccultKnight(WorldState ws, Actor primary) : BossModule(ws, primary, new(100, 100), new ArenaBoundsCircle(20));
