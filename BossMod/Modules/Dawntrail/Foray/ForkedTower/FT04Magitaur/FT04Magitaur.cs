221bd47eeb0edaf0c64e045b93127dc9d0e7d458
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-06-11 01:15:09 -0400

    wip ft04

--- BossMod/Modules/Dawntrail/Foray/ForkedTower/FT04Magitaur/FT04Magitaur.cs
+++ BossMod/Modules/Dawntrail/Foray/ForkedTower/FT04Magitaur/FT04Magitaur.cs
@@ -1,99 +1,24 @@
 ﻿namespace BossMod.Dawntrail.Foray.ForkedTower.FT04Magitaur;
 
-class UnsealedAura(BossModule module) : Components.RaidwideCastDelay(module, AID._Ability_UnsealedAura, AID._Ability_UnsealedAura1, 0.8f);
-
-class AssassinsDagger(BossModule module) : Components.GenericAOEs(module)
-{
-    class Dagger(Angle angle, DateTime d1, DateTime d2)
-    {
-        public Angle Angle = angle;
-        public DateTime Activation1 = d1;
-        public DateTime Activation2 = d2;
-        public int NumCasts;
-    }
-
-    private readonly List<Dagger> _daggers = [];
-
-    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
-    {
-        DateTime next = default;
-        foreach (var dagger in _daggers)
-        {
-            if (next == default)
-                next = dagger.Activation1.AddSeconds(0.5f);
-            if (dagger.Activation1 < next)
-                yield return new AOEInstance(new AOEShapeRect(32, 3), Arena.Center, dagger.Angle, dagger.NumCasts > 0 ? dagger.Activation2 : dagger.Activation1, Color: ArenaColor.Danger);
-            else if (dagger.Activation1 < next.AddSeconds(4))
-                yield return new AOEInstance(new AOEShapeRect(32, 3), Arena.Center, dagger.Angle, dagger.Activation1, Risky: false);
-        }
-    }
-
-    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
-    {
-        if ((AID)spell.Action.ID == AID._Ability_AssassinsDagger1)
-        {
-            var orientation = Angle.FromDirection(spell.LocXZ - caster.Position);
-            var next = Module.CastFinishAt(spell);
-            for (var i = 0; i < 6; i++)
-            {
-                _daggers.Add(new(orientation, next, next.AddSeconds(2)));
-                next = next.AddSeconds(4);
-                orientation += 70.Degrees();
-            }
-            _daggers.SortBy(d => d.Activation1);
-        }
-    }
-
-    public override void OnEventCast(Actor caster, ActorCastEvent spell)
-    {
-        if ((AID)spell.Action.ID is AID._Ability_AssassinsDagger1 or AID._Ability_AssassinsDagger2 or AID._Ability_AssassinsDagger3)
-        {
-            NumCasts++;
-            var dir = Angle.FromDirection(spell.TargetXZ - caster.Position);
-            if (spell.TargetXZ.AlmostEqual(Arena.Center, 1))
-                dir += 180.Degrees();
-
-            var ix = _daggers.FindIndex(d => d.Angle.AlmostEqual(dir, 0.1f));
-            if (ix >= 0)
-            {
-                if (_daggers[ix].NumCasts == 1)
-                    _daggers.RemoveAt(ix);
-                else
-                    _daggers[ix].NumCasts++;
-            }
-        }
-    }
-}
-
-// 12 hits, 10 degrees of rotation between each
-class Dagger1(BossModule module) : Components.ChargeAOEs(module, AID._Ability_AssassinsDagger1, 3);
-class Dagger2(BossModule module) : Components.DebugCasts(module, [AID._Ability_AssassinsDagger1, AID._Ability_AssassinsDagger2], new AOEShapeRect(0, 3, 32));
-
-class CriticalAxeblow(BossModule module) : Components.StandardAOEs(module, AID._Ability_CriticalAxeblow, 20);
-class CriticalAxeblowFloor(BossModule module) : Components.StandardAOEs(module, AID._Ability_CriticalAxeblow, FT04Magitaur.NotPlatforms)
-{
-    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor) => base.ActiveAOEs(slot, actor).Select(s => s with { Rotation = default });
-}
-class CriticalLanceblow(BossModule module) : Components.StandardAOEs(module, AID._Ability_CriticalLanceblow, new AOEShapeDonut(10, 40));
-class CriticalLanceblowFloor(BossModule module) : Components.StandardAOEs(module, AID._Ability_CriticalLanceblow, 0)
-{
-    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
-    {
-        foreach (var aoe in base.ActiveAOEs(slot, actor))
-        {
-            yield return new AOEInstance(new AOEShapeRect(10, 10, 10), Arena.Center + new WDir(0, 14.5f), 45.Degrees(), aoe.Activation);
-            yield return new AOEInstance(new AOEShapeRect(10, 10, 10), Arena.Center + new WDir(0, 14.5f).Rotate(120.Degrees()), 165.Degrees(), aoe.Activation);
-            yield return new AOEInstance(new AOEShapeRect(10, 10, 10), Arena.Center + new WDir(0, 14.5f).Rotate(-120.Degrees()), -75.Degrees(), aoe.Activation);
-        }
-    }
-}
+class UnsealedAura(BossModule module) : Components.RaidwideCastDelay(module, AID.UnsealedAuraCast, AID.UnsealedAura, 0.8f);
 
 [ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1018, NameID = 13947)]
-public class FT04Magitaur(WorldState ws, Actor primary) : BossModule(ws, primary, new(700, -674), new ArenaBoundsCircle(31.5f))
+public class FT04Magitaur(WorldState ws, Actor primary) : BossModule(ws, primary, ArenaCenter, new ArenaBoundsCircle(31.5f))
 {
     public override bool DrawAllPlayers => true;
 
     public static readonly AOEShapeCustom NotPlatforms = MakeNotPlatforms();
+
+    public static readonly (WDir, Angle)[] Platforms = [
+        (new WDir(0, 14.5f).Rotate(-120.Degrees()), -75.Degrees()),
+        (new WDir(0, 14.5f), 45.Degrees()),
+        (new WDir(0, 14.5f).Rotate(120.Degrees()), 165.Degrees())
+    ];
+
+    public static readonly WPos ArenaCenter = new(700, -674);
+
+    public static int GetPlatform(WPos p) => Array.FindIndex(Platforms, pl => p.InRect(ArenaCenter + pl.Item1, pl.Item2.ToDirection(), 10, 10, 10));
+    public static int GetPlatform(Actor a) => GetPlatform(a.Position);
 
     private static AOEShapeCustom MakeNotPlatforms()
     {
@@ -106,5 +31,13 @@ public class FT04Magitaur(WorldState ws, Actor primary) : BossModule(ws, primary
         arena = clipper.Difference(new(arena), new(rect(120.Degrees())));
         arena = clipper.Difference(new(arena), new(rect(-120.Degrees())));
         return new(arena);
+    }
+
+    public override void DrawArena(int pcSlot, Actor pc, bool haveRisks)
+    {
+        base.DrawArena(pcSlot, pc, haveRisks);
+
+        foreach (var (off, angle) in Platforms)
+            Arena.AddRect(Arena.Center + off, angle.ToDirection(), 10, 10, 10, ArenaColor.Border);
     }
 }
--- BossMod/Modules/Dawntrail/Foray/ForkedTower/FT04Magitaur/FT04MagitaurStates.cs
+++ BossMod/Modules/Dawntrail/Foray/ForkedTower/FT04Magitaur/FT04MagitaurStates.cs
@@ -9,32 +9,83 @@ class FT04MagitaurStates : StateMachineBuilder
 
     private void Phase1(uint id)
     {
-        CastStart(id, AID._Ability_UnsealedAura, 10.1f)
-            .ActivateOnEnter<UnsealedAura>();
-
-        ComponentCondition<UnsealedAura>(id + 1, 5, u => u.NumCasts > 0, "Raidwide");
-
-        Cast(id + 0x10, AID._Ability_Unseal, 10.2f, 5, "Select weapon");
-
-        CastStart(id + 0x100, AID._Ability_AssassinsDagger, 16)
-            .ActivateOnEnter<AssassinsDagger>();
-
-        ComponentCondition<AssassinsDagger>(id + 0x200, 5, d => d.NumCasts >= 3, "Daggers start")
-            .ActivateOnEnter<CriticalAxeblow>()
-            .ActivateOnEnter<CriticalAxeblowFloor>()
-            .ActivateOnEnter<CriticalLanceblowFloor>()
-            .ActivateOnEnter<CriticalLanceblow>();
-
-        CastMulti(id + 0x300, [AID._Ability_CriticalAxeblow, AID._Ability_CriticalLanceblow], 7.3f, 5, "Axe/lance 1");
-
-        CastStartMulti(id + 0x400, [AID._Ability_CriticalAxeblow, AID._Ability_CriticalLanceblow], 5.3f);
-
-        ComponentCondition<AssassinsDagger>(id + 0x40A, 4.1f, d => d.NumCasts >= 36, "Daggers end")
-            .DeactivateOnExit<AssassinsDagger>();
-
-        CastEnd(id + 0x410, 1, "Axe/lance 2");
+        UnsealedAura(id, 10.1f);
+        Unseal(id + 0x10000, 10.2f);
+        Daggers1(id + 0x20000, 1.7f);
+        ForkedFury(id + 0x30000, 6.1f);
+        Conduit(id + 0x40000, 11);
 
         Timeout(id + 0xFF0000, 9999, "???")
             .ActivateOnEnter<AssassinsDagger>();
+    }
+
+    private void UnsealedAura(uint id, float delay)
+    {
+        CastStart(id, AID.UnsealedAuraCast, delay)
+            .ActivateOnEnter<UnsealedAura>();
+
+        ComponentCondition<UnsealedAura>(id + 1, 5, u => u.NumCasts > 0, "Raidwide")
+            .DeactivateOnExit<UnsealedAura>();
+    }
+
+    private void Unseal(uint id, float delay, int weapon = 0)
+    {
+        var autosState = weapon switch
+        {
+            0 => CastMulti(id, [AID.UnsealAxe, AID.UnsealLance], delay, 5, "Select weapon"),
+            1 => Cast(id, AID.UnsealAxe, delay, 5, "Select axe"),
+            2 => Cast(id, AID.UnsealLance, delay, 5, "Select lance"),
+            _ => throw new ArgumentException($"invalid value for {nameof(weapon)}")
+        };
+
+        autosState.ActivateOnEnter<UnsealAutos>();
+
+        ComponentCondition<UnsealAutos>(id + 0x10, 8, a => a.NumCasts > 0, "Autos 1");
+        ComponentCondition<UnsealAutos>(id + 0x11, 3.2f, a => a.NumCasts > 6, "Autos 2");
+        ComponentCondition<UnsealAutos>(id + 0x12, 3.2f, a => a.NumCasts > 12, "Autos 3")
+            .DeactivateOnExit<UnsealAutos>();
+    }
+
+    private void Daggers1(uint id, float delay)
+    {
+        CastStart(id, AID._Ability_AssassinsDagger, delay)
+            .ActivateOnEnter<AssassinsDagger>();
+
+        ComponentCondition<AssassinsDagger>(id + 1, 5, d => d.NumCasts >= 3, "Daggers start")
+            .ActivateOnEnter<CriticalCounter>()
+            .ActivateOnEnter<CriticalAxeblow>()
+            .ActivateOnEnter<CriticalLanceblow>();
+
+        CastMulti(id + 0x10, [AID.CriticalAxeblowCast, AID.CriticalLanceblowCast], 7.3f, 5);
+        ComponentCondition<CriticalCounter>(id + 0x12, 1.3f, c => c.NumCasts > 0, "Axe/lance 1");
+
+        CastStartMulti(id + 0x20, [AID.CriticalAxeblowCast, AID.CriticalLanceblowCast], 5.3f);
+
+        ComponentCondition<AssassinsDagger>(id + 0x30, 4.1f, d => d.NumCasts >= 36, "Daggers end")
+            .DeactivateOnExit<AssassinsDagger>();
+
+        ComponentCondition<CriticalCounter>(id + 0x40, 2.1f, c => c.NumCasts > 1, "Axe/lance 2");
+    }
+
+    private void ForkedFury(uint id, float delay)
+    {
+        Cast(id, AID._Ability_ForkedFury, delay, 4.5f)
+            .ActivateOnEnter<ForkedFury>();
+        ComponentCondition<ForkedFury>(id + 0x10, 0.6f, t => t.NumCasts > 0, "Tankbusters")
+            .DeactivateOnExit<ForkedFury>();
+    }
+
+    private void Conduit(uint id, float delay)
+    {
+        CastStartMulti(id, [AID._Ability_AuraBurst, AID._Ability_Holy], delay, "Conduits appear")
+            .ActivateOnEnter<Conduit>()
+            .ActivateOnEnter<ArcaneReaction>()
+            .ActivateOnEnter<ArcaneRecoil>();
+
+        ComponentCondition<ArcaneReaction>(id + 0x10, 8, r => r.NumCasts > 0, "Hysteria bait")
+            .DeactivateOnExit<ArcaneReaction>();
+
+        ComponentCondition<Conduit>(id + 0x100, 11.9f, c => c.NumCasts > 0, "Raidwide + conduit enrage")
+            .SetHint(StateMachine.StateHint.Raidwide);
     }
 }
