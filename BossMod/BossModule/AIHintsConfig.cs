4454262ac2deced64c9216c86b9b1142f57b6587
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-06-25 16:29:14 -0400

    actually put hintsconfig in the changelog

--- BossMod/BossModule/AIHintsConfig.cs
+++ BossMod/BossModule/AIHintsConfig.cs
@@ -11,10 +11,10 @@ public sealed class AIHintsConfig : ConfigNode
         Ignore
     }
 
-    [PropertyDisplay("Behavior for unknown donut AOEs", tooltip: "This setting only applies when no module is active.")]
+    [PropertyDisplay("Behavior for unknown donut AOEs", since: "0.3.0.0", tooltip: "This setting only applies when no module is active.")]
     public DonutFallbackBehavior DonutFallback = DonutFallbackBehavior.AssumeCircle;
 
-    [PropertyDisplay("Guessed angle for unknown cone AOEs", tooltip: "This setting only applies when no module is active.")]
+    [PropertyDisplay("Guessed angle for unknown cone AOEs", since: "0.3.0.0", tooltip: "This setting only applies when no module is active.")]
     [PropertySlider(1, 180, Speed = 5)]
     public float ConeFallbackAngle = 180;
 
@@ -28,9 +28,9 @@ public sealed class AIHintsConfig : ConfigNode
         OmenOnly
     }
 
-    [PropertyDisplay("Behavior for actions without AOE indicators", tooltip: "This setting only applies when no module is active.")]
+    [PropertyDisplay("Behavior for actions without AOE indicators", since: "0.3.0.0", tooltip: "This setting only applies when no module is active.")]
     public OmenBehavior OmenSetting = OmenBehavior.Automatic;
 
-    [PropertyDisplay("Run generic solver for 'Helper'-type actors", tooltip: "This setting only applies when no module is active.\n\nHelpers are invisible enemies that are responsible for casting a majority of the mechanics in boss fights. Enabling this option may cause unwanted results. For example, vbm may treat gaze attacks, knockbacks, raidwides, or other mechanics as regular AOEs and try to run out of them. Enable this setting at your own discretion.")]
+    [PropertyDisplay("Run generic solver for 'Helper'-type actors", since: "0.3.0.0", tooltip: "This setting only applies when no module is active.\n\nHelpers are invisible enemies that are responsible for casting a majority of the mechanics in boss fights. Note that, although this allows the generic solver to support many more mechanics, it may cause unwanted results, since the generic solver cannot distinguish regular AOEs from gaze attacks, knockbacks, non-damaging casts that apply debuffs, and so on. Enable this option at your own discretion.")]
     public bool EnableHelperHints = false;
 }
