05fa14460ee6d60ebcc93767328bb8db1808e42e
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-08-08 21:10:08 -0400

    necron start

---
+++ BossMod/Modules/Dawntrail/Extreme/Ex5Necron/Ex5Necron.cs
@@ -0,0 +1,5 @@
+﻿namespace BossMod.Dawntrail.Extreme.Ex5Necron;
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1062, NameID = 14093, DevOnly = true)]
+public class Ex5Necron(WorldState ws, Actor primary) : BossModule(ws, primary, new(100, 100), new ArenaBoundsSquare(20));
+
---
+++ BossMod/Modules/Dawntrail/Extreme/Ex5Necron/Ex5NecronStates.cs
@@ -0,0 +1,49 @@
+﻿namespace BossMod.Dawntrail.Extreme.Ex5Necron;
+
+class BlueShockwave(BossModule module) : Components.TankSwap(module, default(AID), AID._Weaponskill_BlueShockwave1, AID._Weaponskill_BlueShockwave1, 4.1f, new AOEShapeCone(100, 45.Degrees()), false)
+{
+    public override void OnEventIcon(Actor actor, uint iconID, ulong targetID)
+    {
+        if (iconID == 615)
+        {
+            _source = actor;
+            _prevTarget = targetID;
+            _activation = WorldState.FutureTime(7.2f);
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        base.OnEventCast(caster, spell);
+
+        if (NumCasts >= 2)
+        {
+            CurrentBaits.Clear();
+            _source = null;
+            _activation = default;
+        }
+    }
+}
+
+class Ex5NecronStates : StateMachineBuilder
+{
+    public Ex5NecronStates(BossModule module) : base(module)
+    {
+        SimplePhase(0, P1, "P1");
+    }
+
+    private void P1(uint id)
+    {
+        BlueShockwave(id, 14.2f);
+
+        Timeout(id + 0xFF0000, 9999, "???");
+    }
+
+    private void BlueShockwave(uint id, float delay)
+    {
+        ComponentCondition<BlueShockwave>(id, delay, b => b.NumCasts > 0, "Tankbuster 1")
+            .ActivateOnEnter<BlueShockwave>();
+        ComponentCondition<BlueShockwave>(id + 0x10, 4.1f, b => b.NumCasts > 1, "Tankbuster 2")
+            .DeactivateOnExit<BlueShockwave>();
+    }
+}
