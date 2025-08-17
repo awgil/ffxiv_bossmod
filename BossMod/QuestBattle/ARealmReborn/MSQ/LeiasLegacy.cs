a43e9bdf16bbaaff2266bbdb0a8dd7eb0e468d7b
Liza Carvelli <liza@carvel.li>
2025-08-17 21:27:24 +0200

    Prioritize healers, deprioritize bosses in early MSQ battles

---
+++ BossMod/QuestBattle/ARealmReborn/MSQ/LeiasLegacy.cs
@@ -0,0 +1,16 @@
+﻿namespace BossMod.QuestBattle.ARealmReborn.MSQ;
+
+[ZoneModuleInfo(BossModuleInfo.Maturity.Contributed, 299)]
+internal class LeiasLegacy(WorldState ws) : QuestBattle(ws)
+{
+    public override void CalculateAIHints(int playerSlot, Actor player, AIHints hints)
+    {
+        foreach (var p in hints.PotentialTargets)
+            p.Priority = p.Actor.OID switch
+            {
+                0x9A => 2,
+                0x98 => 0,
+                _ => 1
+            };
+    }
+}
---
+++ BossMod/QuestBattle/ARealmReborn/MSQ/ToGuardAGuardian.cs
@@ -0,0 +1,16 @@
+﻿namespace BossMod.QuestBattle.ARealmReborn.MSQ;
+
+[ZoneModuleInfo(BossModuleInfo.Maturity.Contributed, 298)]
+internal class ToGuardAGuardian(WorldState ws) : QuestBattle(ws)
+{
+    public override void CalculateAIHints(int playerSlot, Actor player, AIHints hints)
+    {
+        foreach (var p in hints.PotentialTargets)
+            p.Priority = p.Actor.OID switch
+            {
+                0x39C => 0,
+                0x39F => 2,
+                _ => 1
+            };
+    }
+}
