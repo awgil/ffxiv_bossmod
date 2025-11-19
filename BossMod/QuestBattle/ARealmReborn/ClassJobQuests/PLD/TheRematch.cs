c7725b08f28c19d6c483f8c6b065effa8f9cbebb
Oblituarius <44579284+Oblituarius@users.noreply.github.com>
2025-11-19 15:36:23 -0600

    normalmove and therematch changes
    Added logic to NormalMove. This will help to not spawn new pathfinding tasks if ForceMovement is set
    Added new nav coords to TheRematch SoloPlayerDuty (QuestBattles) to improve navigation.

--- BossMod/QuestBattle/ARealmReborn/ClassJobQuests/PLD/TheRematch.cs
+++ BossMod/QuestBattle/ARealmReborn/ClassJobQuests/PLD/TheRematch.cs
@@ -5,7 +5,12 @@ internal class TheRematch(WorldState ws) : QuestBattle(ws)
 {
     public override List<QuestObjective> DefineObjectives(WorldState ws) => [
         new QuestObjective(ws)
-            .WithConnection(new Vector3(-0.95f, -10.02f, -16.34f))
+            .WithConnection(new Vector3(-4.35f, -30.00f, -32.35f))
+            .WithConnection(new Vector3(6.30f, -31.00f, -9.42f))
+            .WithConnection(new Vector3(2.00f, -17.54f, 23.70f))
+            .WithConnection(new Vector3(5.00f, -7.85f, 1.40f))
+            .WithConnection(new Vector3(23.48f, -6.01f, -16.19f))
+            .WithConnection(new Vector3(-21.73f, -10.01f, -15.83f))
             .PauseForCombat(false)
     ];
 }
