b4839cbc76d6b9163c31088bb8246226971461a2
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-12-30 19:34:39 -0500

    bosses

--- BossMod/Modules/Dawntrail/Dungeon/D12Mistwake/D122Amdusias.cs
+++ BossMod/Modules/Dawntrail/Dungeon/D12Mistwake/D122Amdusias.cs
@@ -1,51 +1,191 @@
-﻿#pragma warning disable CA1707 // Identifiers should not contain underscores
-namespace BossMod.Dawntrail.Dungeon.D12Mistwake.D122Amdusias;
+﻿namespace BossMod.Dawntrail.Dungeon.D12Mistwake.D122Amdusias;
 
 public enum OID : uint
 {
     Boss = 0x4A77, // R3.960, x1
     Helper = 0x233C, // R0.500, x15, Helper type
-    _Gen_Thunderhead = 0x18D6, // R0.500, x2
-    _Gen_PoisonCloud = 0x4A78, // R1.200, x0 (spawn during fight)
+    Thunderhead = 0x18D6, // R0.500, x2
+    PoisonCloud = 0x4A78, // R1.200, x0 (spawn during fight)
 }
 
 public enum AID : uint
 {
-    _AutoAttack_ = 46839, // Boss->player, no cast, single-target
-    _Weaponskill_ = 45357, // Boss->location, no cast, single-target
-    _Weaponskill_StaticCharge = 45335, // Boss->self, no cast, single-target
-    _Weaponskill_ThunderclapConcerto = 45341, // Boss->self, 5.0+0.5s cast, single-target
-    _Weaponskill_ThunderclapConcerto1 = 45342, // Helper->self, 5.5s cast, range 40 300-degree cone
-    _Weaponskill_StaticCharge1 = 45333, // Boss->self, no cast, single-target
-    _Weaponskill_ThunderclapConcerto2 = 45336, // Boss->self, 5.0+0.5s cast, single-target
-    _Weaponskill_ThunderclapConcerto3 = 45337, // Helper->self, 5.5s cast, range 40 300-degree cone
-    _Weaponskill_BioII = 45344, // Boss->self, 5.0s cast, single-target
-    _Weaponskill_BioII1 = 45345, // Helper->location, 5.0s cast, range 20 circle
-    _Weaponskill_1 = 45348, // Helper->location, 1.5s cast, width 5 rect charge
-    _Weaponskill_GallopingThunder = 45346, // Boss->location, 10.0s cast, single-target
-    _Weaponskill_GallopingThunder1 = 45347, // Boss->location, no cast, width 5 rect charge
-    _Weaponskill_Burst = 45349, // 4A78->self, 2.5s cast, range 9 circle
-    _Weaponskill_ThunderIV = 45350, // Boss->self, 4.4+0.6s cast, single-target
-    _Weaponskill_ThunderIV1 = 45351, // Helper->self, 5.0s cast, range 70 circle
-    _Weaponskill_Shockbolt = 45355, // Boss->self, 4.4+0.6s cast, single-target
-    _Weaponskill_Shockbolt1 = 45356, // Helper->player, 5.0s cast, single-target
-    _Weaponskill_Thunder = 45343, // Helper->player, 5.0s cast, range 5 circle
-    _Weaponskill_ThunderIII = 45352, // Boss->self, 4.4+0.6s cast, single-target
-    _Weaponskill_ThunderIII1 = 45353, // Helper->players, 5.0s cast, range 6 circle
-    _Weaponskill_ThunderIII2 = 45354, // Helper->players, no cast, range 6 circle
+    AutoAttack = 46839, // Boss->player, no cast, single-target
+    Unk = 45357, // Boss->location, no cast, single-target
+    StaticCharge1 = 45335, // Boss->self, no cast, single-target
+    StaticCharge2 = 45333, // Boss->self, no cast, single-target
+    ThunderclapConcertoCast1 = 45341, // Boss->self, 5.0+0.5s cast, single-target
+    ThunderclapConcerto1 = 45342, // Helper->self, 5.5s cast, range 40 300-degree cone
+    ThunderclapConcertoCast2 = 45336, // Boss->self, 5.0+0.5s cast, single-target
+    ThunderclapConcerto2 = 45337, // Helper->self, 5.5s cast, range 40 300-degree cone
+    BioIICast = 45344, // Boss->self, 5.0s cast, single-target
+    BioII = 45345, // Helper->location, 5.0s cast, range 20 circle
+    GallopingThunderPre = 45348, // Helper->location, 1.5s cast, width 5 rect charge
+    GallopingThunderCast = 45346, // Boss->location, 10.0s cast, single-target
+    GallopingThunderCharge = 45347, // Boss->location, no cast, width 5 rect charge
+    Burst = 45349, // PoisonCloud->self, 2.5s cast, range 9 circle
+    ThunderIVCast = 45350, // Boss->self, 4.4+0.6s cast, single-target
+    ThunderIV = 45351, // Helper->self, 5.0s cast, range 70 circle
+    ShockboltCast = 45355, // Boss->self, 4.4+0.6s cast, single-target
+    Shockbolt = 45356, // Helper->player, 5.0s cast, single-target
+    Thunder = 45343, // Helper->player, 5.0s cast, range 5 circle
+    ThunderIIICast = 45352, // Boss->self, 4.4+0.6s cast, single-target
+    ThunderIIIFirst = 45353, // Helper->players, 5.0s cast, range 6 circle
+    ThunderIIIRest = 45354, // Helper->players, no cast, range 6 circle
 }
 
-class ThunderclapConcerto(BossModule module) : Components.GroupedAOEs(module, [AID._Weaponskill_ThunderclapConcerto1, AID._Weaponskill_ThunderclapConcerto3], new AOEShapeCone(40, 150.Degrees()));
+class ThunderclapConcerto(BossModule module) : Components.GroupedAOEs(module, [AID.ThunderclapConcerto1, AID.ThunderclapConcerto2], new AOEShapeCone(40, 150.Degrees()));
+
+class PoisonCloud(BossModule module) : BossComponent(module)
+{
+    public override void DrawArenaForeground(int pcSlot, Actor pc_)
+    {
+        foreach (var pc in Module.Enemies(OID.PoisonCloud))
+        {
+            Arena.Actor(pc.Position, pc.Rotation, ArenaColor.Object);
+            Arena.AddCircle(pc.Position, 1.2f, ArenaColor.Object);
+        }
+    }
+}
+
+class BurstPredict(BossModule module) : Components.GenericAOEs(module, AID.Burst)
+{
+    private readonly List<List<(Actor Source, DateTime Activation)>> _sources = [];
+    private readonly HashSet<ulong> _casters = [];
+
+    private bool _gallopMode;
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        var i = 0;
+        foreach (var grp in _sources.Take(_gallopMode ? 2 : int.MaxValue))
+        {
+            var color = i == 0 && _gallopMode ? ArenaColor.Danger : ArenaColor.AOE;
+            foreach (var src in grp)
+                yield return new AOEInstance(new AOEShapeCircle(9), src.Source.Position, default, src.Activation, color);
+            i++;
+        }
+    }
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.GallopingThunderPre)
+        {
+            _gallopMode = true;
+            var from = caster.Position;
+            var to = spell.LocXZ;
+            var balls = Module.Enemies(OID.PoisonCloud).Where(c => c.Position.InRect(from, to - from, 2.5f + 1.2f) && !_casters.Contains(c.InstanceID));
+            var next = _sources.Count == 0 ? Module.CastFinishAt(spell, 13.7f) : _sources[^1][0].Activation.AddSeconds(2.1f);
+            _sources.Add([]);
+            foreach (var ball in balls)
+            {
+                _sources[^1].Add((ball, next));
+                _casters.Add(ball.InstanceID);
+            }
+            _sources.RemoveAll(s => s.Count == 0);
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if (spell.Action == WatchedAction)
+        {
+            NumCasts++;
+            _casters.Remove(caster.InstanceID);
+            if (_sources.Count > 0)
+            {
+                if (_sources[0].Count > 0)
+                    _sources[0].RemoveAt(0);
+                if (_sources[0].Count == 0)
+                    _sources.RemoveAt(0);
+            }
+            if (_sources.Count == 0)
+                _gallopMode = false;
+        }
+
+        if ((AID)spell.Action.ID == AID.ThunderIVCast)
+            foreach (var src in Module.Enemies(OID.PoisonCloud).Where(e => !e.IsDead))
+                _sources.Add([(src, WorldState.FutureTime(5.9f))]);
+    }
+}
+
+// 46.34 -> 60
+class GallopingThunder(BossModule module) : Components.GenericAOEs(module, AID.GallopingThunderCharge)
+{
+    private readonly List<(WPos From, WPos To, DateTime Activate)> _aoes = [];
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        var i = 0;
+        foreach (var aoe in Enumerable.Reverse(_aoes))
+        {
+            var dir = aoe.To - aoe.From;
+            var len = dir.Length();
+            yield return new AOEInstance(new AOEShapeRect(len, 2.5f), aoe.From, dir.ToAngle(), aoe.Activate, ++i == _aoes.Count ? ArenaColor.Danger : ArenaColor.AOE);
+        }
+    }
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.GallopingThunderPre)
+        {
+            var next = _aoes.Count == 0 ? Module.CastFinishAt(spell, 8.7f) : _aoes[^1].Activate.AddSeconds(2.1f);
+            _aoes.Add((caster.Position, spell.LocXZ, next));
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if (spell.Action == WatchedAction)
+        {
+            NumCasts++;
+            if (_aoes.Count > 0)
+                _aoes.RemoveAt(0);
+        }
+    }
+}
+
+class ThunderSpread(BossModule module) : Components.SpreadFromCastTargets(module, AID.Thunder, 5);
+
+class ThunderIII(BossModule module) : Components.StackWithCastTargets(module, AID.ThunderIIIFirst, 5, 4)
+{
+    private int _numCasts;
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID is AID.ThunderIIIFirst or AID.ThunderIIIRest)
+        {
+            _numCasts++;
+            if (_numCasts >= 3)
+            {
+                Stacks.Clear();
+                _numCasts = 0;
+            }
+        }
+    }
+}
+
+class BioII(BossModule module) : Components.RaidwideCast(module, AID.BioII);
+class Shockbolt(BossModule module) : Components.SingleTargetCast(module, AID.Shockbolt);
+class ThunderIV(BossModule module) : Components.RaidwideCast(module, AID.ThunderIV);
 
 class D122AmdusiasStates : StateMachineBuilder
 {
     public D122AmdusiasStates(BossModule module) : base(module)
     {
         TrivialPhase()
-            .ActivateOnEnter<ThunderclapConcerto>();
+            .ActivateOnEnter<ThunderclapConcerto>()
+            .ActivateOnEnter<PoisonCloud>()
+            .ActivateOnEnter<BurstPredict>()
+            .ActivateOnEnter<GallopingThunder>()
+            .ActivateOnEnter<ThunderSpread>()
+            .ActivateOnEnter<ThunderIII>()
+            .ActivateOnEnter<BioII>()
+            .ActivateOnEnter<Shockbolt>()
+            .ActivateOnEnter<ThunderIV>();
     }
 }
 
-[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1064, NameID = 14271, DevOnly = true)]
+[ModuleInfo(BossModuleInfo.Maturity.Verified, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1064, NameID = 14271)]
 public class D122Amdusias(WorldState ws, Actor primary) : BossModule(ws, primary, new(281, -285), new ArenaBoundsCircle(19.5f));
 
---
+++ BossMod/Modules/Dawntrail/Dungeon/D12Mistwake/D123ThundergustGriffin.cs
@@ -0,0 +1,120 @@
+﻿namespace BossMod.Dawntrail.Dungeon.D12Mistwake.D123ThundergustGriffin;
+
+public enum OID : uint
+{
+    Boss = 0x4A65, // R6.900, x1
+    Helper = 0x233C, // R0.500, x41, Helper type
+    BallLightning = 0x4A66, // R2.000, x0 (spawn during fight)
+    ShockStorm = 0x4A67, // R1.000, x0 (spawn during fight)
+}
+
+public enum AID : uint
+{
+    AutoAttack = 45307, // Boss->player, no cast, single-target
+    Thunderspark = 45291, // Boss->self, 5.0s cast, range 60 circle
+    Jump = 46852, // Boss->location, no cast, single-target
+    HighVolts = 45292, // Boss->self, 4.0s cast, single-target
+    LightningBoltGround = 45293, // Helper->self, 5.0s cast, range 5 circle
+    LightningBoltSpread = 46856, // Helper->player, 5.0s cast, range 5 circle
+    Jump2 = 45294, // Boss->location, no cast, single-target
+    ThunderboltPre1 = 46943, // Helper->self, 4.3s cast, range 20 width 3 rect
+    ThunderboltPre2 = 46944, // Helper->self, 4.3s cast, range 16 width 3 rect
+    ThunderboltPre3 = 46945, // Helper->self, 4.3s cast, range 16 width 3 rect
+    ThunderingRoar = 45295, // Boss->self, 5.0s cast, single-target
+    Thunderbolt1 = 45296, // Helper->self, 5.5s cast, range 92 width 6 rect
+    Thunderbolt2 = 45297, // Helper->self, 5.5s cast, range 92 width 6 rect
+    Thunderbolt3 = 45298, // Helper->self, 5.5s cast, range 92 width 6 rect
+    GoldenTalons = 45305, // Boss->player, 5.0s cast, single-target
+    FulgurousFall = 45301, // Boss->location, 6.0s cast, single-target
+    Rush = 45302, // Helper->self, 6.0s cast, range 40 width 10 rect
+    ElectrifyingFlight = 47022, // Helper->self, 6.2s cast, range 50 width 40 rect
+    ElectrogeneticForce = 45304, // Helper->self, 9.6s cast, range 40 width 18 rect
+    StormSurge = 45299, // Boss->self, 3.0s cast, range 50 width 10 rect
+    ElectriflyingFlightStorm = 45300, // ShockStorm->location, 0.5s cast, single-target
+}
+
+class Thunderspark(BossModule module) : Components.RaidwideCast(module, AID.Thunderspark);
+class LightningBoltGround(BossModule module) : Components.StandardAOEs(module, AID.LightningBoltGround, 5);
+class LightningBoltSpread(BossModule module) : Components.SpreadFromCastTargets(module, AID.LightningBoltSpread, 5);
+class Thunderbolt(BossModule module) : Components.GroupedAOEs(module, [AID.Thunderbolt2, AID.Thunderbolt1, AID.Thunderbolt3], new AOEShapeRect(92, 3));
+
+class GoldenTalons(BossModule module) : Components.SingleTargetCast(module, AID.GoldenTalons);
+class Rush(BossModule module) : Components.StandardAOEs(module, AID.Rush, new AOEShapeRect(40, 5));
+
+class ElectrifyingFlight(BossModule module) : Components.Knockback(module)
+{
+    private Actor? _caster;
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.ElectrifyingFlight)
+            _caster = caster;
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID == AID.ElectrifyingFlight)
+            _caster = null;
+    }
+
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        foreach (var src in Sources(slot, actor))
+        {
+            var kbDir = src.Kind switch
+            {
+                Kind.DirLeft => src.Direction.ToDirection().OrthoL(),
+                Kind.DirRight => src.Direction.ToDirection().OrthoR(),
+                _ => default
+            };
+            hints.AddForbiddenZone(p => !(p + kbDir * src.Distance).InCircle(Arena.Center, 20), src.Activation);
+        }
+    }
+
+    public override IEnumerable<Source> Sources(int slot, Actor actor)
+    {
+        if (_caster == null)
+            yield break;
+
+        var activation = Module.CastFinishAt(_caster.CastInfo);
+        if (IsImmune(slot, activation))
+            yield break;
+
+        var dir = _caster.Rotation.ToDirection().OrthoL().Dot(_caster.DirectionTo(actor)) > 0 ? Kind.DirLeft : Kind.DirRight;
+
+        yield return new(_caster.Position, 12, activation, Direction: _caster.Rotation, Kind: dir);
+    }
+}
+
+class ElectrogeneticForce(BossModule module) : Components.StandardAOEs(module, AID.ElectrogeneticForce, new AOEShapeRect(40, 9))
+{
+    private readonly ElectrifyingFlight _flight = module.FindComponent<ElectrifyingFlight>()!;
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        return _flight.Sources(slot, actor).Any() ? [] : base.ActiveAOEs(slot, actor);
+    }
+}
+
+class StormSurge(BossModule module) : Components.StandardAOEs(module, AID.StormSurge, new AOEShapeRect(50, 5));
+
+class D123ThundergustGriffinStates : StateMachineBuilder
+{
+    public D123ThundergustGriffinStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<Thunderspark>()
+            .ActivateOnEnter<LightningBoltGround>()
+            .ActivateOnEnter<LightningBoltSpread>()
+            .ActivateOnEnter<Thunderbolt>()
+            .ActivateOnEnter<GoldenTalons>()
+            .ActivateOnEnter<Rush>()
+            .ActivateOnEnter<ElectrifyingFlight>()
+            .ActivateOnEnter<ElectrogeneticForce>()
+            .ActivateOnEnter<StormSurge>();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.Verified, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1064, NameID = 14288)]
+public class D123ThundergustGriffin(WorldState ws, Actor primary) : BossModule(ws, primary, new(281, -620), new ArenaBoundsCircle(20));
+
