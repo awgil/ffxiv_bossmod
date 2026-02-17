6a68a69672c708286b5d2d7b2f553e88a4d33645
Simon Latusek <Simon.Latusek@gmail.com>
2026-02-17 20:56:09 +0100

    Hall of the Novice Modules

---
+++ BossMod/Modules/RealmReborn/Novice/NoviceBasicFinal.cs
@@ -0,0 +1,31 @@
+﻿namespace BossMod.RealmReborn.Novice.NoviceBasicFinal;
+
+public enum OID : uint
+{
+    Boss = 0x1591,
+    Helper = 0x233C,
+}
+
+class N12AttackAsTargeted(BossModule module) : BossComponent(module)
+{
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        foreach (var stateActor in WorldState.Actors)
+        {
+            if (stateActor.OID == (uint)OID.Boss)
+                hints.SetPriority(stateActor, 1);
+        }
+    }
+}
+
+class NoviceBasicFinalStates : StateMachineBuilder
+{
+    public NoviceBasicFinalStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+           .ActivateOnEnter<N12AttackAsTargeted>();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 166, NameID = 4787)]
+public class NoviceBasicFinal(WorldState ws, Actor primary) : BossModule(ws, primary, new(457.383f, 271.721f), new ArenaBoundsCircle(20));
---
+++ BossMod/Modules/RealmReborn/Novice/NoviceTank04.cs
@@ -0,0 +1,64 @@
+﻿namespace BossMod.RealmReborn.Novice.NoviceTank04;
+
+public enum OID : uint
+{
+    Boss = 0x1564,
+    Helper = 0x233C,
+}
+
+class Tank04Dummies(BossModule module) : BossComponent(module)
+{
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        base.AddAIHints(slot, actor, assignment, hints);
+
+        var count = 0;
+        float minX = float.MaxValue, minZ = float.MaxValue, maxX = float.MinValue, maxZ = float.MinValue;
+
+        foreach (var stateActor in WorldState.Actors)
+        {
+            if (stateActor.OID == (uint)OID.Boss)
+            {
+                count++;
+                minX = Math.Min(minX, stateActor.Position.X);
+                minZ = Math.Min(minZ, stateActor.Position.Z);
+                maxX = Math.Max(maxX, stateActor.Position.X);
+                maxZ = Math.Max(maxZ, stateActor.Position.Z);
+                hints.SetPriority(stateActor, AIHints.Enemy.PriorityForbidden);
+            }
+        }
+
+        if (count == 3)
+        {
+            var target = new WPos((maxX - minX) / 2f + minX, (maxZ - minZ) / 2f + minZ);
+            hints.GoalZones.Add(hints.GoalSingleTarget(target, 1f));
+
+            if (actor.DistanceToPoint(target) < 1f)
+            {
+                hints.ActionsToExecute.Clear();
+                hints.ActionsToExecute.Push(new ActionID(ActionType.Spell, actor.Class switch
+                {
+                    Class.MRD or Class.WAR => 41u,
+                    Class.GLA or Class.PLD => 7381u,
+                    Class.DRK => 3621u,
+                    _ => 11u
+                }), null, 1f);
+            }
+        }
+    }
+}
+
+class NoviceTank04States : StateMachineBuilder
+{
+    public NoviceTank04States(BossModule module) : base(module)
+    {
+        TrivialPhase()
+           .ActivateOnEnter<Tank04Dummies>();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 154, NameID = 541)]
+public class NoviceTank04(WorldState ws, Actor primary) : BossModule(ws, primary, new(0, 0), new ArenaBoundsCircle(20))
+{
+    protected override bool CheckPull() => PrimaryActor.IsTargetable;
+}
