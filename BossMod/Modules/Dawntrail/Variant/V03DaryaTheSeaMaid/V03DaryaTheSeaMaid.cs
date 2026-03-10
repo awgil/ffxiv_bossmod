25dacc31e67a6b2af05bacec402f4835f4875a84
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2026-03-10 14:44:43 -0400

    hide forbidden modules

---
+++ BossMod/Modules/Dawntrail/Variant/V03DaryaTheSeaMaid/V03DaryaTheSeaMaid.cs
@@ -0,0 +1,18 @@
+﻿namespace BossMod.Dawntrail.Variant.V03DaryaSeaMaid;
+
+public enum OID : uint
+{
+    Boss = 0x4A94,
+    Helper = 0x233C,
+}
+
+class V03DaryaTheSeaMaidStates : StateMachineBuilder
+{
+    public V03DaryaTheSeaMaidStates(BossModule module) : base(module)
+    {
+        TrivialPhase();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1084, NameID = 14291, DevOnly = true)]
+public class V03DaryaTheSeaMaid(WorldState ws, Actor primary) : BossModule(ws, primary, new(375, 530), new ArenaBoundsSquare(20));
