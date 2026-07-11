02bad21aa60f7eb55bdf61ed81a34cdb67f9fd11
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2026-07-11 15:51:14 -0400

    cleanup gauge code

--- BossMod/Replay/Visualization/GaugeVisualizer.cs
+++ BossMod/Replay/Visualization/GaugeVisualizer.cs
@@ -1,6 +1,7 @@
 ﻿using Dalamud.Bindings.ImGui;
 using Dalamud.Interface.GameFonts;
 using Dalamud.Interface.ManagedFontAtlas;
+using Dalamud.Interface.Textures;
 using Dalamud.Interface.Textures.TextureWraps;
 using Dalamud.Interface.Utility;
 using FFXIVClientStructs.FFXIV.Client.Game.Gauge;
@@ -33,21 +34,12 @@ internal class GaugeVisualizer
         GaugeFont.Dispose();
     }
 
-    readonly TexFile GaugeSimple;
-    readonly TexFile StackA;
-    readonly TexFile StackB;
-    readonly TexFile HudMNK;
     readonly IFontHandle GaugeFont;
 
     readonly Dictionary<string, IDalamudTextureWrap> _tex = [];
 
     private GaugeVisualizer()
     {
-        GaugeSimple = Service.LuminaGameData.GetFile<TexFile>("ui/uld/Parameter_Gauge_hr1.tex")!;
-        StackA = Service.LuminaGameData.GetFile<TexFile>("ui/uld/JobHudSimple_StackA_hr1.tex")!;
-        StackB = Service.LuminaGameData.GetFile<TexFile>("ui/uld/JobHudSimple_StackB_hr1.tex")!;
-        HudMNK = Service.LuminaGameData.GetFile<TexFile>("ui/uld/JobHudMNK0_hr1.tex")!;
-
         GaugeFont = Service.FontAtlas.NewGameFontHandle(new GameFontStyle(GameFontFamilyAndSize.MiedingerMid18));
     }
 
@@ -68,43 +60,47 @@ internal class GaugeVisualizer
         }
     }
 
-    private IDalamudTextureWrap Wrap(TexFile tf, string key)
+    private IDalamudTextureWrap Generate(string key, Func<IDalamudTextureWrap> generate)
     {
-        if (_tex.TryGetValue(key, out var wrap))
-            return wrap;
-
-        var id = ImageData.FromTexFile(tf);
-
-        wrap = Service.Texture.CreateFromRaw(id.ImageSpecification, id.Data);
-        _tex[key] = wrap;
-        return wrap;
+        return _tex.TryGetValue(key, out var wrap) ? wrap : (_tex[key] = generate());
     }
 
-    private IDalamudTextureWrap Tint(TexFile tf, int[] color, string key)
+    private IDalamudTextureWrap Wrap(string key, string path) => Generate(key, () =>
     {
-        if (_tex.TryGetValue(key, out var wrap))
-            return wrap;
-
-        var id = ImageData.FromTexFile(tf);
-
-        uint tint = 0;
-        uint bias = 0;
-
-        for (var i = 0; i < 3; i++)
+        if (Service.LuminaGameData.GetFile<TexFile>(path) is { } tex)
         {
-            var col = color[i];
-            if (col > 0)
-                tint |= (uint)col << (i * 8);
-            else
-                bias |= (uint)-col << (i * 8);
+            var im = ImageData.FromTexFile(tex);
+            return Service.Texture.CreateFromRaw(im.ImageSpecification, im.Data);
         }
+        else
+            return Service.Texture.CreateEmpty(RawImageSpecification.Rgba32(1, 1), true, true);
+    });
 
-        id = id.Tint(tint, bias);
+    private IDalamudTextureWrap Tint(string key, string path, int[] color) => Generate(key, () =>
+    {
+        if (Service.LuminaGameData.GetFile<TexFile>(path) is { } tex)
+        {
+            var id = ImageData.FromTexFile(tex);
 
-        wrap = Service.Texture.CreateFromRaw(id.ImageSpecification, id.Data);
-        _tex[key] = wrap;
-        return wrap;
-    }
+            uint tint = 0;
+            uint bias = 0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                var col = color[i];
+                if (col > 0)
+                    tint |= (uint)col << (i * 8);
+                else
+                    bias |= (uint)-col << (i * 8);
+            }
+
+            id = id.Tint(tint, bias);
+
+            return Service.Texture.CreateFromRaw(id.ImageSpecification, id.Data);
+        }
+        else
+            return Service.Texture.CreateEmpty(RawImageSpecification.Rgba32(1, 1), true, true);
+    });
 
     static Vector2 Scale(float x, float y) => new(x * ImGuiHelpers.GlobalScale, y * ImGuiHelpers.GlobalScale);
 
@@ -116,11 +112,11 @@ internal class GaugeVisualizer
         var crAbs = ImGui.GetCursorScreenPos();
         // -7px on left and right edges (i think ninegrid handles this normally)
         dl.PushClipRect(crAbs + Scale(7, 0), crAbs + Scale(153, 20), true);
-        ImGui.Image(Tint(GaugeSimple, color, key).Handle, Scale(current * 160 / maximum, 20), new Vector2(0.0000f, 0.2632f), new Vector2(1, 0.3947f));
+        ImGui.Image(Tint(key, "ui/uld/Parameter_Gauge_hr1.tex", color).Handle, Scale(current * 160 / maximum, 20), new Vector2(0, 0.2632f), new Vector2(1, 0.3947f));
         dl.PopClipRect();
 
         ImGui.SetCursorPos(origin);
-        ImGui.Image(Wrap(GaugeSimple, "gauge_simple").Handle, Scale(160, 20), new Vector2(0.0000f, 0.0000f), new Vector2(1.0000f, 0.1316f));
+        ImGui.Image(Wrap("gauge_simple", "ui/uld/Parameter_Gauge_hr1.tex").Handle, Scale(160, 20), new Vector2(0, 0), new Vector2(1, 0.1316f));
 
         if (showText)
         {
@@ -138,18 +134,14 @@ internal class GaugeVisualizer
 
     void DrawDiamond(Vector2 origin, string key, bool active, int[] activeColor)
     {
-        var orig = ImGui.GetCursorPos();
-
         ImGui.SetCursorPos(origin);
-        ImGui.Image(Wrap(StackA, "diamond").Handle, Scale(32, 32), new Vector2(0.0000f, 0.0000f), new Vector2(0.5000f, 0.5000f));
+        ImGui.Image(Wrap("diamond", "ui/uld/JobHudSimple_StackA_hr1.tex").Handle, Scale(32, 32), new Vector2(0, 0), new Vector2(0.5f, 0.5f));
 
         if (active)
         {
             ImGui.SetCursorPos(origin);
-            ImGui.Image(Tint(StackA, activeColor, key).Handle, Scale(32, 32), new Vector2(0.5f, 0), new Vector2(1, 0.5f));
+            ImGui.Image(Tint(key, "ui/uld/JobHudSimple_StackA_hr1.tex", activeColor).Handle, Scale(32, 32), new Vector2(0.5f, 0), new Vector2(1, 0.5f));
         }
-
-        ImGui.SetCursorPos(orig);
     }
 
     void DrawDiamond(Vector2 origin, bool active) => DrawDiamond(origin, "diamond_active", active, [20, -50, -150]);
@@ -189,7 +181,7 @@ internal class GaugeVisualizer
         {
             ImGui.SetCursorPos(origin + Scale(0, 36));
             if (Service.Texture.GetFromGame("ui/uld/JobHudDRK0_hr1.tex").TryGetWrap(out var wrap, out _))
-                ImGui.Image(wrap.Handle, Scale(28, 28), new Vector2(0.8526f, 0.0000f), new Vector2(0.9263f, 0.0946f));
+                ImGui.Image(wrap.Handle, Scale(28, 28), new Vector2(0.8526f, 0.f), new Vector2(0.9263f, 0.0946f));
         }
     }
 
@@ -198,10 +190,11 @@ internal class GaugeVisualizer
         var gauge = clientState.GetGauge<MonkGauge>();
         var origin = ImGui.GetCursorPos();
 
-        var wrap = Wrap(HudMNK, "mnk_hud");
+        if (!Service.Texture.GetFromGame("ui/uld/JobHudMNK0_hr1.tex").TryGetWrap(out var wrap, out _))
+            return;
 
         // beast chakra
-        ImGui.Image(wrap.Handle, Scale(114, 48), new Vector2(0.0000f, 0.4602f), new Vector2(0.2767f, 0.6726f));
+        ImGui.Image(wrap.Handle, Scale(114, 48), new Vector2(0, 0.4602f), new Vector2(0.2767f, 0.6726f));
 
         void drawChakra(int num)
         {
@@ -213,11 +206,11 @@ internal class GaugeVisualizer
                     break;
                 case BeastChakraType.Raptor:
                     ImGui.SetCursorPos(origin + Scale(8 + 30 * num, 8));
-                    ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.4320f, 0.4602f), new Vector2(0.5097f, 0.6018f));
+                    ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.432f, 0.4602f), new Vector2(0.5097f, 0.6018f));
                     break;
                 case BeastChakraType.Coeurl:
                     ImGui.SetCursorPos(origin + Scale(8 + 30 * num, 8));
-                    ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.3544f, 0.4602f), new Vector2(0.4320f, 0.6018f));
+                    ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.3544f, 0.4602f), new Vector2(0.432f, 0.6018f));
                     break;
             }
         }
@@ -233,46 +226,46 @@ internal class GaugeVisualizer
         if (gauge.Nadi.HasFlag(NadiFlags.Lunar))
         {
             ImGui.SetCursorPos(origin + Scale(98, 9));
-            ImGui.Image(Tint(HudMNK, [60, -20, 255], "mnk_purple").Handle, Scale(30, 30), new Vector2(0.4612f, 0.6018f), new Vector2(0.3883f, 0.7345f));
+            ImGui.Image(Tint("mnk_purple", "ui/uld/JobHudMNK0_hr1.tex", [60, -20, 255]).Handle, Scale(30, 30), new Vector2(0.4612f, 0.6018f), new Vector2(0.3883f, 0.7345f));
         }
 
         if (gauge.Nadi.HasFlag(NadiFlags.Solar))
         {
             ImGui.SetCursorPos(origin + Scale(118, 9));
-            ImGui.Image(Tint(HudMNK, [100, 50, -40], "mnk_yellow").Handle, Scale(30, 30), new Vector2(0.3883f, 0.6018f), new Vector2(0.4612f, 0.7345f));
+            ImGui.Image(Tint("mnk_yellow", "ui/uld/JobHudMNK0_hr1.tex", [100, 50, -40]).Handle, Scale(30, 30), new Vector2(0.3883f, 0.6018f), new Vector2(0.4612f, 0.7345f));
         }
 
         // balls
         ImGui.SetCursorPos(origin + Scale(0, 42));
-        ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.4126f, 0.7434f), new Vector2(0.4903f, 0.8850f));
+        ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.4126f, 0.7434f), new Vector2(0.4903f, 0.885f));
         DrawDiamond(origin + Scale(26, 42), "stacks_opo", gauge.OpoOpoStacks == 1, [80, -80, 0]);
 
         ImGui.SetCursorPos(origin + Scale(50, 42));
-        ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.4903f, 0.7434f), new Vector2(0.5680f, 0.8850f));
+        ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.4903f, 0.7434f), new Vector2(0.568f, 0.885f));
         DrawDiamond(origin + Scale(76, 42), "stacks_raptor", gauge.RaptorStacks == 1, [-50, -100, 255]);
 
         ImGui.SetCursorPos(origin + Scale(100, 42));
-        ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.5680f, 0.7434f), new Vector2(0.6456f, 0.8850f));
+        ImGui.Image(wrap.Handle, Scale(32, 32), new Vector2(0.568f, 0.7434f), new Vector2(0.6456f, 0.885f));
         DrawDiamond(origin + Scale(126, 42), "stacks_coeurl", gauge.CoeurlStacks > 0, [-255, 60, 50]);
         DrawDiamond(origin + Scale(145, 42), "stacks_coeurl", gauge.CoeurlStacks == 2, [-255, 60, 50]);
 
-        var chevron = Wrap(StackB, "stack_b");
-        var chevronLit = Tint(StackB, [95, 0, -115], "stack_b_active");
-        var chevronOvercap = Tint(StackB, [0, -168, -255], "stack_b_overcap");
+        var chevron = Wrap("stack_b", "ui/uld/JobHudSimple_StackB_hr1.tex");
+        var chevronLit = Tint("stack_b_active", "ui/uld/JobHudSimple_StackB_hr1.tex", [95, 0, -115]);
+        var chevronOvercap = Tint("stack_b_overcap", "ui/uld/JobHudSimple_StackB_hr1.tex", [0, -168, -255]);
 
         for (var i = 0; i < 5; i++)
         {
             ImGui.SetCursorPos(origin + Scale(i * 18, 75));
-            ImGui.Image(chevron.Handle, new Vector2(32, 32), new Vector2(0.0000f, 0.0000f), new Vector2(0.5000f, 0.5000f));
+            ImGui.Image(chevron.Handle, new Vector2(32, 32), new Vector2(0, 0), new Vector2(0.5f, 0.5f));
             if (gauge.Chakra > i)
             {
                 ImGui.SetCursorPos(origin + Scale(i * 18, 75));
-                ImGui.Image(chevronLit.Handle, new Vector2(32, 32), new Vector2(0.5000f, 0.0000f), new Vector2(1.0000f, 0.5000f));
+                ImGui.Image(chevronLit.Handle, new Vector2(32, 32), new Vector2(0.5f, 0), new Vector2(1, 0.5f));
             }
             if (gauge.Chakra > i + 5)
             {
                 ImGui.SetCursorPos(origin + Scale(i * 18, 75));
-                ImGui.Image(chevronOvercap.Handle, new Vector2(32, 32), new Vector2(0.5000f, 0.0000f), new Vector2(1.0000f, 0.5000f));
+                ImGui.Image(chevronOvercap.Handle, new Vector2(32, 32), new Vector2(0.5f, 0), new Vector2(1, 0.5f));
             }
         }
     }
