a5acc7cf644694c32823c7f6d90616fe28f89ab9
Equilius <equiliuschamberlain@gmail.com>
2026-07-23 06:40:33 +0100

    Feat: GiantBird AI support

--- BossMod/Modules/Dawntrail/Foray/FATE/GiantBird.cs
+++ BossMod/Modules/Dawntrail/Foray/FATE/GiantBird.cs
@@ -22,7 +22,21 @@ public enum AID : uint {
 }
 
 class GaleCannon(BossModule module) : Components.StandardAOEs(module, AID.GaleCannon, new AOEShapeRect(40.0f, 5.0f));
-class SphereShatter(BossModule module) : Components.StandardAOEs(module, AID.SphereShatter, new AOEShapeCircle(7.0f));
+
+class SphereShatter(BossModule module) : Components.StandardAOEs(module, AID.SphereShatter, new AOEShapeCircle(7.0f)) {
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints) {
+        base.AddAIHints(slot, actor, assignment, hints);
+
+        // This isn't a perfect solution, but the idea is to just keep the player away from the orb radius and direction its moving to prevent
+        // it being caught in the cast. If players are still getting hit can try increasing the radius size to keep them even further out.
+        var petrifogs = WorldState.Actors.Where(a => a.OID is (uint)OID.Petrifog or
+            (uint)OID.Petrifog1 or (uint)OID.Petrifog2 or (uint)OID.Petrifog3 or
+            (uint)OID.Petrifog4 or (uint)OID.Petrifog5 or (uint)OID.Petrifog6).ToList();
+        foreach (var petrifog in petrifogs.Where(a => !a.IsDead && a.CastInfo == null)) {
+            hints.AddForbiddenZone(ShapeContains.Capsule(petrifog.Position, petrifog.Rotation.ToDirection(), 7.0f, 4.0f), WorldState.FutureTime(5.0f));
+        }
+    }
+}
 
 class GiantBirdStates : StateMachineBuilder {
     public GiantBirdStates(BossModule module) : base(module) {
