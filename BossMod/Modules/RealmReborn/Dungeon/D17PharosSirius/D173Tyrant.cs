e2244bf92c2312b89865ec8702c19472d3574a07
Wen <179552879+wen-ff@users.noreply.github.com>
2026-08-03 19:38:04 +0900

    Clean up and squash commits.

--- BossMod/Modules/RealmReborn/Dungeon/D17PharosSirius/D173Tyrant.cs
+++ BossMod/Modules/RealmReborn/Dungeon/D17PharosSirius/D173Tyrant.cs
@@ -22,13 +22,12 @@ public enum SID : uint
     Windburn = 269, // Tyrant->player, extra=0x0
 }
 
-
 sealed class AeroBlast(BossModule module) : Components.RaidwideCast(module, (uint)AID.AeroBlast);
 
-sealed class Bombination(BossModule module) : Components.SimpleAOEs(module, (uint)AID.Bombination, new AOEShapeCircle(6f));
+sealed class Bombination(BossModule module)
+    : Components.SimpleAOEs(module, (uint)AID.Bombination, new AOEShapeCircle(6f));
 
 sealed class SailorAdds(BossModule module) : Components.Adds(module, (uint)OID.ZombieSailor);
-
 
 [SkipLocalsInit]
 sealed class TyrantStates : StateMachineBuilder
@@ -43,7 +42,7 @@ sealed class TyrantStates : StateMachineBuilder
     }
 }
 
-[ModuleInfo(BossModuleInfo.Maturity.WIP,
+[ModuleInfo(BossModuleInfo.Maturity.Contributed,
     StatesType = typeof(TyrantStates),
     ConfigType = null, // replace null with typeof(TyrantConfig) if applicable
     ObjectIDType = typeof(OID),
@@ -66,8 +65,7 @@ public sealed class D173Tyrant : BossModule
 {
     public D173Tyrant(WorldState ws, Actor primary) : this(ws, primary, BuildArena()) { }
 
-
-// Constructor so we can build arena
+    // Constructor so we can build arena
     private D173Tyrant(WorldState ws, Actor primary, (WPos center, ArenaBoundsCustom arena) a) : base(ws, primary,
         a.center,
         a.arena)
@@ -95,27 +93,29 @@ public sealed class D173Tyrant : BossModule
             [
                 new Circle(ArenaCenter, 20f)
             ],
-        [
-            .._shapes,
-            new Rectangle(new(-0f, -20.0f), 4.45f, 0.5f, -0f.Degrees()), // flat shape for gate
-            new Rectangle(new(0f, 20.0f), 4.45f, 0.5f, 0f.Degrees()), // flat shape for gate
-            new Rectangle(new WPos(8.1f, 18.2f), 1.8f, 1f, 30.Degrees()), // first pipe box that connects to pipe column from south ccw
-            new Circle(new WPos(9.5f, 16.5f), 1f), new Rectangle(new WPos(10f, 17.3f), 1f, 1f, 30f.Degrees()),
-            new Rectangle(new WPos(18.5f, 7.5f), 1f, 0.75f, 65f.Degrees()),
-            new Circle(new WPos(19f, 2.5f), 1f), new Rectangle(new WPos(19.8f, 2.6f), 1f, 1f, 1f.Degrees()),
-            new Rectangle(new WPos(20f, 0f), 1f, 0.75f),
-            new Circle(new WPos(18.8f, -2.6f), 1f), new Rectangle(new WPos(19.8f, -2.8f), 1f, 1f, 6f.Degrees()),
-            new Square(new WPos(18.5f, -7.7f), 0.75f, 26f.Degrees()),
-            new Rectangle(new WPos(11.7f, -16.2f), 1.8f, 1f, -30f.Degrees()),
-            new Circle(new WPos(9.5f, -16.5f), 1f), new Rectangle(new WPos(10.0f, -17.3f), 1f, 1f, 60f.Degrees()),
-            new Rectangle(new WPos(-18.25f, -7.0f), 1.8f, 1f, -105f.Degrees()),
-            new Circle(new WPos(-18.4f, -4.9f), 1f), new Rectangle(new WPos(-19.3f, -5.2f), 1f, 1f, -106.5f.Degrees()),
-            new Circle(new WPos(-18.8f, -2.6f), 1f), new Rectangle(new WPos(-19.8f, -2.8f), 1f, 1f, -90.5f.Degrees()),
-            new Square(new WPos(-20f, 0f), 0.75f),
-            new Circle(new WPos(-18.8f, 2.6f), 1f), new Rectangle(new WPos(-19.8f, 2.7f), 1f, 1f, 3f.Degrees()),
-            new Square(new WPos(-18.5f, 7.7f), 0.75f, 25f.Degrees()), // last square cutout before sw lantern.
-        ]);
-
+            [
+                .. _shapes,
+                new Rectangle(new(-0f, -20.0f), 4.45f, 0.5f, -0f.Degrees()), // flat shape for gate
+                new Rectangle(new(0f, 20.0f), 4.45f, 0.5f, 0f.Degrees()), // flat shape for gate
+                new Rectangle(new WPos(8.1f, 18.2f), 1.8f, 1f,
+                    30.Degrees()), // first pipe box that connects to pipe column from south ccw
+                new Circle(new WPos(9.5f, 16.5f), 1f), new Rectangle(new WPos(10f, 17.3f), 1f, 1f, 30f.Degrees()),
+                new Rectangle(new WPos(18.5f, 7.5f), 1f, 0.75f, 65f.Degrees()),
+                new Circle(new WPos(19f, 2.5f), 1f), new Rectangle(new WPos(19.8f, 2.6f), 1f, 1f, 1f.Degrees()),
+                new Rectangle(new WPos(20f, 0f), 1f, 0.75f),
+                new Circle(new WPos(18.8f, -2.6f), 1f), new Rectangle(new WPos(19.8f, -2.8f), 1f, 1f, 6f.Degrees()),
+                new Square(new WPos(18.5f, -7.7f), 0.75f, 26f.Degrees()),
+                new Rectangle(new WPos(11.7f, -16.2f), 1.8f, 1f, -30f.Degrees()),
+                new Circle(new WPos(9.5f, -16.5f), 1f), new Rectangle(new WPos(10.0f, -17.3f), 1f, 1f, 60f.Degrees()),
+                new Rectangle(new WPos(-18.25f, -7.0f), 1.8f, 1f, -105f.Degrees()),
+                new Circle(new WPos(-18.4f, -4.9f), 1f),
+                new Rectangle(new WPos(-19.3f, -5.2f), 1f, 1f, -106.5f.Degrees()),
+                new Circle(new WPos(-18.8f, -2.6f), 1f),
+                new Rectangle(new WPos(-19.8f, -2.8f), 1f, 1f, -90.5f.Degrees()),
+                new Square(new WPos(-20f, 0f), 0.75f),
+                new Circle(new WPos(-18.8f, 2.6f), 1f), new Rectangle(new WPos(-19.8f, 2.7f), 1f, 1f, 3f.Degrees()),
+                new Square(new WPos(-18.5f, 7.7f), 0.75f, 25f.Degrees()), // last square cutout before sw lantern.
+            ]);
         return (ArenaCenter, arena);
     }
 }
