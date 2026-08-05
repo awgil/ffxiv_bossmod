44705e10b471949a395b60e950e2838a258e0f51
Equilius <equiliuschamberlain@gmail.com>
2026-08-05 17:17:35 +0100

    Feat: normal fork tower boss 1

--- BossMod/Modules/Dawntrail/Foray/ForkedTowerMagic/Normal/FTMN1TwoHeadedAevis/FTMN1TwoHeadedAevis.cs
+++ BossMod/Modules/Dawntrail/Foray/ForkedTowerMagic/Normal/FTMN1TwoHeadedAevis/FTMN1TwoHeadedAevis.cs
@@ -1,5 +1,185 @@
 ﻿namespace BossMod.Dawntrail.Foray.ForkedTowerMagic.Normal.FTMN1TwoHeadedAevis;
 
+sealed class ThunderfrostTempest(BossModule module) : Components.RaidwideCast(module, (uint)AID.ThunderfrostTempest);
+sealed class PoisonBreath(BossModule module) : Components.SimpleAOEs(module, (uint)AID.PoisonBreath, new AOEShapeCircle(18.0f));
+sealed class StormsBreath(BossModule module) : Components.SimpleKnockbacks(module, (uint)AID.StormsBreath2, 14.0f);
+sealed class TwoTerrors(BossModule module) : Components.SimpleAOEs(module, (uint)AID.TwoTerrors, new AOEShapeRect(40.0f, 5.0f));
+
+sealed class HissingReprise(BossModule module) : Components.GenericKnockback(module) {
+    private List<Knockback> knockbacks = [];
+    private enum knockbackType { None, West, East}
+    private (knockbackType type, DateTime expireAt)[] knockbackDebuffs = new (knockbackType, DateTime)[PartyState.MaxPartySize];
+    private const float knockbackDistance = 20.0f;
+
+    public override void OnStatusGain(Actor actor, ref ActorStatus status) {
+        var debuff = status.ID switch {
+            (uint)SID.EasterlyReprise => knockbackType.East,
+            (uint)SID.WesterlyReprise => knockbackType.West,
+            _ => knockbackType.None
+        };
+
+        if (debuff != knockbackType.None) {
+            var slot = Raid.FindSlot(actor.InstanceID);
+            if (slot >= 0 && slot < PartyState.MaxPartySize) {
+                knockbackDebuffs[slot] = (debuff, status.ExpireAt);
+            }
+        }
+    }
+
+    public override void OnStatusLose(Actor actor, ref ActorStatus status) {
+        if (status.ID is (uint)SID.EasterlyReprise or (uint)SID.WesterlyReprise) {
+            var slot = Raid.FindSlot(actor.InstanceID);
+            if (slot >= 0 && slot < PartyState.MaxPartySize) {
+                knockbackDebuffs[slot] = (knockbackType.None, default);
+            }
+        }
+    }
+
+    public override ReadOnlySpan<Knockback> ActiveKnockbacks(int slot, Actor actor) {
+        knockbacks.Clear();
+
+        if (slot > PartyState.MaxPartySize) {
+            return [];
+        }
+
+        foreach (var (i, player) in Raid.WithSlot(false, true, true)) {
+            if (knockbackDebuffs[i].type == knockbackType.East) {
+                knockbacks.Add(new(player.Position, knockbackDistance, knockbackDebuffs[i].expireAt, kind: Kind.DirRight));
+            }
+
+            if (knockbackDebuffs[i].type == knockbackType.West) {
+                knockbacks.Add(new(player.Position, knockbackDistance, knockbackDebuffs[i].expireAt, kind: Kind.DirLeft));
+            }
+        }
+
+        return CollectionsMarshal.AsSpan(knockbacks);
+    }
+}
+
+// TODO figure out cast timers for ones that explode after being hit
+// TODO consider changing the raidwide part to the single cast one + figure out the cast timers for the raidwide difference one as well
+// TODO clean up
+sealed class TwoHeadedAevisCluster(BossModule module) : Components.GenericAOEs(module) {
+    private List<AOEInstance> aoes = [];
+    public List<(Actor actor, bool clusterHit)> orbs = []; // clusterHit is for when the orb is hit by its cluster element attack
+    public readonly AOEShapeCircle shape = new(15.0f);
+
+    public override void OnActorCreated(Actor actor) {
+        if (actor.OID is (uint)OID.SwirlingOrb or (uint)OID.BallLightning) {
+            orbs.Add((actor, false));
+        }
+    }
+
+    public override void OnActorDeath(Actor actor) {
+        if (actor.OID is (uint)OID.SwirlingOrb or (uint)OID.BallLightning) {
+            orbs.Remove((actor, false));
+        }
+    }
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell) {
+        if (spell.Action.ID == (uint)AID.Summon)
+        {
+            orbs.Clear();
+            aoes.Clear();
+        }
+
+        if (spell.Action.ID == (uint)AID.IceClusterTeleport) {
+            var aoe = new AOEInstance(shape, spell.LocXZ, spell.Rotation, Module.CastFinishAt(spell));
+            aoes.Add(aoe);
+        }
+
+        if (spell.Action.ID == (uint)AID.IceClusterTeleport) {
+            var aoe = new AOEInstance(shape, spell.LocXZ, spell.Rotation, Module.CastFinishAt(spell));
+            for (int i = 0; i < orbs.Count; i++) {
+                var orb = orbs[i];
+                if (orb.actor.OID == (uint)OID.SwirlingOrb && aoe.Shape.Check(aoe.Origin, orb.actor.Position, orb.actor.Rotation)) {
+                    aoes.Add(new(shape, orb.actor.Position, orb.actor.Rotation, Module.CastFinishAt(spell, 3.0f)));
+                    orb.clusterHit = true;
+                    orbs[i] = orb;
+                }
+            }
+        }
+
+        if (spell.Action.ID == (uint)AID.LightningClusterTeleport) {
+            var aoe = new AOEInstance(shape, spell.LocXZ, spell.Rotation, Module.CastFinishAt(spell));
+            aoes.Add(aoe);
+        }
+
+
+        if (spell.Action.ID == (uint)AID.LightningClusterTeleport) {
+            var aoe = new AOEInstance(shape, spell.LocXZ, spell.Rotation, Module.CastFinishAt(spell));
+            for (int i = 0; i < orbs.Count; i++) {
+                var orb = orbs[i];
+                if (orb.actor.OID == (uint)OID.BallLightning && aoe.Shape.Check(aoe.Origin, orb.actor.Position, orb.actor.Rotation)) {
+                    aoes.Add(new(shape, orb.actor.Position, orb.actor.Rotation, Module.CastFinishAt(spell, 3.0f)));
+                    orb.clusterHit = true;
+                    orbs[i] = orb;
+                }
+            }
+        }
+
+        if (spell.Action.ID == (uint)AID.TwoHeadedAevisThunderfrostTempest) { // Uses sinlge cast, so the aoes are not drawn multiple times
+            foreach (var orb in orbs) {
+                if (orb.clusterHit == false) {
+                    aoes.Add(new(shape, orb.actor.Position, orb.actor.Rotation, Module.CastFinishAt(spell, 3.0f)));
+                }
+            }
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell) {
+        if (spell.Action.ID is (uint)AID.IceCluster or (uint)AID.HypothermalCombustion or (uint)AID.LightningCluster or (uint)AID.Shock) {
+            aoes.Sort((a, b) => a.Activation.CompareTo(b.Activation));
+            if (aoes.Count > 0) {
+                aoes.RemoveAt(0);
+            }
+        }
+    }
+
+    public override ReadOnlySpan<AOEInstance> ActiveAOEs(int slot, Actor actor) => CollectionsMarshal.AsSpan(aoes);
+}
+
+// TODO confirm spell timers
+sealed class Blaze(BossModule module) : Components.GenericAOEs(module) {
+    private List<AOEInstance> aoes = [];
+    private readonly AOEShapeCircle inner = new(5.0f);
+    private readonly AOEShapeDonut outer = new(5.0f, 60.0f);
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell) {
+        if (spell.Action.ID is (uint)AID.BlazeInner or (uint)AID.BlazeInner1 or (uint)AID.BlazeInner2) {
+            aoes.Add(new(inner, spell.LocXZ, spell.Rotation, Module.CastFinishAt(spell)));
+            aoes.Add(new(outer, spell.LocXZ, spell.Rotation, Module.CastFinishAt(spell, 2.5f)));
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell) {
+        if (spell.Action.ID is (uint)AID.BlazeInner or (uint)AID.BlazeInner1 or (uint)AID.BlazeInner2 or (uint)AID.BlazeloopOuter) {
+            if (aoes.Count > 0) {
+                aoes.Sort((a, b) => a.Activation.CompareTo(b.Activation));
+                aoes.RemoveAt(0);
+            }
+        }
+    }
+
+    public override ReadOnlySpan<AOEInstance> ActiveAOEs(int slot, Actor actor) {
+        int show = 0;
+        var incomingAOEs = aoes.OrderBy(a => a.Activation).Take(2).ToList();
+        foreach (ref var aoe in CollectionsMarshal.AsSpan(incomingAOEs)) {
+            aoe.Color = show == 0 ? Colors.Danger : Colors.AOE;
+            aoe.Risky = show == 0;
+            show++;
+        }
+
+        return CollectionsMarshal.AsSpan(incomingAOEs);
+    }
+}
+
+sealed class ArcaneBeacon : Components.SimpleAOEs {
+    public ArcaneBeacon(BossModule module) : base(module, (uint)AID.ArcaneBeacon, new AOEShapeRect(60.0f, 2.5f)) {
+        MaxCasts = 8;
+    }
+}
+
 [ModuleInfo(BossModuleInfo.Maturity.Dummy,
     StatesType = typeof(TwoHeadedAevisStates),
     ConfigType = null, // replace null with typeof(TwoHeadedAevisConfig) if applicable
@@ -9,7 +189,7 @@
     TetherIDType = typeof(TetherID),
     IconIDType = typeof(IconID),
     PrimaryActorOID = (uint)OID.GreenHead1,
-    Contributors = "The Combat Reborn Team (LTS)",
+    Contributors = "Equilius",
     Expansion = BossModuleInfo.Expansion.Dawntrail,
     Category = BossModuleInfo.Category.Foray,
     GroupType = BossModuleInfo.GroupType.CFC,
--- BossMod/Modules/Dawntrail/Foray/ForkedTowerMagic/Normal/FTMN1TwoHeadedAevis/FTMN1TwoHeadedAevisEnums.cs
+++ BossMod/Modules/Dawntrail/Foray/ForkedTowerMagic/Normal/FTMN1TwoHeadedAevis/FTMN1TwoHeadedAevisEnums.cs
@@ -1,93 +1,106 @@
 ﻿namespace BossMod.Dawntrail.Foray.ForkedTowerMagic.Normal.FTMN1TwoHeadedAevis;
 
-public enum OID : uint
-{
-    Helper = 0x233C, // R0.500, x16, Helper type
-    UnknownActor = 0x4C24, // R1.000, x2
+public enum OID : uint {
+    TwoHeadedAevis = 0x4C11, // R18.000, x1
     GreenHead1 = 0x4C12, // R15.000, x1
+    GreenHead2 = 0x4C14, // R1.000, x1
     BlueHead1 = 0x4C13, // R15.000, x1
     BlueHead2 = 0x4C15, // R1.000, x1
-    TwoHeadedAevis = 0x4C11, // R18.000, x1
-    Actor1ea1a1 = 0x1EA1A1, // R0.500-2.000, x3, EventObj type
-    GreenHead2 = 0x4C14, // R1.000, x1
-    Actor1e8fb8 = 0x1E8FB8, // R2.000, x1, EventObj type
-    Actor1e8f2f = 0x1E8F2F, // R0.500, x1, EventObj type
+    Helper = 0x233C, // R0.500, x16, Helper type
     SwirlingOrb = 0x4C17, // R2.800, x0 (spawn during fight)
     BallLightning = 0x4C16, // R2.400, x0 (spawn during fight)
     ArcaneFont = 0x4B73, // R1.000, x0 (spawn during fight)
+
+    UnknownActor = 0x4C24, // R1.000, x2
+    Actor1e8fb8 = 0x1E8FB8, // R2.000, x1, EventObj type
+    Actor1e8f2f = 0x1E8F2F, // R0.500, x1, EventObj type
+    Actor1ea1a1 = 0x1EA1A1, // R0.500-2.000, x3, EventObj type
 }
 
-public enum AID : uint
-{
-    Ability_ = 47614, // TwoHeadedAevis->self, 7.2+0.8s cast, single-target
-    Ability_1 = 48243, // Helper->location, 8.0s cast, range 30 circle
-    Ability_StormsBreath = 47616, // Helper->location, 8.0s cast, ???
-    Ability_StormsBreath1 = 47613, // GreenHead1->self, 8.0s cast, single-target
+public enum AID : uint {
+    AutoAttackBlueHead = 47754, // BlueHead2->player, no cast, single-target
+    AutoAttackGreenHead = 47753, // GreenHead2->player, no cast, single-target
+    Buffet = 49726, // BlueHead1/GreenHead1->self, 5.0s cast, single-target - Applies debuffs of hero
+    Buffet1 = 49727, // TwoHeadedAevis->self, 5.0s cast, single-target
+    TwoHeadedAevisPoisonBreath = 47615, // TwoHeadedAevis->self, 7.2+0.8s cast, single-target
+    PoisonBreath = 47617, // Helper->location, 8.0s cast, range 18 circle
+    PoisonBreath1 = 50715, // BlueHead1->self, 8.0s cast, single-target
+
+    TwoHeadedAevisStormsBreath = 47614, // TwoHeadedAevis->self, 7.2+0.8s cast, single-target
+    StormsBreath = 47616, // Helper->location, 8.0s cast, ???
+    StormsBreath1 = 47613, // GreenHead1->self, 8.0s cast, single-target
+    StormsBreath2 = 48243, // Helper->location, 8.0s cast, range 30 circle
+
+    TwoHeadedAevisThunderfrostTempest= 47736, // TwoHeadedAevis->self, 5.0s cast, single-target
+    ThunderfrostTempest = 47735, // GreenHead1/BlueHead1->self, 5.0s cast, single-target
+    ThunderfrostTempestVisual = 47737, // Helper->self, no cast, ???
+    ThunderfrostTempestVisual1 = 47738, // Helper->self, no cast, ???
+
+    TwoHeadedAevisTwoTerrors = 50656, // TwoHeadedAevis->self, 5.0s cast, single-target
+    TwoHeadedAevisTwoTerrors1 = 50657, // TwoHeadedAevis->self, 5.0s cast, single-target
+    TwoTerrorsCast = 50655, // GreenHead1/BlueHead1->self, 6.0s cast, single-target
+    TwoTerrors = 50658, // Helper->self, 6.0s cast, range 40 width 10 rect
+
+    TwoHeadedAevisHissingReprise = 49723, // TwoHeadedAevis->self, 3.0s cast, single-target
+    HissingReprise = 49722, // GreenHead1/BlueHead1->self, 3.0s cast, single-target - Applies EasterlyReprise/WesterlyReprise
+    HissingRepriseBuffet = 49725, // Helper->self, no cast, ???
+    HissingRepriseBuffet1 = 49724, // Helper->self, no cast, ???
+
+    TwoHeadedAevisSummon = 47705, // TwoHeadedAevis->self, 3.0s cast, single-target
+    Summon = 47704, // GreenHead1/BlueHead1->self, 3.0s cast, single-target
+
+    TwoHeadedAevisCluster = 47643, // TwoHeadedAevis->self, 7.4s cast, single-target
+    IceCluster = 48220, // BlueHead1->self, 8.0s cast, single-target
+    IceClusterTeleport = 50698, // Helper->location, 8.0s cast, range 15 circle
+    IceClusterTeleport1 = 47645, // BlueHead2->location, 8.0s cast, single-target
+    HypothermalCombustion = 47707, // SwirlingOrb->self, 2.0s cast, range 15 circle
+
+    LightningCluster = 47642, // GreenHead1->self, 8.0s cast, single-target
+    LightningClusterTeleport = 50697, // Helper->location, 8.0s cast, range 15 circle
+    LightningClusterTeleport1 = 47644, // GreenHead2->location, 8.0s cast, single-target
+    Shock = 47706, // BallLightning->self, 2.0s cast, range 15 circle
+
+    TwoHeadedAevisBlaze = 47656, // TwoHeadedAevis->self, 5.3s cast, single-target
+    Blaze = 47659, // BlueHead2->location, 6.0s cast, single-target
+    Blaze1 = 47663, // BlueHead2->location, 6.0s cast, single-target
+    Blaze2 = 47664, // GreenHead2->location, 6.0s cast, single-target
+    BlazeInner = 50703, // Helper->location, 6.0s cast, range 5 circle
+    BlazeInner1 = 50704, // Helper->location, 6.0s cast, range 5 circle
+    BlazeInner2 = 50705, // Helper->location, 6.0s cast, range 5 circle
+    Blazeloop = 47654, // BlueHead1->self, 6.0s cast, single-target
+    Blazeloop2 = 47661, // BlueHead1->self, 6.0s cast, single-target
+    Blazeloop3 = 47662, // GreenHead1->self, 5.3+0.7s cast, single-target
+    BlazeloopOuter = 47660, // Helper->self, 2.5s cast, range 5-60 donut
+
+    TwoHeadedAevisArcaneRevelation = 49717, // TwoHeadedAevis->self, 3.0s cast, single-target
+    ArcaneRevelation = 49716, // GreenHead1/BlueHead1->self, 3.0s cast, single-target
+    ArcaneBeacon = 49720, // ArcaneFont->self, 4.0s cast, range 60 width 5 rect
+
+    // TODO visual stuff / instant casts from actors most likely
     Weaponskill_Aethersplit = 48642, // GreenHead2->BlueHead2, no cast, single-target
-    AutoAttack_ = 47754, // BlueHead2->player, no cast, single-target
-    AutoAttack_1 = 47753, // GreenHead2->player, no cast, single-target
     Ability_2 = 50710, // Helper->player, no cast, single-target
     Ability_3 = 50709, // Helper->player, no cast, single-target
-    Ability_ThunderfrostTempest = 47735, // GreenHead1/BlueHead1->self, 5.0s cast, single-target
-    Ability_4 = 47736, // TwoHeadedAevis->self, 5.0s cast, single-target
-    Ability_ThunderfrostTempest1 = 47737, // Helper->self, no cast, ???
-    Ability_ThunderfrostTempest2 = 47738, // Helper->self, no cast, ???
-    Ability_5 = 50656, // TwoHeadedAevis->self, 5.0s cast, single-target
-    Ability_TwoTerrors = 50655, // GreenHead1/BlueHead1->self, 6.0s cast, single-target
-    Ability_TwoTerrors1 = 50658, // Helper->self, 6.0s cast, range 40 width 10 rect
-    Ability_6 = 50657, // TwoHeadedAevis->self, 5.0s cast, single-target
-    Ability_HissingReprise = 49722, // GreenHead1/BlueHead1->self, 3.0s cast, single-target
-    Ability_7 = 49723, // TwoHeadedAevis->self, 3.0s cast, single-target
-    Ability_Buffet = 49725, // Helper->self, no cast, ???
-    Ability_Buffet1 = 49724, // Helper->self, no cast, ???
-    Ability_Summon = 47704, // GreenHead1/BlueHead1->self, 3.0s cast, single-target
-    Ability_8 = 47705, // TwoHeadedAevis->self, 3.0s cast, single-target
-    Ability_9 = 47643, // TwoHeadedAevis->self, 7.4s cast, single-target
-    Ability_IceCluster = 47645, // BlueHead2->location, 8.0s cast, single-target
-    Ability_LightningCluster = 50697, // Helper->location, 8.0s cast, range 15 circle
-    Ability_LightningCluster1 = 47642, // GreenHead1->self, 8.0s cast, single-target
-    Ability_IceCluster1 = 50698, // Helper->location, 8.0s cast, range 15 circle
-    Ability_LightningCluster2 = 47644, // GreenHead2->location, 8.0s cast, single-target
-    Ability_IceCluster2 = 48220, // BlueHead1->self, 8.0s cast, single-target
-    Ability_HypothermalCombustion = 47707, // SwirlingOrb->self, 2.0s cast, range 15 circle
-    Ability_Shock = 47706, // BallLightning->self, 2.0s cast, range 15 circle
-    Ability_10 = 47656, // TwoHeadedAevis->self, 5.3s cast, single-target
-    Ability_Blaze = 47659, // BlueHead2->location, 6.0s cast, single-target
-    Ability_Blaze1 = 50703, // Helper->location, 6.0s cast, range 5 circle
-    Ability_Blazeloop = 47654, // BlueHead1->self, 6.0s cast, single-target
-    Ability_Blazeloop1 = 47660, // Helper->self, 2.5s cast, range 5-60 donut
-    Ability_Blaze2 = 47663, // BlueHead2->location, 6.0s cast, single-target
-    Ability_Blaze3 = 50704, // Helper->location, 6.0s cast, range 5 circle
-    Ability_Blazeloop2 = 47661, // BlueHead1->self, 6.0s cast, single-target
-    Ability_Blazeloop3 = 47662, // GreenHead1->self, 5.3+0.7s cast, single-target
     Ability_11 = 47657, // TwoHeadedAevis->self, no cast, single-target
-    Ability_Blaze4 = 47664, // GreenHead2->location, 6.0s cast, single-target
-    Ability_Blaze5 = 50705, // Helper->location, 6.0s cast, range 5 circle
-    Ability_ArcaneRevelation = 49716, // GreenHead1/BlueHead1->self, 3.0s cast, single-target
-    Ability_12 = 49717, // TwoHeadedAevis->self, 3.0s cast, single-target
-    Ability_ArcaneBeacon = 49720, // ArcaneFont->self, 4.0s cast, range 60 width 5 rect
-    Ability_13 = 47615, // TwoHeadedAevis->self, 7.2+0.8s cast, single-target
-    Ability_PoisonBreath = 47617, // Helper->location, 8.0s cast, range 18 circle
-    Ability_PoisonBreath1 = 50715, // BlueHead1->self, 8.0s cast, single-target
 }
 
-public enum SID : uint
-{
-    UnknownStatus = 2552, // none->TwoHeadedAevis, extra=0x470/0x471
-    VulnerabilityUp = 2347, // Helper/SwirlingOrb/BallLightning->player, extra=0x1/0x2/0x3/0x4/0x5/0x6
+public enum SID : uint {
+    EpicHero = 4192, // none->player, extra=0x0
+    FatedHero = 4194, // none->player, extra=0x0
+    FatedVillain = 5401, // none->BlueHead1, extra=0x0
+    EpicVillain = 5400, // none->GreenHead1, extra=0x0
     EasterlyReprise = 5403, // none->player, extra=0x0
     WesterlyReprise = 5404, // none->player, extra=0x0
-
+    UnknownStatus = 2552, // none->TwoHeadedAevis, extra=0x470/0x471
 }
 
-public enum IconID : uint
-{
-    Icon_m0811trg02t0a1 = 585, // player->self
+public enum IconID : uint {
+    KnockbackTimer = 585, // player->self
 }
 
-public enum TetherID : uint
-{
+public enum TetherID : uint {
     Tether_chn_m0560_0t2 = 411, // GreenHead2/BlueHead2->UnknownActor
     Tether_chn_m0560_ice_0t2 = 413, // BlueHead2->UnknownActor
     Tether_chn_m0560_elc_0t2 = 412, // GreenHead2->UnknownActor
+    _Gen_Tether_chn_tergetfix2k1 = 429, // player->GreenHead2/BlueHead2
+
 }
