4d857b4095342a434877872a6c2e45d22f2ddc6b
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2026-08-05 18:10:58 -0400

    a couple of modules having a biscuit

--- BossMod/Modules/Dawntrail/Foray/CriticalEngagement/ElmGigas.cs
+++ BossMod/Modules/Dawntrail/Foray/CriticalEngagement/ElmGigas.cs
@@ -2,17 +2,109 @@
 
 public enum OID : uint
 {
-    Boss = 0x4BD9,
-    Helper = 0x233C,
+    Boss = 0x4BD9, // R3.500, x1
+    Helper = 0x233C, // R0.500, x26, Helper type
+    UnbowedSpirit = 0x4BDA, // R4.000, x0 (spawn during fight)
 }
+
+public enum AID : uint
+{
+    AutoAttack = 50851, // Boss->player, no cast, single-target
+    AncientAeroIIICast = 47544, // Boss->self, 3.5+1.5s cast, single-target
+    AncientAeroIII = 48041, // Helper->self, 5.0s cast, ???
+    UnbowedSpiritCast = 47530, // Boss->self, 3.0+1.0s cast, single-target
+    UnbowedSpiritVoidzone = 47531, // Helper->self, no cast, range 4 circle
+    SpinningSweep = 47541, // Boss->self, 6.0s cast, range 40 120-degree cone
+    InspiritedCrosswindsCast = 47533, // Boss->self, 6.0+0.8s cast, single-target
+    InspiritedCrosswinds = 47535, // 4BDA->self, 6.0s cast, range 60 width 8 cross
+    InspiritedImpactCast = 47542, // Boss->self, 3.0s cast, single-target
+    InspiritedImpact = 47543, // Helper->self, 9.6s cast, range 25 circle
+    InspiritedHurricaneCast = 47536, // Boss->self, 4.3+0.7s cast, single-target
+    InspiritedHurricaneCircle = 47537, // Helper->self, 5.0s cast, range 12 circle
+    InspiritedHurricaneCross = 47538, // Helper->self, 5.0s cast, range 60 width 10 cross
+    AncientAero = 47540, // Helper->self, 3.0s cast, range 70 width 6 rect
+    InspiritedCycloneCast = 47532, // Boss->self, 5.0+1.0s cast, single-target
+    InspiritedCyclone = 47534, // 4BDA->self, 6.0s cast, range 12 circle
+}
+
+public enum SID : uint
+{
+    Unk = 2234, // none->4BDA, extra=0xFFE4/0x1E/0xFFAB
+}
+
+class AncientAeroIII(BossModule module) : Components.RaidwideCastDelay(module, AID.AncientAeroIIICast, AID.AncientAeroIII, 1.5f);
+
+class SpinningSweep(BossModule module) : Components.StandardAOEs(module, AID.SpinningSweep, new AOEShapeCone(40, 60.Degrees()));
+
+class UnbowedSpirit(BossModule module) : Components.PersistentVoidzone(module, 4, m => m.Enemies(OID.UnbowedSpirit))
+{
+    enum Mode
+    {
+        Line,
+        Circle,
+        None
+    }
+
+    Mode _currentMode;
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID is AID.InspiritedCrosswinds or AID.InspiritedCyclone)
+            _currentMode = Mode.None;
+
+        if ((AID)spell.Action.ID is AID.UnbowedSpiritCast)
+            _currentMode = Mode.Line;
+
+        if ((AID)spell.Action.ID == AID.InspiritedHurricaneCast)
+            _currentMode = Mode.Circle;
+    }
+
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        foreach (var wind in Sources(Module))
+        {
+            hints.AddForbiddenZone(ShapeDistance.Circle(wind.Position, 4));
+
+            switch (_currentMode)
+            {
+                case Mode.Line:
+                    hints.AddForbiddenZone(ShapeDistance.Capsule(wind.Position, wind.Rotation, 4, 4), WorldState.FutureTime(2));
+                    break;
+                case Mode.Circle:
+                    var off = wind.Position - Arena.Center;
+                    var start = wind.Position;
+                    var end = Arena.Center + off.Rotate(-60.Degrees());
+                    hints.AddForbiddenZone(ShapeDistance.DonutSector(Arena.Center, off.Length() - 4, off.Length() + 4, off.ToAngle() - 30.Degrees(), 30.Degrees()), WorldState.FutureTime(4));
+                    hints.AddForbiddenZone(ShapeDistance.Circle(end, 4), WorldState.FutureTime(4));
+                    break;
+            }
+        }
+    }
+}
+
+class InspiritedCrosswinds(BossModule module) : Components.StandardAOEs(module, AID.InspiritedCrosswinds, new AOEShapeCross(60, 4));
+class InspiritedImpact(BossModule module) : Components.StandardAOEs(module, AID.InspiritedImpact, 25, 3);
+class InspiritedHurricane1(BossModule module) : Components.StandardAOEs(module, AID.InspiritedHurricaneCircle, 12);
+class InspiritedHurricane2(BossModule module) : Components.StandardAOEs(module, AID.InspiritedHurricaneCross, new AOEShapeCross(60, 5));
+class InspiritedCyclone(BossModule module) : Components.StandardAOEs(module, AID.InspiritedCyclone, 12);
+class AncientAero(BossModule module) : Components.StandardAOEs(module, AID.AncientAero, new AOEShapeRect(70, 3));
 
 class ElmGigasStates : StateMachineBuilder
 {
     public ElmGigasStates(BossModule module) : base(module)
     {
-        TrivialPhase();
+        TrivialPhase()
+            .ActivateOnEnter<AncientAeroIII>()
+            .ActivateOnEnter<SpinningSweep>()
+            .ActivateOnEnter<UnbowedSpirit>()
+            .ActivateOnEnter<InspiritedCrosswinds>()
+            .ActivateOnEnter<InspiritedImpact>()
+            .ActivateOnEnter<InspiritedHurricane1>()
+            .ActivateOnEnter<InspiritedHurricane2>()
+            .ActivateOnEnter<AncientAero>()
+            .ActivateOnEnter<InspiritedCyclone>();
     }
 }
 
-[ModuleInfo(Incomplete = true, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1093, NameID = 14508)]
+[ModuleInfo(GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1093, NameID = 14508)]
 public class ElmGigas(WorldState ws, Actor primary) : CEModule(ws, primary, new(-390, 700), new ArenaBoundsCircle(29.5f));
--- BossMod/Modules/Dawntrail/Foray/CriticalEngagement/Metamorph.cs
+++ BossMod/Modules/Dawntrail/Foray/CriticalEngagement/Metamorph.cs
@@ -2,17 +2,283 @@
 
 public enum OID : uint
 {
-    Boss = 0x4C77,
-    Helper = 0x233C,
+    Boss = 0x4C77, // R3.000-6.000, x1
+    Helper = 0x233C, // R0.500, x39, Helper type
+    Metamorph = 0x4DFD, // R1.000, x1
+}
+
+public enum AID : uint
+{
+    AutoAttack = 48334, // Boss->player, no cast, single-target
+    AutoAttackCerberus = 48368, // Boss->player, no cast, single-target
+    AutoAttackFan = 48369, // Boss->player, no cast, single-target
+    BlackenedRainCast = 48335, // Boss->self, 4.0+1.0s cast, single-target
+    BlackenedRain = 48336, // Helper->self, 5.0s cast, ???
+    DeathWall = 48367, // 4DFD->self, no cast, range 25-30 donut
+    ChangeCerberus = 48338, // Boss->self, 4.0s cast, single-target
+    ChangeFan = 48339, // Boss->self, 4.0s cast, single-target
+    Revert = 48340, // Boss->self, no cast, single-target
+    CyclonicRing = 48354, // Boss->self, 4.0s cast, range 10-30 donut
+    ShapeshiftingSupercellCast = 48356, // Boss->self, 5.5+0.5s cast, single-target
+    ShapeshiftingSupercellConeSlow = 48357, // Helper->self, 6.0s cast, range 60 90-degree cone
+    ShapeshiftingSupercellConeFast = 48359, // Helper->self, 1.5s cast, range 60 90-degree cone
+    ShapeshiftingSupercellBossRepeat = 48358, // Boss->self, no cast, single-target
+    ShapeshiftingSupercellCircleStart = 50767, // Helper->self, 6.0s cast, range 8 circle
+    ShapeshiftingSupercellCircleRepeat = 48360, // Helper->self, 6.0s cast, range 8 circle
+    ShapeshiftingSupercellDonut1 = 48361, // Helper->self, 6.0s cast, range 8-16 donut
+    ShapeshiftingSupercellDonut2 = 48362, // Helper->self, 6.0s cast, range 16-30 donut
+    MadeMagicCast = 48363, // Boss->self, 4.0s cast, single-target
+    MadeMagicPuddle = 48364, // Helper->self, no cast, range 0 circle
+    CycloneCrossingCast = 48365, // Boss->self, 10.5+1.0s cast, single-target
+    CycloneCrossing = 48366, // Helper->self, 11.5s cast, range 60 width 16 cross
+    DarkDealing = 48337, // Boss->player, 5.0s cast, single-target
+    TongueOfFlame = 48341, // Boss->self, 4.0s cast, range 15 circle
+    HellfireFetch = 48342, // Boss->self, no cast, single-target
+    HellwardBoundFirst = 48343, // Boss->location, 6.0s cast, width 10 rect charge
+    HellwardBoundRest = 48344, // Boss->location, no cast, width 10 rect charge
+    HellfireFetchPuddle = 48345, // Helper->location, 7.0s cast, range 6 circle
+    Jump1 = 50720, // Boss->location, no cast, single-target
+    Jump2 = 48353, // Boss->self, no cast, single-target
+    HellishBreathPre1 = 48347, // Helper->self, 2.0s cast, range 60 60-degree cone
+    HellishBreathPre2 = 48348, // Helper->self, 4.0s cast, range 60 60-degree cone
+    HellishBreathPre3 = 48349, // Helper->self, 6.0s cast, range 60 60-degree cone
+    HellishBreathCast = 48346, // Boss->self, 6.0s cast, single-target
+    HellishBreathBoss1 = 48350, // Boss->self, no cast, single-target
+    HellishBreathBoss2 = 48351, // Boss->self, no cast, single-target
+    HellishBreathBoss3 = 48352, // Boss->self, no cast, single-target
+    HellishBreathAOE1 = 48662, // Helper->self, 1.1s cast, range 60 60-degree cone
+    HellishBreathAOE2 = 48663, // Helper->self, 1.1s cast, range 60 60-degree cone
+    HellishBreathAOE3 = 50677, // Helper->self, 1.1s cast, range 60 60-degree cone
+}
+
+public enum SID : uint
+{
+    Transfiguration = 2548, // Boss->Boss, extra=0x174/0x173
+    AreaOfInfluenceUp = 1909, // none->Helper, extra=0x1/0x2/0x3/0x4/0x5/0x6/0x7
+}
+
+class BlackenedRain(BossModule module) : Components.RaidwideCastDelay(module, AID.BlackenedRainCast, AID.BlackenedRain, 1.1f);
+class DarkDealing(BossModule module) : Components.SingleTargetCast(module, AID.DarkDealing);
+
+class CyclonicRing(BossModule module) : Components.StandardAOEs(module, AID.CyclonicRing, new AOEShapeDonut(10, 30));
+
+class ShapeshiftingSupercellCone(BossModule module) : Components.GenericRotatingAOE(module)
+{
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor) => base.ActiveAOEs(slot, actor).Select(a => a with { Color = ArenaColor.AOE });
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.ShapeshiftingSupercellConeSlow)
+            Sequences.Add(new(new AOEShapeCone(60, 45.Degrees()), spell.LocXZ, spell.Rotation, 30.Degrees(), Module.CastFinishAt(spell), 2.4f, 6, 1));
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID is AID.ShapeshiftingSupercellConeSlow or AID.ShapeshiftingSupercellConeFast)
+            AdvanceSequence(caster.Position, spell.Rotation, WorldState.CurrentTime);
+    }
+}
+class ShapeshiftingSupercellDonut(BossModule module) : Components.ConcentricAOEs(module, [Shape1, Shape2, Shape3, Shape1, Shape2, Shape3])
+{
+    public static readonly AOEShape Shape1 = new AOEShapeCircle(8);
+    public static readonly AOEShape Shape2 = new AOEShapeDonut(8, 16);
+    public static readonly AOEShape Shape3 = new AOEShapeDonut(16, 30);
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.ShapeshiftingSupercellCircleStart)
+            AddSequence(spell.LocXZ, Module.CastFinishAt(spell));
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        var seq = (AID)spell.Action.ID switch
+        {
+            AID.ShapeshiftingSupercellCircleStart => 0,
+            AID.ShapeshiftingSupercellDonut1 => Sequences[0].NumCastsDone > 2 ? 4 : 1,
+            AID.ShapeshiftingSupercellDonut2 => Sequences[0].NumCastsDone > 2 ? 5 : 2,
+            AID.ShapeshiftingSupercellCircleRepeat => 3,
+            _ => -1
+        };
+
+        if (seq >= 0)
+        {
+            AdvanceSequence(seq, caster.Position, WorldState.FutureTime(10));
+            Sequences.RemoveAll(s => s.NumCastsDone >= 6);
+        }
+    }
+}
+
+class CycloneCrossing(BossModule module) : Components.StandardAOEs(module, AID.CycloneCrossing, new AOEShapeCross(60, 8));
+
+class MadeMagic(BossModule module) : Components.GenericAOEs(module)
+{
+    readonly List<(Actor puddle, DateTime max)> _puddle = [];
+    float _radius;
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        foreach (var (p, t) in _puddle)
+            yield return new(new AOEShapeCircle(17.5f), p.Position, default, t);
+
+        if (_radius > 0)
+            foreach (var (p, _) in _puddle)
+                yield return new(new AOEShapeCircle(_radius), p.Position, Color: ArenaColor.Danger);
+    }
+
+    public override void OnActorCreated(Actor actor)
+    {
+        if (actor.OID == 0x1EC09C)
+        {
+            _radius = 0;
+            _puddle.Add((actor, WorldState.FutureTime(13.5f)));
+        }
+    }
+
+    public override void OnActorDestroyed(Actor actor)
+    {
+        if (actor.OID == 0x1EC09C)
+            _puddle.RemoveAll(p => p.puddle == actor);
+    }
+
+    public override void OnStatusGain(Actor actor, ActorStatus status)
+    {
+        if ((SID)status.ID == SID.AreaOfInfluenceUp)
+            _radius = status.Extra * 2.5f;
+    }
+}
+
+class TongueOfFlame(BossModule module) : Components.StandardAOEs(module, AID.TongueOfFlame, 15);
+
+class HellwardBound(BossModule module) : Components.GenericAOEs(module)
+{
+    readonly List<Actor> _arrows = [];
+    readonly List<AOEInstance> _predicted = [];
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor) => _predicted;
+
+    public override void OnActorCreated(Actor actor)
+    {
+        if (actor.OID == 0x1EC09B)
+        {
+            _arrows.Add(actor);
+            Predict();
+        }
+    }
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        if ((AID)spell.Action.ID == AID.HellwardBoundFirst)
+            Predict();
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID is AID.HellwardBoundFirst or AID.HellwardBoundRest && _predicted.Count > 0)
+            _predicted.RemoveAt(0);
+    }
+
+    void Predict()
+    {
+        if (_arrows.Count < 4 || Module.PrimaryActor.CastInfo is not { } ci || _predicted.Count > 0)
+            return;
+
+        _arrows.RemoveAll(a => a.Position.AlmostEqual(Arena.Center, 5));
+
+        var pDir = ci.LocXZ - Module.PrimaryActor.Position;
+
+        try
+        {
+            var a1 = _arrows.First(a => a.Position.InRect(Module.PrimaryActor.Position, pDir.ToAngle(), 90, 0, 6));
+            _arrows.Remove(a1);
+            var a2 = _arrows.First(a => a.Position.InRect(a1.Position, a1.Rotation, 90, 0, 6));
+            _arrows.Remove(a2);
+            var a3 = _arrows[0];
+
+            void dash(WPos start, WPos finish, DateTime activation)
+            {
+                var d = finish - start;
+
+                _predicted.Add(new(new AOEShapeRect(d.Length(), 5), start, d.ToAngle(), activation));
+            }
+
+            dash(Module.PrimaryActor.Position, a1.Position, Module.CastFinishAt(ci));
+            dash(a1.Position, a2.Position, Module.CastFinishAt(ci, 2.1f));
+            dash(a2.Position, a3.Position, Module.CastFinishAt(ci, 4.2f));
+            dash(a3.Position, a3.Position + a3.Rotation.ToDirection() * 35.4f, Module.CastFinishAt(ci, 6.3f));
+        }
+        catch (InvalidOperationException ex)
+        {
+            ReportError($"Unable to predict dashes: {ex}");
+            // add a single zone so we don't loop forever
+            _predicted.Add(new(new AOEShapeRect(pDir.Length(), 5), Module.PrimaryActor.Position, pDir.ToAngle(), Module.CastFinishAt(ci)));
+        }
+    }
+}
+
+class HellfireFetch(BossModule module) : Components.StandardAOEs(module, AID.HellfireFetchPuddle, 6);
+
+class HellishBreath(BossModule module) : Components.GenericAOEs(module)
+{
+    readonly List<AOEInstance> _predicted = [];
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        for (var i = 0; i < Math.Min(2, _predicted.Count); i++)
+        {
+            yield return _predicted[i] with { Color = i == 0 ? ArenaColor.Danger : ArenaColor.AOE, Risky = i == 0 };
+        }
+    }
+
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        base.AddAIHints(slot, actor, assignment, hints);
+
+        if (_predicted.Count > 0)
+        {
+            var src = _predicted[0].Origin;
+            hints.GoalZones.Add(p => p.InCircle(src, 15) ? 0.5f : 0);
+        }
+    }
+
+    public override void OnCastStarted(Actor caster, ActorCastInfo spell)
+    {
+        switch ((AID)spell.Action.ID)
+        {
+            case AID.HellishBreathPre1:
+            case AID.HellishBreathPre2:
+            case AID.HellishBreathPre3:
+                _predicted.Add(new(new AOEShapeCone(60, 30.Degrees()), spell.LocXZ, spell.Rotation, Module.CastFinishAt(spell, 6.3f)));
+                _predicted.SortBy(p => p.Activation);
+                break;
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID is AID.HellishBreathAOE2 or AID.HellishBreathAOE3 or AID.HellishBreathAOE1 && _predicted.Count > 0)
+            _predicted.RemoveAt(0);
+    }
 }
 
 class MetamorphStates : StateMachineBuilder
 {
     public MetamorphStates(BossModule module) : base(module)
     {
-        TrivialPhase();
+        TrivialPhase()
+            .ActivateOnEnter<BlackenedRain>()
+            .ActivateOnEnter<DarkDealing>()
+            .ActivateOnEnter<CyclonicRing>()
+            .ActivateOnEnter<ShapeshiftingSupercellCone>()
+            .ActivateOnEnter<ShapeshiftingSupercellDonut>()
+            .ActivateOnEnter<CycloneCrossing>()
+            .ActivateOnEnter<MadeMagic>()
+            .ActivateOnEnter<TongueOfFlame>()
+            .ActivateOnEnter<HellwardBound>()
+            .ActivateOnEnter<HellfireFetch>()
+            .ActivateOnEnter<HellishBreath>();
     }
 }
 
-[ModuleInfo(Incomplete = true, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1093, NameID = 14801)]
+[ModuleInfo(GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1093, NameID = 14801)]
 public class Metamorph(WorldState ws, Actor primary) : CEModule(ws, primary, new(500, -310), new ArenaBoundsCircle(25));
