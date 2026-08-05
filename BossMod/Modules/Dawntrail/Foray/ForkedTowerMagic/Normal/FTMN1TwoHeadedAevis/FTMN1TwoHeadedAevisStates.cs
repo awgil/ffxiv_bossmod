1384a9958f51991d3413a0e266cd7d94a2bac74e
Equilius <equiliuschamberlain@gmail.com>
2026-08-05 20:06:34 +0100

    revert back

--- BossMod/Modules/Dawntrail/Foray/ForkedTowerMagic/Normal/FTMN1TwoHeadedAevis/FTMN1TwoHeadedAevisStates.cs
+++ BossMod/Modules/Dawntrail/Foray/ForkedTowerMagic/Normal/FTMN1TwoHeadedAevis/FTMN1TwoHeadedAevisStates.cs
@@ -1,16 +1,10 @@
 ﻿namespace BossMod.Dawntrail.Foray.ForkedTowerMagic.Normal.FTMN1TwoHeadedAevis;
 
 [SkipLocalsInit]
-sealed class TwoHeadedAevisStates : StateMachineBuilder {
-    public TwoHeadedAevisStates(BossModule module) : base(module) {
-        TrivialPhase()
-            .ActivateOnEnter<PoisonBreath>()
-            .ActivateOnEnter<StormsBreath>()
-            .ActivateOnEnter<ThunderfrostTempest>()
-            .ActivateOnEnter<TwoTerrors>()
-            .ActivateOnEnter<HissingReprise>()
-            .ActivateOnEnter<TwoHeadedAevisCluster>()
-            .ActivateOnEnter<Blaze>()
-            .ActivateOnEnter<ArcaneBeacon>();
+sealed class TwoHeadedAevisStates : StateMachineBuilder
+{
+    public TwoHeadedAevisStates(BossModule module) : base(module)
+    {
+        TrivialPhase();
     }
 }
