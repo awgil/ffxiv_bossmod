cfcf5f6ede128f03810730f27286a36254d74328
bilk <cR0N7CXAC9O0yJ9dd@gmail.com>
2026-08-06 14:35:07 +0100

    formatting

--- BossMod/QuestBattle/ARealmReborn/ClassJobQuests/ROG/CloyingVictory.cs
+++ BossMod/QuestBattle/ARealmReborn/ClassJobQuests/ROG/CloyingVictory.cs
@@ -15,16 +15,21 @@ internal class CloyingVictory(WorldState ws) : QuestBattle(ws)
             .PauseForCombat(true)
             .WithConnection(new Vector3(-49f, 40f, 37.29f))
             .CompleteAtDestination(),
+
         GoKill(ws, new (-13.95f, 44f, -34.36f), 0xD8E),
         GoKill(ws, new (-13.95f, 44f, -34.36f), 0xD8C),
+
         GoKill(ws, new (-34.90f, 32f, 3.65f), 0xD8E),
-        GoKill(ws, new (-13.95f, 44f, -34.36f), 0xD8D),
+        GoKill(ws, new (-34.90f, 32f, 3.65f), 0xD8D),
+
         new QuestObjective(ws)
             .PauseForCombat(false)
             .WithConnection(new Vector3(-36f, 40f, 17f))
             .CompleteAtDestination(),
+
         GoKill(ws, new (16f, 40f, 37f), 0xD8E),
         GoKill(ws, new (-46f, 40f, 26f), 0xD8A),
+
         new QuestObjective(ws)
             .Hints((player, hints) => hints.PrioritizeAll())
     ];
