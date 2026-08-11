eb3dbaca46de117dd150eecef6cecfcb77215842
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2026-08-11 06:34:23 -0400

    mark tinymage as done

--- BossMod/Modules/Dawntrail/Foray/CriticalEngagement/TinyMage.cs
+++ BossMod/Modules/Dawntrail/Foray/CriticalEngagement/TinyMage.cs
@@ -362,6 +362,6 @@ class TinyMageStates : StateMachineBuilder
     }
 }
 
-[ModuleInfo(Incomplete = true, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1093, NameID = 14795)]
+[ModuleInfo(GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1093, NameID = 14795)]
 public class TinyMage(WorldState ws, Actor primary) : CEModule(ws, primary, new(152, 716), new ArenaBoundsCircle(20));
 
